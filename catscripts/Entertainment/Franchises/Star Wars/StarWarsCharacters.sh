566e8fbd38d8de80b4812ddc14285eb8b184922f
I am not me <impeccabletiming@gmail.com>
2012-10-25 22:12:32 -0400

    Adding C-3PO, R2-D2, Plo Koon, Darth Maul, Revan

--- catscripts/Entertainment/Franchises/Star Wars/StarWarsCharacters.sh
+++ catscripts/Entertainment/Franchises/Star Wars/StarWarsCharacters.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 cat newpages.txt | egrep -i 'Cad Bane|Cadbane' >> CadBane.txt
+cat newpages.txt | egrep -i 'C-3PO|C3PO' >> C-3PO.txt
 cat newpages.txt | egrep -i 'Chewbacca' >> Chewbacca.txt
 cat newpages.txt | egrep -i 'Boba Fett|Bobafett' >> BobaFett.txt
 cat newpages.txt | egrep -i 'Jango Fett|Jangofett' >> JangoFett.txt
 cat newpages.txt | egrep -i 'Jabba the Hutt|\bJabba\b' >> JabbaTheHutt.txt
 cat newpages.txt | egrep -i 'Obi-Wan|Obiwan|Kenobi' >> Obi-WanKenobi.txt
+cat newpages.txt | egrep -i 'Darth Maul' >> DarthMaul.txt
+cat newpages.txt | egrep -i 'Plo Koon' >> PloKoon.txt
+cat newpages.txt | egrep -i 'R2-D2|R2D2' >> R2-D2.txt
+cat newpages.txt | egrep -i '\bRevan' >> Revan.txt
 cat newpages.txt | egrep -i 'Anakin Skywalker|Anakinskywalker|Darth Vader|\bVader\b' >> DarthVader.txt
 cat newpages.txt | egrep -i 'Luke Skywalker|Lukeskywalker' >> LukeSkywalker.txt
 cat newpages.txt | egrep -i 'Mara Jade|Marajade|Mara Skywalker' >> MaraJadeSkywalker.txt
@@ -17,11 +22,16 @@ cat newpages.txt | egrep -i 'Ahsoka Tano|Ahsokatano|Asoka Tano|Asokatano|Ashoka 
 cat newpages.txt | egrep -i '\bYoda\b' >> Yoda.txt
 
 CADBANE=`stat --print=%s CadBane.txt`
+C3PO=`stat --print=%s C-3PO.txt`
 CHEWBACCA=`stat --print=%s Chewbacca.txt`
 BOBAFETT=`stat --print=%s BobaFett.txt`
 JANGOFETT=`stat --print=%s JangoFett.txt`
 JABBA=`stat --print=%s JabbaTheHutt.txt`
 KENOBI=`stat --print=%s Obi-WanKenobi.txt`
+MAUL=`stat --print=%s DarthMaul.txt`
+KOON=`stat --print=%s PloKoon.txt`
+R2D2=`stat --print=%s R2-D2.txt`
+REVAN=`stat --print=%s Revan.txt`
 VADER=`stat --print=%s DarthVader.txt`
 LUKE=`stat --print=%s LukeSkywalker.txt`
 MARA=`stat --print=%s MaraJadeSkywalker.txt`
@@ -36,6 +46,13 @@ if [ $CADBANE -ne 0 ];
 then
   export CATFILE="CadBane.txt"
   export CATNAME="Cad Bane"
+  ./catscripts/Categorize.sh
+fi
+
+if [ $C3PO -ne 0 ];
+then
+  export CATFILE="C-3PO.txt"
+  export CATNAME="C-3PO"
   ./catscripts/Categorize.sh
 fi
 
@@ -71,6 +88,34 @@ if [ $KENOBI -ne 0 ];
 then
   export CATFILE="Obi-WanKenobi.txt"
   export CATNAME="Obi-Wan Kenobi"
+  ./catscripts/Categorize.sh
+fi
+
+if [ $KOON -ne 0 ];
+then
+  export CATFILE="PloKoon.txt"
+  export CATNAME="Plo Koon"
+  ./catscripts/Categorize.sh
+fi
+
+if [ $MAUL -ne 0 ];
+then
+  export CATFILE="DarthMaul.txt"
+  export CATNAME="Darth Maul"
+  ./catscripts/Categorize.sh
+fi
+
+if [ $R2D2 -ne 0 ];
+then
+  export CATFILE="R2-D2.txt"
+  export CATNAME="R2-D2"
+  ./catscripts/Categorize.sh
+fi
+
+if [ $REVAN -ne 0 ];
+then
+  export CATFILE="Revan.txt"
+  export CATNAME="Revan"
   ./catscripts/Categorize.sh
 fi
 
@@ -138,11 +183,16 @@ then
 fi
 
 rm CadBane.txt
+rm C-3PO.txt
 rm Chewbacca.txt
 rm BobaFett.txt
 rm JangoFett.txt
 rm JabbaTheHutt.txt
 rm Obi-WanKenobi.txt
+rm PloKoon.txt
+rm DarthMaul.txt
+rm R2-D2.txt
+rm Revan.txt
 rm DarthVader.txt
 rm LukeSkywalker.txt
 rm MaraJadeSkywalker.txt
