4164731338b7a617d9bea407e3ddb85f32d58edb
I am not me <impeccabletiming@gmail.com>
2012-12-09 13:57:21 -0500

    Adding United States Army, Navy, Air Force, Marines. and Coast Guard

---
+++ catscripts/Government/Countries/United States/US military/USmilitary.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+cat newpages.txt | egrep -i '(US|United States) Marine(|s) (|Corps)|USMC' >> USMC.txt
+cat newpages.txt | egrep -i '(US|United States) Army' >> USArmy.txt
+cat newpages.txt | egrep -i '(US|United States) (|ch)Air Force|USAF' >> USAF.txt
+cat newpages.txt | egrep -i '(US|United States) Navy|\bUSN\b' >> USNavy.txt
+cat newpages.txt | egrep -i '(US|United States) Coast Guard|USCG' >> USCG.txt
+
+USMC=`stat --print=%s USMC.txt`
+USARMY=`stat --print=%s USArmy.txt`
+USAF=`stat --print=%s USAF.txt`
+USNAVY=`stat --print=%s USNavy.txt`
+USCG=`stat --print=%s USCG.txt`
+
+if [ $USMC -ne 0 ];
+then
+  export CATFILE="USMC.txt"
+  export CATNAME="UNited States Marine Corps"
+  ./catscripts/Categorize.sh
+fi
+
+if [ $USARMY -ne 0 ];
+then
+  export CATFILE="USArmy.txt"
+  export CATNAME="United States Army"
+  ./catscripts/Categorize.sh
+fi
+
+if [ $USAF -ne 0 ];
+then
+  export CATFILE="USAF.txt"
+  export CATNAME="United States Air Force"
+  ./catscripts/Categorize.sh
+fi
+
+if [ $USNAVY -ne 0 ];
+then
+  export CATFILE="USNavy.txt"
+  export CATNAME="United States Navy"
+  ./catscripts/Categorize.sh
+fi
+
+if [ $USCG -ne 0 ];
+then
+  export CATFILE="USCG.txt"
+  export CATNAME="United States Coast Guard"
+  ./catscripts/Categorize.sh
+fi
+
+rm USMC.txt
+rm USArmy.txt
+rm USAF.txt
+rm USNavy.txt
+rm USCG.txt
--- catscripts/Government/Countries/United States/UnitedStates.sh
+++ catscripts/Government/Countries/United States/UnitedStates.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-./catscripts/Government/Countries/United\ States/US\ Presidents/Presidents.sh 
+./catscripts/Government/Countries/United\ States/US\ military/USmilitary.sh
+./catscripts/Government/Countries/United\ States/US\ Presidents/Presidents.sh
