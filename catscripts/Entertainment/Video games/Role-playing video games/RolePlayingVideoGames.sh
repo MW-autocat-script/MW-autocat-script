773f7b71e86b191c2f7ff32adcc6be4fe03815ef
I am not me <impeccabletiming@gmail.com>
2012-12-20 18:32:29 -0500

    oops

--- catscripts/Entertainment/Video games/Role-playing video games/RolePlayingVideoGames.sh
+++ catscripts/Entertainment/Video games/Role-playing video games/RolePlayingVideoGames.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 ./catscripts/Entertainment/Video\ games/Role-playing\ video\ games/The\ Last\ Remnant/TheLastRemnant.sh
+./catscripts/Entertainment/Video\ games/Role-playing\ video\ games/Final\ Fantasy\ series/FinalFantasy.sh
+
--- catscripts/Government/Countries/Countries.sh
+++ catscripts/Government/Countries/Countries.sh
@@ -2,4 +2,4 @@
 
 ./catscripts/Government/Countries/Afghanistan/Afghanistan.sh
 ./catscripts/Government/Countries/United\ States/UnitedStates.sh
-./catscripts/Government/Countires/Norway/Norway.sh 
+./catscripts/Government/Countries/Norway/Norway.sh 
