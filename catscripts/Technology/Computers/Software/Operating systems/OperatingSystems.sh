9c3b9d1267096b2eaaafc525d7aedf4e8c889db2
I am not me <impeccabletiming@gmail.com>
2013-01-17 00:51:28 -0500

    Adding Operating systems

--- catscripts/Technology/Computers/Software/Operating systems/OperatingSystems.sh
+++ catscripts/Technology/Computers/Software/Operating systems/OperatingSystems.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+cat newpages.txt | egrep -i 'operating system|\bOS\b' | egrep -iv 'Windows|Unix|Linux|Mac OS|Open(| )BSD|Net(| )BSD|Free(| )BSD|\bDOS\b' >> OperatingSystems.txt
+
+OS=`stat --print=%s OperatingSystems.txt`
+
+if [ $OS -ne 0 ];
+then
+  export CATFILE="OperatingSystems.txt"
+  export CATNAME="Operating systems"
+  ./catscripts/Categorize.sh
+fi
+
 ./catscripts/Technology/Computers/Software/Operating\ systems/Linux/Linux.sh
 ./catscripts/Technology/Computers/Software/Operating\ systems/Windows/Windows.sh
+
+rm OperatingSystems.txt
