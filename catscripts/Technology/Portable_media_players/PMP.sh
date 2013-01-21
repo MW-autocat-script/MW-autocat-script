f3dc40efe8230ab0d6f67f21d5755ea2a7a7a576
I am not me <impeccabletiming@gmail.com>
2013-01-20 19:07:18 -0500

    Adding iPods, iPod touch, iPod shuffle, Zune, and Portable media players

---
+++ catscripts/Technology/Portable_media_players/PMP.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cat newpages.txt | egrep -i 'MP(3|4) player|portable media player' | egrep -iv 'iPod|Zune' > PMP.txt
+
+PMP=`stat --print=%s PMP.txt`
+
+if [ $PMP -ne 0 ];
+then
+  export CATFILE="PMP.txt"
+  export CATNAME="Portable media players"
+  ./catscripts/Categorize.sh
+fi
+
+rm PMP.txt
+
+./catscripts/Technology/Portable_media_players/iPods/iPods.sh
+./catscripts/Technology/Portable_media_players/Zune/Zune.sh
