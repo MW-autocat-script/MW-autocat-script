ebc47207a656af837a65418c2369ef7f3b58f02f
I am not me <impeccabletiming@gmail.com>
2013-02-09 02:49:01 -0500

    Adding Pennsylvania

---
+++ catscripts/Government/Countries/United States/US states/Pennsylvania/Pennsylvania.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+egrep -i 'Pennsylvania|Pennsylvannia|Pennnsylvania|Pensylvania|Pensylvannia' newpages.txt >> Pennsylvania.txt
+
+PENNSYLVANIA=`stat --print=%s Pennnsylvania.txt`
+
+if [ $PENNSYLVANIA -ne 0 ];
+then
+  export CATFILE="Pennnsylvania.txt"
+  export CATNAME="Pennsylvania"
+  ./catscripts/Categorize.sh
+fi
+
+rm Pennnsylvania.txt
--- catscripts/Government/Countries/United States/US states/States.sh
+++ catscripts/Government/Countries/United States/US states/States.sh
@@ -3,4 +3,5 @@
 ./catscripts/Government/Countries/United\ States/US\ states/Alaska/Alaska.sh
 ./catscripts/Government/Countries/United\ States/US\ states/Arizona/Arizona.sh
 ./catscripts/Government/Countries/United\ States/US\ states/Arkansas/Arkansas.sh
+./catscripts/Government/Countries/United\ States/US\ states/Pennsylvania/Pennsylvania.sh
 ./catscripts/Government/Countries/United\ States/US\ states/Wyoming/Wyoming.sh
