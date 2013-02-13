a646c2a0366e4eebb4bc54fcffb9f2ee9b2f147b
TooDumbForAName <impeccabletiming@gmail.com>
2013-02-13 09:28:23 -0500

    forgot to add line for Polio

--- catscripts/Lifestyle/Health/Diseases/Infectious_diseases/Infectiousdiseases.sh
+++ catscripts/Lifestyle/Health/Diseases/Infectious_diseases/Infectiousdiseases.sh
@@ -2,3 +2,4 @@
 
 ./catscripts/Lifestyle/Health/Diseases/Infectious_diseases/Smallpox/Smallpox.sh
 ./catscripts/Lifestyle/Health/Diseases/Infectious_diseases/Chicken_pox/Chickenpox.sh
+./catscripts/Lifestyle/Health/Diseases/Infectious_diseases/Polio/Polio.sh
