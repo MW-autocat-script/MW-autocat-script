f45958bc953c8c92f7bb499e3de18ef6d67ff5eb
I am not me <impeccabletiming@gmail.com>
2013-03-08 20:51:27 -0500

    Cleanup start script

--- catscripts/Entertainment/Video games/VideoGames.sh
+++ catscripts/Entertainment/Video games/VideoGames.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+egrep -i 'Moshi Monsters|moshimonsters|moshling|furnando|furando|meowford|dustbin beaver|dustbinbeaver|dustin beaver|meowford' newpages.txt > MoshiMonsters.txt
+
+MOSHIMONSTERS=`stat --print=%s MoshiMonsters.txt`
+
+if [ $MOSHIMONSTERS -ne 0 ];
+then
+  export CATFILE="MoshiMonsters.txt"
+  export CATNAME="Moshi Monsters"
+  ./catscripts/Categorize.sh
+fi
+
+rm MoshiMonsters.txt
+
 ./catscripts/Entertainment/Video\ games/Cheat\ codes/CheatCodes.sh 
 ./catscripts/Entertainment/Video\ games/MMORPGs/MMORPGs.sh
 ./catscripts/Entertainment/Video\ games/Video\ game\ consoles/Consoles.sh
--- newpagescat.sh
+++ newpagescat.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 date > lastrun
 #Export settings. Copy settings.conf.example to settings.conf as a starting point
 
@@ -36,51 +35,6 @@ python $PYWIKIPEDIADIR/pagegenerators.py -recentchanges -ns:0|sed 's|[0-9][0-9][
 #This fetches a random redirect and checks its target. Inefficient, but better than nothing ATM
 python $PYWIKIPEDIADIR/pagegenerators.py -randomredirect:1 -ns:0|sed 's|[0-9][0-9][0-9]: |\* \[\[|'| sed 's|[0-9][0-9]: |\* \[\[|' | sed 's|[0-9]: |\* \[\[|' | sed 's:$:]]:' >> newpages.txt
  
-#grep keywords from list
-
-
-##Poptropica
-
-cat newpages.txt | egrep -i 'poptropica|potropica|poptopica' > Poptropica.txt
-
-##Moshi Monsters
-
-cat newpages.txt | egrep -i 'Moshi Monsters|moshimonsters|moshling|furnando|furando|meowford|dustbin beaver|dustbinbeaver|dustin beaver|meowford' > MoshiMonsters.txt
-
-##PNPA
-
-cat newpages.txt | egrep -i 'PNPA' > PNPA.txt
-cat newpages.txt | egrep -i '(pnp\b.+neuro|neuro.+pnp)' >> PNPA.txt
-
-
-#Size test
-
-POPTROPICA=`stat --print=%s Poptropica.txt`
-MOSHIMONSTERS=`stat --print=%s MoshiMonsters.txt`
-PNPA=`stat --print=%s PNPA.txt`
-
-
-if [ $POPTROPICA -ne 0 ];
-then
-  export CATFILE="Poptropica.txt"
-  export CATNAME="Poptropica"
-  ./catscripts/Categorize.sh
-fi
-
-if [ $MOSHIMONSTERS -ne 0 ];
-then
-  export CATFILE="MoshiMonsters.txt"
-  export CATNAME="Moshi Monsters"
-  ./catscripts/Categorize.sh
-fi
-
-if [ $PNPA -ne 0 ];
-then
-  export CATFILE="PNPA.txt"
-  export CATNAME="PNPA"
-  ./catscripts/Categorize.sh
-fi
-
 ./catscripts/Entertainment/Entertainment.sh
 ./catscripts/Government/Government.sh
 ./catscripts/Language/Language.sh
@@ -90,14 +44,5 @@ fi
 ./catscripts/Science/Science.sh
 ./catscripts/Technology/Technology.sh
 ./catscripts/Transportation/Transportation.sh
-
-
-#Cleanup
-
-echo "Cleanup"
-
-rm Poptropica.txt
-rm MoshiMonsters.txt
-rm PNPA.txt
 
 rm $PIDFILE
