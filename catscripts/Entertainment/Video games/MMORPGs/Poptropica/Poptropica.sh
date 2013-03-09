460413149ac8436305be4b4f4a5783e02bae4e52
I am not me <impeccabletiming@gmail.com>
2013-03-08 20:34:57 -0500

    Move Poptropica to proper category

---
+++ catscripts/Entertainment/Video games/MMORPGs/Poptropica/Poptropica.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+egrep -i 'poptropica|potropica|poptopica' newpages.txt > Poptropica.txt
+
+POPTROPICA=`stat --print=%s Poptropica.txt`
+
+if [ $POPTROPICA -ne 0 ];
+then
+  export CATFILE="Poptropica.txt"
+  export CATNAME="Poptropica"
+  ./catscripts/Categorize.sh
+fi
+
+rm Poptropica.sh
