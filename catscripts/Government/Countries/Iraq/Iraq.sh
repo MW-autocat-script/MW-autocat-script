d45da0337ae29b35e17e1b070ef605a5b240813a
I am not me <impeccabletiming@gmail.com>
2013-03-17 01:22:10 -0400

    Adding Baghdad

--- catscripts/Government/Countries/Iraq/Iraq.sh
+++ catscripts/Government/Countries/Iraq/Iraq.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-egrep -i 'Iraq' newpages.txt >> Iraq.txt
+egrep -i 'Iraq' newpages.txt | egrep -iv 'Baghdad' >> Iraq.txt
+egrep -i 'Baghdad' newpages.txt >> Baghdad.txt
 
 IRAQ=`stat --print=%s Iraq.txt`
+BAGHDAD=`stat --print=%s Baghdad.txt`
 
 if [ $IRAQ -ne 0 ];
 then
@@ -11,4 +13,12 @@ then
   $CATEGORIZE
 fi
 
+if [ $BAGHDAD -ne 0 ];
+then
+  export CATFILE="Baghdad.txt"
+  export CATNAME="Baghdad"
+  $CATEGORIZE
+fi
+
 rm Iraq.txt
+rm Baghdad.txt
