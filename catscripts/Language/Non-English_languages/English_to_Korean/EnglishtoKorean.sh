f17b02854fc702ca07e740df9da83ddb8dd83a8a
I am not me <impeccabletiming@gmail.com>
2013-03-19 16:31:06 -0400

    Adding English to Korean

---
+++ catscripts/Language/Non-English_languages/English_to_Korean/EnglishtoKorean.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+egrep -i 'How do you say.+in Korean\]\]' newpages.txt >> EnglishtoKorean.txt
+egrep -i 'How do you spell.+in Korean\]\]' newpages.txt >> EnglishtoKorean.txt
+egrep -i '^\[\[[abcdefghijklmopqrstuvwxyz]{1,} in Korean\]\]' newpages.txt >> EnglishtoKorean.txt
+egrep -i 'What is the Korean word for' newpages.txt >> EnglishtoKorean.txt
+egrep -i 'How to say.+in Korean' newpages.txt >> EnglishtoKorean.txt
+
+KOREAN=`stat --print=%s EnglishtoKorean.txt`
+
+if [ $KOREAN -ne 0 ];
+then
+  export CATFILE="EnglishtoKorean.txt"
+  export CATNAME="English to Korean"
+  $CATEGORIZE
+fi
+
+rm EnglishtoKorean.txt
