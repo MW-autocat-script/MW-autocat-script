f2ce16400a9ffd155617da54abe11f24399b6ee5
I am not me <impeccabletiming@gmail.com>
2013-03-19 16:32:33 -0400

    Adding English to Korean

--- catscripts/Language/Non-English_languages/NonEnglish.sh
+++ catscripts/Language/Non-English_languages/NonEnglish.sh
@@ -2,8 +2,9 @@
 
 CURRENTDIR="./catscripts/Language/Non-English_languages"
 
-$CURRENTDIR/English_to_Spanish/EnglishtoSpanish.sh 
+$CURRENTDIR/English_to_French/EnglishtoFrench.sh
 $CURRENTDIR/English_to_German/EnglishtoGerman.sh
 $CURRENTDIR/English_to_Greek/EnglishtoGreek.sh
-$CURRENTDIR/English_to_French/EnglishtoFrench.sh
-$CURRENTDIR/English_to_Italian/EnglishtoItalian.sh 
+$CURRENTDIR/English_to_Italian/EnglishtoItalian.sh
+$CURRENTDIR/English_to_Korean/EnglishtoKorean.sh
+$CURRENTDIR/English_to_Spanish/EnglishtoSpanish.sh
