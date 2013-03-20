7629a3a79e1617d10627ef92eea451beeb848319
I am not me <impeccabletiming@gmail.com>
2013-03-20 02:55:03 -0400

    Adding Nile River; minor fix for Rome

--- catscripts/Government/Countries/Egypt/Egypt.sh
+++ catscripts/Government/Countries/Egypt/Egypt.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-egrep -i 'Egypt(|s|ian)' newpages.txt | egrep -iv 'ancient Egypt' >> Egypt.txt
+egrep -i 'Egypt(|s|ian)' newpages.txt | egrep -iv 'ancient Egypt|Nile Rile|River Nile|Nile delta|Aswan Dam' >> Egypt.txt
 egrep -i 'ancient Egypt' newpages.txt >> AncientEgypt.txt
+egrep -i 'Nile River|River Nile|Nile delta|Aswan dam' newpages.txt >> NileRiver.txt
 
 EGYPT=`stat --print=%s Egypt.txt`
 ANCIENT=`stat --print=%s AncientEgypt.txt`
+NILE=`stat --print=%s NileRiver.txt`
 
 if [ $EGYPT -ne 0 ];
 then
@@ -20,5 +22,13 @@ then
   $CATEGORIZE
 fi
 
+if [ $NILE -ne 0 ];
+then
+  export CATFILE="NileRiver.txt"
+  export CATNAME="Nile River"
+  $CATEGORIZE
+fi
+
 rm Egypt.txt
 rm AncientEgypt.txt
+rm NileRiver.txt
