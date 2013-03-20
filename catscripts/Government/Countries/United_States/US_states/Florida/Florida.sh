9eefce4a6dd041e9a98cf2a655685ef5f3076911
I am not me <impeccabletiming@gmail.com>
2013-03-19 21:23:23 -0400

    Adding Orlando

--- catscripts/Government/Countries/United_States/US_states/Florida/Florida.sh
+++ catscripts/Government/Countries/United_States/US_states/Florida/Florida.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-egrep -i 'Florida' newpages.txt | egrep -iv 'Miami' >> Florida.txt
+egrep -i 'Florida' newpages.txt | egrep -iv 'Miami|Orlando' >> Florida.txt
 egrep -i 'Everglades' newpages.txt >> Florida.txt
 egrep -i 'Miami' newpages.txt | egrep -iv 'CSI(:|| )Miami' >> Miami.txt
+egrep -i 'Orlando' newpages.txt | egrep -iv 'Orlando Bloom' >> Orlando.txt
 
 FLORIDA=`stat --print=%s Florida.txt`
 MIAMI=`stat --print=%s Miami.txt`
+ORLANDO=`stat --print=%s Orlando.txt`
 
 if [ $FLORIDA -ne 0 ];
 then
@@ -21,5 +23,13 @@ then
   $CATEGORIZE
 fi
 
+if [ $ORLANDO -ne 0 ];
+then
+  export CATFILE="Orlando.txt"
+  export CATNAME="Orlando"
+  $CATEGORIZE
+fi
+
 rm Florida.txt
 rm Miami.txt
+rm Orlando.txt
