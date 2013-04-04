bda4f68b4bb3edf4daad664fce8ab69d42fada6e
I am not me <impeccabletiming@gmail.com>
2013-04-04 18:10:30 -0400

    Adding Transportation

--- catscripts/Transportation/Transportation.sh
+++ catscripts/Transportation/Transportation.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+egrep -i 'transportation' newpages.txt >> Transportation.txt
+egrep -i 'school bus' newpages.txt | egrep -iv 'Magic School Bus' >> Transportation.txt
+
+TRANSPORTATION=`stat --print=%s Transportation.txt`
+
+if [ $TRANSPORTATION -ne 0 ];
+then
+  export CATFILE="Transportation.txt"
+  export CATNAME="Transportation"
+  $CATEGORIZE
+fi
+
 CURRENTDIR="./catscripts/Transportation"
 
 #$CURRENTDIR/Airplanes/Airplanes.sh
@@ -7,3 +19,5 @@ $CURRENTDIR/Cars/Cars.sh
 $CURRENTDIR/SUVs/SUVs.sh
 #$CURRENTDIR/Trucks/Trucks.sh
 #$CURRENTDIR/Vans/Vans.sh 
+
+rm Transportation.txt
