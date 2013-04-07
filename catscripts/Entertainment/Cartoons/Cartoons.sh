a7eb22c7262a9a762c7c117034e8812ab5da65fe
I am not me <impeccabletiming@gmail.com>
2013-04-07 12:30:31 -0400

    Adding Pokémon anime

--- catscripts/Entertainment/Cartoons/Cartoons.sh
+++ catscripts/Entertainment/Cartoons/Cartoons.sh
@@ -7,6 +7,7 @@ $CURRENTDIR/Family_Guy/FamilyGuy.sh
 $CURRENTDIR/Futurama/Futurama.sh
 $CURRENTDIR/Kim_Possible/KimPossible.sh
 $CURRENTDIR/My\ Little\ Pony\:\ Friendship\ is\ Magic/MLP.sh
+$CURRENTDIR/Pokémon_anime/PokémonAnime.sh
 $CURRENTDIR/South\ Park/SouthPark.sh
 $CURRENTDIR/Danny\ Phantom/DannyPhantom.sh
 $CURRENTDIR/SpongeBob\ SquarePants/SpongeBobSquarePants.sh
---
+++ catscripts/Entertainment/Cartoons/Pokémon_anime/PokémonAnime.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+egrep -i "\bAsh.+(Misty|Brock|May|Dawn|Iris|Pikachu|Pokemon|Pokémon|Poke'mon)" newpages.txt >> Anime.txt
+egrep -i "(Misty|Brock|May|Dawn|Iris|Pokémon|Pikachu|Pokemon|Poke'mon)" newpages.txt >> Anime.txt
+egrep -i "(Pokémon|Pokemon|Poke'mon).+(cartoon|anime)" newpages.txt >> Anime.txt
+egrep -i "(Anime|cartoon).+(Pokemon|Pokémon|Poke'mon)" newpages.txt >> Anime.txt
+
+ANIME=`stat --print=%s Anime.txt`
+
+if [ $ANIME -ne 0 ];
+then
+  export CATFILE="Anime.txt"
+  export CATNAME="Pokémon anime"
+  $CATEGORIZE
+fi
+
+rm Anime.txt
