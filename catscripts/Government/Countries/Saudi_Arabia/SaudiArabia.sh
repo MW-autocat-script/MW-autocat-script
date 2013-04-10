c0bfd5a7bf5b5427360fc45b0ba430303ffe9e1d
I am not me <impeccabletiming@gmail.com>
2013-04-09 21:52:11 -0400

    Adding Medina

--- catscripts/Government/Countries/Saudi_Arabia/SaudiArabia.sh
+++ catscripts/Government/Countries/Saudi_Arabia/SaudiArabia.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-egrep -i 'Saudi(| )Arabia' newpages.txt | egrep -iv 'Mecca|Riyadh' >> SaudiArabia.txt
+egrep -i 'Saudi(| )Arabia' newpages.txt | egrep -iv 'Mecca|Riyadh|Medina' >> SaudiArabia.txt
 egrep -i 'Mecca' newpages.txt >> Mecca.txt
+egrep -i 'Medina' newpages.txt >> Medina.txt
 egrep -i 'Riyadh' newpages.txt >> Riyadh.txt
 
 SAUDIARABIA=`stat --print=%s SaudiArabia.txt`
 MECCA=`stat --print=%s Mecca.txt`
+MEDINA=`stat --print=%s Medina.txt`
 RIYADH=`stat --print=%s Riyadh.txt`
 
 if [ $SAUDIARABIA -ne 0 ];
@@ -22,6 +24,13 @@ then
   $CATEGORIZE
 fi
 
+if [ $MEDINA -ne 0 ];
+then
+  export CATFILE="Medina.txt"
+  export CATNAME="Medina"
+  $CATEGORIZE
+fi
+
 if [ $RIYADH -ne 0 ];
 then
   export CATFILE="Riyadh.txt"
@@ -31,4 +40,5 @@ fi
 
 rm SaudiArabia.txt
 rm Mecca.txt
+rm Medina.txt
 rm Riyadh.txt
