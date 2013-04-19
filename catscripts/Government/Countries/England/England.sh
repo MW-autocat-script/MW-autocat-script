cf75edc1f4732dd2895ff978b71423431e432575
I am not me <impeccabletiming@gmail.com>
2013-04-19 19:07:26 -0400

    Adding Stonehenge

--- catscripts/Government/Countries/England/England.sh
+++ catscripts/Government/Countries/England/England.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-egrep -i 'England|British' newpages.txt | egrep -iv 'British Isles|London|Church of England|New England' >> England.txt #Since most people don't know the difference between British and English
+egrep -i 'England|British' newpages.txt | egrep -iv 'British Isles|London|Church of England|New England|Stonehenge' >> England.txt #Since most people don't know the difference between British and English
 egrep -i 'London' newpages.txt | egrep -iv 'Jack London' >> London.txt #Jack London was an American author
+egrep -i 'Stonehenge' newpages.txt >> Stonehenge.txt
 
 ENGLAND=`stat --print=%s England.txt`
 LONDON=`stat --print=%s London.txt`
+STONEHENGE=`stat --print=%s Stonehenge.txt`
 
 if [ $ENGLAND -ne 0 ];
 then
@@ -20,5 +22,13 @@ then
   $CATEGORIZE
 fi
 
+if [ $STONEHENGE -ne 0 ];
+then
+  export CATFILE="Stonehenge.txt"
+  export CATNAME="Stonehenge"
+  $CATEGORIZE
+fi
+
 rm England.txt
 rm London.txt
+rm Stonehenge.txt
