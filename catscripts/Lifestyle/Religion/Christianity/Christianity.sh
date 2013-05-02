242c1ce62e84f1a56a0388a528ca4f56886b8f80
I am not me <impeccabletiming@gmail.com>
2013-05-02 18:19:59 -0400

    Adding Mormonism

--- catscripts/Lifestyle/Religion/Christianity/Christianity.sh
+++ catscripts/Lifestyle/Religion/Christianity/Christianity.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-egrep -i 'Christianity|Jesus' newpages.txt | egrep -iv 'Anglican|Catholic|Church of England|Mormon|pope|Lutheran|Martin Luther' >> Christianity.txt
+egrep -i 'Christianity|Jesus' newpages.txt | egrep -iv 'Anglican|Catholic|Church of England|Mormon|pope|Lutheran|Martin Luther|Mormon|Latter(-| )Day(| )(|saint)' >> Christianity.txt
 egrep -i 'Anglican|Church of England' newpages.txt >> Anglican.txt
 egrep -i 'Catholic|Vatican|\bpope(|s)\b|papal' newpages.txt | egrep -iv 'Alexander Pope' >> Catholicism.txt
 egrep -i 'Martin Luther|Lutheran|(95|Ninety-Five) Theses' newpages,txt | egrep -iv 'Martin Luther King' >> Lutheranism.txt
+egrep -i 'Mormon|Latter(-| )Day(| )(|saint)' newpages.txt >> Mormonism.txt
 
 CHRISTIANITY=`stat --print=%s Christianity.txt`
 ANGLICAN=`stat --print=%s Anglican.txt`
 CATHOLICISM=`stat --print=%s Catholicism.txt`
 LUTHERANISM=`stat --print=%s Lutheranism.txt`
+MORMONISM=`stat --print=%s Mormonism.txt`
 
 if [ $CHRISTIANITY -ne 0 ];
 then
@@ -38,7 +40,15 @@ then
   $CATEGORIZE
 fi
 
+if [ $MORMONISM -ne 0 ];
+then
+  export CATFILE="Mormonism.txt"
+  export CATNAME="Mormonism"
+  $CATEGORIZE
+fi
+
 rm Christianity.txt
 rm Anglican.txt
 rm Catholicism.txt
 rm Lutheranism.txt
+rm Mormonism.txt
