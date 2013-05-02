87c88a827d3d26f7fe3747396017d13f97ba0c4c
I am not me <impeccabletiming@gmail.com>
2013-05-02 17:48:12 -0400

    Adding Anglican and Catholicism

--- catscripts/Lifestyle/Religion/Christianity/Christianity.sh
+++ catscripts/Lifestyle/Religion/Christianity/Christianity.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-egrep -i 'Christianity|Jesus' newpages.txt >> Christianity.txt
+egrep -i 'Christianity|Jesus' newpages.txt | egrep -iv 'Anglican|Catholic|Church of England|Mormon|pope' >> Christianity.txt
+egrep -i 'Anglican|Church of England' newpages.txt >> Anglican.txt
+egrep -i 'Catholic|Vatican|\bpope(|s)\b|papal' newpages.txt | egrep -iv 'Alexander Pope' >> Catholicism.txt
 
 CHRISTIANITY=`stat --print=%s Christianity.txt`
+ANGLICAN=`stat --print=%s Anglican.txt`
+CATHOLICISM=`stat --print=%s Catholicism.txt`
 
 if [ $CHRISTIANITY -ne 0 ];
 then
@@ -11,4 +15,20 @@ then
   $CATEGORIZE
 fi
 
+if [ $ANGLICAN -ne 0 ];
+then
+  export CATFILE="Anglican.txt"
+  export CATNAME="Anglican"
+  $CATEGORIZE
+fi
+
+if [ $CATHOLICISM -ne 0 ];
+then
+  export CATFILE="Catholicism.txt"
+  export CATNAME="Catholicism"
+  $CATEGORIZE
+fi
+
 rm Christianity.txt
+rm Anglican.txt
+rm Catholicism.txt
