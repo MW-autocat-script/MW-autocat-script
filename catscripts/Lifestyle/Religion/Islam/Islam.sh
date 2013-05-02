1d15a9a82c3197bdc61347c3128c2cafa75f0b2f
I am not me <impeccabletiming@gmail.com>
2013-05-02 19:27:20 -0400

    Adding Ramadan

--- catscripts/Lifestyle/Religion/Islam/Islam.sh
+++ catscripts/Lifestyle/Religion/Islam/Islam.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 egrep -i 'Islam|Muslim|\bSunni(|s)\b|Shiite|hajj|Prophet (Muhammed|Muhammad)' newpages.txt | egrep -iv 'Ramadan|Mecca|Medina' >> Islam.txt
+egrep -i 'Ramadan' newpages.txt >> Ramadan.txt
 
 ISLAM=`stat --print=%s Islam.txt`
+RAMADAN=`stat --print=%s Ramadan.txt`
 
 if [ $ISLAM -ne 0 ];
 then
@@ -11,4 +13,12 @@ then
   $CATEGORIZE
 fi
 
+if [ $RAMADAN -ne 0 ];
+then
+  export CATFILE="Ramadan.txt"
+  export CATNAME="Ramadan"
+  $CATEGORIZE
+fi
+
 rm Islam.txt
+rm Ramadan.txt
