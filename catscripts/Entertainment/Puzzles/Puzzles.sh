fad00f0f2d021cab27d1deaeb1e2961e58e3db71
I am not me <impeccabletiming@gmail.com>
2013-05-02 16:56:43 -0400

    Moving Crossword Puzzles to Crossword puzzles

--- catscripts/Entertainment/Puzzles/Puzzles.sh
+++ catscripts/Entertainment/Puzzles/Puzzles.sh
@@ -2,7 +2,8 @@
 
 egrep -i 'Sudoku' newpages.txt >> Sudoku.txt
 egrep -i 'Crossword' newpages.txt >> Crossword.txt
-egrep -i '[0-9]{1,}(|-)letter word for' newpages.txt >> Crossword.txt
+egrep -i '[0-9]{1,}( |-)letter word for' newpages.txt >> Crossword.txt
+egrep -i '(three|four|five|six|seven|eight|nine|ten|eleven|twelve|thirteen|fourteen|fifteen|sixteen|seventeen|eighteen|nineteen|twenty)( |-)letter word for' newpages.txt >> Crossword.txt
 
 SUDOKU=`stat --print=%s Sudoku.txt`
 CROSSWORD=`stat --print=%s Crossword.txt`
@@ -17,7 +18,7 @@ fi
 if [ $CROSSWORD -ne 0 ];
 then
   export CATFILE="Crossword.txt"
-  export CATNAME="Crossword Puzzles"
+  export CATNAME="Crossword puzzles"
   $CATEGORIZE
 fi
 
