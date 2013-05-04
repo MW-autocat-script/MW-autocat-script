db5a998e96a691d2af50d3a50e60e7ed1fbf3e9e
I am not me <impeccabletiming@gmail.com>
2013-05-04 19:37:24 -0400

    Adding Terry Fox

--- catscripts/Lifestyle/Health/Diseases/Cancer/Cancer.sh
+++ catscripts/Lifestyle/Health/Diseases/Cancer/Cancer.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-egrep -i 'Cancer' newpages.txt | egrep -iv 'Lung(| )cancer|Breast(| )cancer|leukemia|Tropic of Cancer|Cancer the Crab|Zodiac|Horoscope' > Cancer.txt
+egrep -i 'Cancer' newpages.txt | egrep -iv 'Lung(| )cancer|Breast(| )cancer|leukemia|Tropic of Cancer|Cancer the Crab|Zodiac|Horoscope|Terry Fox' > Cancer.txt
+egrep -i 'Terry Fox' newpages.txt >> TerryFox.txt
 
 CANCER=`stat --print=%s Cancer.txt`
+TERRYFOX=`stat --print=%s TerryFox.txt`
 
 if [ $CANCER -ne 0 ];
 then
@@ -11,8 +13,16 @@ then
   $CATEGORIZE
 fi
 
+if [ $TERRYFOX -ne 0 ];
+then
+  export CATFILE="TerryFox.txt"
+  export CATNAME="Terry Fox"
+  $CATEGORIZE
+fi
+
 ./catscripts/Lifestyle/Health/Diseases/Cancer/Lung_cancer/Lungcancer.sh
 ./catscripts/Lifestyle/Health/Diseases/Cancer/Breast_cancer/Breastcancer.sh
 ./catscripts/Lifestyle/Health/Diseases/Cancer/Leukemia/Leukemia.sh
 
 rm Cancer.txt
+rm TerryFox.txt
