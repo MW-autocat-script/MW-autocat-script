c12f003067f365faf432c9bc34ac3efe69625d91
I am not me <impeccabletiming@gmail.com>
2013-05-03 21:22:39 -0400

    Adding Pittsburgh

--- catscripts/Government/Countries/United_States/US_states/Pennsylvania/Pennsylvania.sh
+++ catscripts/Government/Countries/United_States/US_states/Pennsylvania/Pennsylvania.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-egrep -i 'Pennsylvania|Pennsylvannia|Pennnsylvania|Pensylvania|Pensylvannia' newpages.txt >> Pennsylvania.txt
+egrep -i 'Pennsylvania|Pennsylvannia|Pennnsylvania|Pensylvania|Pensylvannia' newpages.txt | egrep -iv 'Pittsburgh' >> Pennsylvania.txt
+egrep -i 'Pittsburgh' newpages.txt | egrep -iv "Steelers" >> Pittsburgh.txt  
 
 PENNSYLVANIA=`stat --print=%s Pennsylvania.txt`
+PITTSBURGH=`stat --print=%s Pittsburgh.txt`
 
 if [ $PENNSYLVANIA -ne 0 ];
 then
@@ -11,4 +13,12 @@ then
   $CATEGORIZE
 fi
 
+if [ $PITTSBURGH -ne 0 ];
+then
+  export CATFILE="Pittsburgh.txt"
+  export CATNAME="Pittsburgh"
+  $CATEGORIZE
+fi
+
+rm Pittsburgh.txt
 rm Pennsylvania.txt
