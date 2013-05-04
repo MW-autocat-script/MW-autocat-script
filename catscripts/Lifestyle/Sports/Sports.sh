bdf2bca43d570ac71d10290486930463f5dc9e2f
I am not me <impeccabletiming@gmail.com>
2013-05-03 22:25:04 -0400

    Adding Hockey and NHL

--- catscripts/Lifestyle/Sports/Sports.sh
+++ catscripts/Lifestyle/Sports/Sports.sh
@@ -6,6 +6,39 @@ egrep -i 'ping(|-| )pong' newpages.txt >> PingPong.txt
 egrep -i 'Badminton' newpages.txt >> Badminton.txt
 egrep -i 'Volleyball' newpages.txt >> Volleyball.txt
 egrep -i 'softball' newpages.txt >> Softball.txt
+egrep -i 'hockey' newpages.txt | egrep -iv 'field hockey|street hockey|indoor hockey|NHL|Stanley Cup|National Hockey League' >> Hockey.txt
+egrep -i 'NHL|National Hockey League|Stanley Cup' newpages.txt >> NHL.txt
+egrep -i 'Anaheim Duck' newpages.txt >> NHL.txt
+egrep -i 'Boston Bruin' newpages.txt >> NHL.txt
+egrep -i 'Buffalo Sabre' newpages.txt >> NHL.txt
+egrep -i 'Calgary Flame' newpages.txt >> NHL.txt
+egrep -i 'Carolina Hurricane' newpages.txt >> NHL.txt
+egrep -i 'Chicago Blackhawk' newpages.txt >> NHL.txt
+egrep -i 'Colorado Avalanche' newpages.txt >> NHL.txt
+egrep -i 'Columbus Blue Jacket' newpages.txt >> NHL.txt
+egrep -i 'Dallas Star' newpages.txt >> NHL.txt
+egrep -i 'Detroit Red Wing' newpages.txt >> NHL.txt
+egrep -i 'Edmonton Oiler' newpages.txt >> NHL.txt
+egrep -i 'Florida Panther' newpages.txt >> NHL.txt
+egrep -i 'Los Angeles King' newpages.txt >> NHL.txt
+egrep -i 'Minnesota Wild' newpages.txt >> NHL.txt
+egrep -i 'Montreal Canadien' newpages.txt >> NHL.txt
+egrep -i 'Nashville Predator' newpages.txt >> NHL.txt
+egrep -i 'New Jersey Devil' newpages.txt >> NHL.txt
+egrep -i 'New York Islander' newpages.txt >> NHL.txt
+egrep -i 'New York Ranger' newpages.txt >> NHL.txt
+egrep -i 'Philadelphia Flyer' newpages.txt >> NHL.txt
+egrep -i 'Phoenix Coyote' newpages.txt >> NHL.txt
+egrep -i 'Pittsburgh Penguin' newpages.txt >> NHL.txt
+egrep -i 'Ottawa Senator' newpages.txt >> NHL.txt
+egrep -i 'San Jose Shark' newpages.txt >> NHL.txt
+egrep -i 'St Louis Blue' newpages.txt >> NHL.txt
+egrep -i 'Tampa Bay Lightning' newpages.txt >> NHL.txt
+egrep -i 'Toronto Maple Leaf' newpages.txt >> NHL.txt
+egrep -i 'Vancouver Canuck' newpages.txt >> NHL.txt
+egrep -i 'Washington Capital' newpages.txt >> NHL.txt
+egrep -i 'Winnipeg Jet' newpages.txt >> NHL.txt
+
 
 SOCCER=`stat --print=%s Soccer.txt`
 TENNIS=`stat --print=%s Tennis.txt`
@@ -13,6 +46,8 @@ PINGPONG=`stat --print=%s PingPong.txt`
 BADMINTON=`stat --print=%s Badminton.txt`
 VOLLEYBALL=`stat --print=%s Volleyball.txt`
 SOFTBALL=`stat --print=%s Softball.txt`
+HOCKEY=`stat --print=%s Hockey.txt`
+NHL=`stat --print=%s NHL.txt`
 
 if [ $SOCCER -ne 0 ];
 then
@@ -56,9 +91,25 @@ then
   $CATEGORIZE
 fi
 
+if [ $HOCKEY -ne 0 ];
+then
+  export CATFILE="Hockey.txt"
+  export CATNAME="Hockey"
+  $CATEGORIZE
+fi
+
+if [ $NHL -ne 0 ];
+then
+  export CATFILE="NHL.txt"
+  export CATNAME="NHL"
+  $CATEGORIZE
+fi
+
 rm Soccer.txt
 rm Tennis.txt
 rm PingPong.txt
 rm Badminton.txt
 rm Volleyball.txt
 rm Softball.txt
+rm Hockey.txt
+rm NHL.txt
