3011490d56845b4a97c4127d9b989b132a250fd3
I am not me <impeccabletiming@gmail.com>
2013-05-11 15:55:07 -0400

    Adding Venice

--- catscripts/Government/Countries/Italy/Italy.sh
+++ catscripts/Government/Countries/Italy/Italy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-egrep -i 'Italy' newpages.txt | egrep -iv 'Rome|Pompeii|Pisa' >> Italy.txt 
+egrep -i 'Italy' newpages.txt | egrep -iv 'Rome|Pompeii|Pisa|Venice' >> Italy.txt 
 egrep -i '\bRome(|s)\b' newpages.txt | egrep -iv 'Ancient Rome' >> Rome.txt
 egrep -i 'Ancient Rome|Romans|Roman times|Augustus Caesar|Emperor Nero|Caligula' newpages.txt | egrep -iv 'Book of Romans|Romans [0-9]{1,}\:[0-9]{1,}|Julius Caesar' >> AncientRome.txt #Exclude biblical book and Julius Caesar
 egrep -i 'Julius Caesar' newpages.txt >> JuliusCaesar.txt
 egrep -i 'Pisa' newpages.txt >> Pisa.txt
 egrep -i 'Pompeii' newpages.txt >> Pompeii.txt
+egrep -i 'Venice' newpages.txt >> Venice.txt
 
 ITALY=`stat --print=%s Italy.txt`
 ROME=`stat --print=%s Rome.txt`
@@ -13,6 +14,7 @@ ANCIENTROME=`stat --print=%s AncientRome.txt`
 PISA=`stat --print=%s Pisa.txt`
 POMPEII=`stat --print=%s Pompeii.txt`
 JCAESAR=`stat --print=%s JuliusCaesar.txt`
+VENICE=`stat --print=%s Venice.txt`
 
 if [ $ITALY -ne 0 ];
 then
@@ -56,9 +58,17 @@ then
   $CATEGORIZE
 fi
 
+if [ $VENICE -ne 0 ];
+then
+  export CATFILE="Venice.txt"
+  export CATNAME="Venice"
+  $CATEGORIZE
+fi
+
 rm Italy.txt
 rm Rome.txt
 rm AncientRome.txt
 rm Pisa.txt
 rm Pompeii.txt
 rm JuliusCaesar.txt
+rm Venice.txt
