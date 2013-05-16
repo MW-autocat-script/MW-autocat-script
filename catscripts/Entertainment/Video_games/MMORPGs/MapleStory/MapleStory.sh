f4135728a8184a97b411caa39943cafa8cd02406
I am not me <impeccabletiming@gmail.com>
2013-05-16 04:00:22 -0400

    Adding MapleStory

---
+++ catscripts/Entertainment/Video_games/MMORPGs/MapleStory/MapleStory.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export KEYWORDS_MMORPG_MAPLESTORY="Maple(| )Story"
+
+egrep -i "$KEYWORDS_MMORPG_MAPLESTORY" newpages.txt > MapleStory.txt
+
+MAPLESTORY=`stat --print=%s MapleStory.txt`
+
+if [ $MAPLESTORY -ne 0 ];
+then
+  export CATFILE="MapleStory.txt"
+  export CATNAME="MapleStory"
+  $CATEGORIZE
+fi
+
+rm MapleStory.txt
