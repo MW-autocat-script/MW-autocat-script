53473d61946fbe76238fdbc8e575fce4f990ff82
I am not me <impeccabletiming@gmail.com>
2013-05-16 03:49:08 -0400

    Adding Runes of Magic

---
+++ catscripts/Entertainment/Video_games/MMORPGs/Runes_of_Magic/RunesofMagic.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export KEYWORDS_MMORPG_RUNESOFMAGIC="Runes(| )of(| )Magic"
+
+egrep -i "$KEYWORDS_MMORPG_RUNESOFMAGIC" newpages.txt >> RunesofMagic.txt
+
+RUNESOFMAGIC=`stat --print=%s RunesofMagic.txt`
+
+if [ $RUNESOFMAGIC -ne 0 ];
+then
+  export CATFILE="RunesofMagic.txt"
+  export CATNAME="Runes of Magic"
+  $CATEGORIZE
+fi
+
+rm RunesofMagic.txt
