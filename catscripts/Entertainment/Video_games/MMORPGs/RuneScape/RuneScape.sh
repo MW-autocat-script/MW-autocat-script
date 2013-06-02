21fb212686914b8f3e33d951c8906793bb1eccc1
I am not me <impeccabletiming@gmail.com>
2013-06-02 10:01:49 -0400

    Minor fix in RuneScape (bracket instead of parantheses)

--- catscripts/Entertainment/Video_games/MMORPGs/RuneScape/RuneScape.sh
+++ catscripts/Entertainment/Video_games/MMORPGs/RuneScape/RuneScape.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export KEYWORDS_MMORPG_RUNESCAPE="RuneScape|Rune scape|rune longsword|rune kite|Lumbridge|Falador|Karamja|Daemonheim|Varrock|Edgeville|Elvarg|strength potion|(Zamorak|Armadyl|Zaros|Saradomin|Guthix|Bandos)|(Zamorak|Armadyl|Zaros|Saradomin|Guthix|Bandos).+godsword|Third Age Arm(or|our)|(abyssal|abby) whip|(addy|rune|dragon) kite|(rune|dragon) (scimitar|scimmy)|zammy|full sara|(obsidian|obby) maul|(king|queen) black dragon|corporeal beast|cockroach (soldier|worker|drone)|strength pure|God Wars Dungeon|ectofuntus|abyssal demon|Shilo Village|Soul Wars|(air|water|earth|fire|Mind|Body|Chaos|Law|Blood|Soul|Cosmic|Death|Nature)(| ){rune|talisman|altar)"
+export KEYWORDS_MMORPG_RUNESCAPE="RuneScape|Rune scape|rune longsword|rune kite|Lumbridge|Falador|Karamja|Daemonheim|Varrock|Edgeville|Elvarg|strength potion|(Zamorak|Armadyl|Zaros|Saradomin|Guthix|Bandos)|(Zamorak|Armadyl|Zaros|Saradomin|Guthix|Bandos).+godsword|Third Age Arm(or|our)|(abyssal|abby) whip|(addy|rune|dragon) kite|(rune|dragon) (scimitar|scimmy)|zammy|full sara|(obsidian|obby) maul|(king|queen) black dragon|corporeal beast|cockroach (soldier|worker|drone)|strength pure|God Wars Dungeon|ectofuntus|abyssal demon|Shilo Village|Soul Wars|(air|water|earth|fire|Mind|Body|Chaos|Law|Blood|Soul|Cosmic|Death|Nature)(| )(rune|talisman|altar)"
 
 egrep -i "$KEYWORDS_MMORPG_RUNESCAPE" newpages.txt >> RuneScape.txt
 
