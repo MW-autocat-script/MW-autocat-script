e850a97376aeae47788aa78b437209f06252e87c
I am not me <impeccabletiming@gmail.com>
2013-06-08 00:47:09 -0400

    Adding keywords to South Park

--- catscripts/Entertainment/Cartoons/South Park/SouthPark.sh
+++ catscripts/Entertainment/Cartoons/South Park/SouthPark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Categorization script for the adult cartoon South Park
-egrep -i 'south park|southpark|kenny mccormick|cartman|kyle broflovski|stan marsh|butters stotch|(Herbet|Mr(|\.))(| )Garrison|choksondik|Mr(|\.)(| )Mackey' newpages.txt >> SouthPark.txt
+egrep -i 'South(| )Park|Kenny(| )McCormick|Cartman|(Kyle|Gerald|Sheila|Ike)(| )Broflovski|(Randy|Stan)(| )Marsh|Butters(| )Stotch|(Herbert|Mr(|\.))(| )Garrison|choksondik|Mr(|\.)(| )Mackey|Matt(| )Stone|Trey(| )Parker|Matt.+\bTrey|\bTrey.+Matt|Wendy(| )Testaburger|Jesus and Pals|Make Love(|,)(| )Not Warcraft|Good Times With Weapons|Mysterion|The Coon\b|Karen(| )McCormick|Clyde(| )Donovan|Clyde(| )Frog|(Scott|Jack|Mr\. and Mrs\.)(| )Tenorman' newpages.txt >> SouthPark.txt
 
 SOUTHPARK=`stat --print=%s SouthPark.txt`
 
