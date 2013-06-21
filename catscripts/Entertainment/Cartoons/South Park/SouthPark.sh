7b87108e62ed8529993360151397faedd69c59ea
I am not me <impeccabletiming@gmail.com>
2013-06-21 00:14:09 -0400

    Adding keywords to South Park

--- catscripts/Entertainment/Cartoons/South Park/SouthPark.sh
+++ catscripts/Entertainment/Cartoons/South Park/SouthPark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Categorization script for the adult cartoon South Park
 
-SOUTHPARK_CHARACTERS="(Kenny|Karen|Kevin)(| )McCormick|Cartman|(Kyle|Ike|Sheila|Gerald)(| )Broflovski|(Randy|Stan|Sharon|Shelly)(| )Marsh|(Butters|Leopold)(| )Stotch|((M(r|s|rs)(|\.)|Herbert))(| )Garrison|Mr(|\.)(| )Mackey|Wendy(| )Testaburger|Choksondick|Clyde(| )Donovan|(Scott|Jack|Mr\. and Mrs\.)(| )Tenorman|Clyde(| )Frog|Craig(| )Tucker|Jimmy(| )Valmer|Timmy(| )Burch|Token(| )Black|Mr(|\.)(| )Hat|Mayor McDaniels|Tweek(| )Tweak|(Phillip|Pip)(| )Pirrup|Towelie|Mr(|\.)(| )Hanky"
+SOUTHPARK_CHARACTERS="(Kenny|Karen|Kevin)(| )McCormick|Cartman|(Kyle|Ike|Sheila|Gerald)(| )Broflovski|(Randy|Stan|Sharon|Shelly)(| )Marsh|(Butters|Leopold)(| )Stotch|((M(r|s|rs)(|\.)|Herbert))(| )Garrison|Mr(|\.)(| )Mackey|Wendy(| )Testaburger|Choksondick|Clyde(| )Donovan|(Scott|Jack|Mr\. and Mrs\.)(| )Tenorman|Clyde(| )Frog|Craig(| )Tucker|Jimmy(| )Valmer|Timmy(| )Burch|Token(| )Black|Mr(|\.)(| )Hat|Mayor McDaniels|Tweek(| )Tweak|(Phillip|Pip)(| )Pirrup|Towelie|Mr(|\.)(| )Hanky|Starvin(|')(| )Marvin|Dr(|\.)(| )Janus|Terrance and Phillip|Captain Hindsight|The Human Kite|Professor(| )Chaos"
 SOUTHPARK_MUSIC="Let(|'s)(| )Fighting(| )Love|Eyes of a Child"
 SOUTHPARK_EPISODES="Make(| )Love(|,)(| )Not(| )Warcraft|Good(| )Times(| )With Weapons|The Coon\b|Imaginationland|Insheeption|Ass Burgers|Kenny Dies|Mecha-Streisand|Trapped in the Closet|Lice(| )Capades|You Have 0 Friends"
 
