e2fd28b13487b323cff533ff0c8ec87b01dca890
I am not me <impeccabletiming@gmail.com>
2013-06-24 01:22:19 -0400

    Adding Sexuality; adding keyword to Fractions

--- catscripts/Lifestyle/Lifestyle.sh
+++ catscripts/Lifestyle/Lifestyle.sh
@@ -5,3 +5,4 @@ CURRENTDIR="./catscripts/Lifestyle"
 $CURRENTDIR/Health/Health.sh
 $CURRENTDIR/Religion/Religion.sh
 $CURRENTDIR/Sports/Sports.sh
+$CURRENTDIR/Sexuality/Sexuality.sh
---
+++ catscripts/Lifestyle/Sexuality/Sexuality.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+KEYWORDS_SEXUALITY="sexuality|sexual orientation|\bgay(|s)\b|lesbian|bi(|-)sexual|trans(-|)gender|pansexual|\bBDSM\b|fellatio|cunnilingus|anal(| )sex|vaginal(| )sex|omnisexual|sado-masochis(m|t)"
+KEYWORDS_MASTURBATION="Masturbat(e|ion)"
+
+egrep -i "$KEYWORDS_SEXUALITY" newpages.txt | egrep -iv "$KEYWORDS_MASTURBATION" >> Sexuality.txt
+egrep -i "$KEYWORDS_MASTURBATION" newpages.txt >> Masturbation.txt
+
+SEXUALITY=`stat --print=%s Sexuality.txt`
+MASTURBATION=`stat --print=%s Masturbation.txt`
+
+if [ $SEXUALITY -ne 0 ];
+then
+  export CATFILE="Sexuality.txt"
+  export CATNAME="Sexuality"
+  $CATEGORIZE
+fi
+
+if [ $MASTURBATION -ne 0 ];
+then
+  export CATFILE="Masturbation.txt"
+  export CATNAME="Masturbation"
+  $CATEGORIZE
+fi
+
+rm Sexuality.txt
+rm Masturbation.txt
