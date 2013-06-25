69cbcc03dc0647917400a0a486097e9ac9ec921d
I am not me <impeccabletiming@gmail.com>
2013-06-25 11:59:49 -0400

    Add keywords to Computer hardware

--- catscripts/Technology/Computers/Computer_hardware/Computerhardware.sh
+++ catscripts/Technology/Computers/Computer_hardware/Computerhardware.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-egrep -i 'Motherboard' newpages.txt >> Motherboards.txt
-egrep -i 'Video card|videocard|graphics card|graphicscard|\bGPU\b|GeForce|Riva TNT|(ATI|AMD) Radeon' newpages.txt >> VideoCards.txt
-egrep -i 'Sound card|Soundcard|audio card|audiocard|audio device|sound.+computer|computer.+sound' newpages.txt >> SoundCards.txt
-egrep -i '\bPrinter' newpages.txt >> Printers.txt
-egrep -i 'Microprocessor|\bPentium|\bi[357]\b|\bAthlon|Celeron|Sempron|\bTurion|\b808[568]\b|80286|80386|80586|CPU|Motorola 680(00|10|20|30|40|50|60)|Motorola 88000|M68k|Intel (4004|8008|8086|8088)|(8085|8086|8088) (assembly|program)|Socket A\b|Socket 5|Socket 7|Socket 370|PGA370|Socket 478|Slot 1\b|Slot 2\b|Socket 423|LGA(| )775|LGA(| )1366|LGA(| )1156|LGA(| )1155|LGA(| )2011|LGA(| )1150|Socket 441|Socket 479|Socket 495|Socket M\b|Socket P\b|Socket G1|Socket G2|Socket G3|Socket 8\b|Socket 603|Socket 604|PowerPC' newpages.txt >> Microprocessors.txt
-egrep -i 'Hard (disk|drive)' newpages.txt >> Harddrive.txt
-egrep -i 'Device driver|Computer hardware' newpages.txt | egrep -iv '(Video|Graphics) card|Motherboard|(sound|audio) card|\bPrinter|hard (disk|drive)' >> ComputerHardware.txt
+KEYWORDS_MOTHERBOARD="Motherboard"
+KEYWORDS_VIDEO_CARDS="Video card|videocard|graphics card|graphicscard|\bGPU\b|GeForce|Riva TNT|(ATI|AMD) Radeon"
+KEYWORDS_SOUND_CARDS="Sound card|Soundcard|audio card|audiocard|audio device|sound.+computer|computer.+sound"
+KEYWORDS_PRINTER="\bPrinter"
+KEYWORDS_MICROPROCESSOR="Microprocessor|\bPentium|\bi[357]\b|\bAthlon|Celeron|Sempron|\bTurion|\b808[568]\b|80286|80386|80586|CPU|Motorola 680(00|10|20|30|40|50|60)|Motorola 88000|M68k|Intel (4004|8008|8086|8088)|(8085|8086|8088) (assembly|program)|Socket A\b|Socket 5|Socket 7|Socket 370|PGA370|Socket 478|Slot 1\b|Slot 2\b|Socket 423|LGA(| )775|LGA(| )1366|LGA(| )1156|LGA(| )1155|LGA(| )2011|LGA(| )1150|Socket 441|Socket 479|Socket 495|Socket M\b|Socket P\b|Socket G1|Socket G2|Socket G3|Socket 8\b|Socket 603|Socket 604|PowerPC"
+KEYWORDS_HARD_DRIVE="Hard (disk|drive)"
+KEYWORDS_COMPUTER_HARDWARE_MAIN="Device driver|Computer hardware|adapter card" #Stuff to put in the parent category
+KEYWORDS_COMPUTER_HARDWARE="$KEYWORDS_HARD_DRIVE|$KEYWORDS_MICROPROCESSOR|$KEYWORDS_MOTHERBOARD|$KEYWORDS_PRINTER|$KEYWORDS_SOUND_CARDS|$KEYWORDS_VIDEO_CARDS"
+
+egrep -i "$KEYWORDS_MOTHERBOARD" newpages.txt >> Motherboards.txt
+egrep -i "$KEYWORDS_VIDEO_CARDS" newpages.txt >> VideoCards.txt
+egrep -i "$KEYWORDS_SOUND_CARDS" newpages.txt >> SoundCards.txt
+egrep -i "$KEYWORDS_PRINTER" newpages.txt >> Printers.txt
+egrep -i "$KEYWORDS_MICROPROCESSOR" newpages.txt >> Microprocessors.txt
+egrep -i "$KEYWORDS_HARD_DRIVE" newpages.txt >> Harddrive.txt
+egrep -i "$KEYWORDS_COMPUTER_HARDWARE_MAIN" newpages.txt | egrep -iv "$KEYWORDS_COMPUTER_HARDWARE_MAIN" >> ComputerHardware.txt
 
 Motherboards=`stat --print=%s Motherboards.txt`
 VideoCards=`stat --print=%s VideoCards.txt`
