f3374fe9f9ff5fa2edb76fe8ebefd7a9c912b6c5
I am not me <impeccabletiming@gmail.com>
2013-06-26 19:36:23 -0400

    Adding PlayStation 4

--- catscripts/Entertainment/Video_games/Video_game_consoles/Consoles.sh
+++ catscripts/Entertainment/Video_games/Video_game_consoles/Consoles.sh
@@ -5,4 +5,5 @@ CURRENTDIR="./catscripts/Entertainment/Video_games/Video_game_consoles"
 . $CURRENTDIR/PlayStation.sh
 . $CURRENTDIR/PlayStation2.sh
 . $CURRENTDIR/PlayStation3.sh
+. $CURRENTDIR/PlayStation4.sh
 . $CURRENTDIR/Xbox360.sh 
---
+++ catscripts/Entertainment/Video_games/Video_game_consoles/PlayStation4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+KEYWORDS_PS4="\bPS(| )(4|four)|Play(| )Station(| )(4|four)"
+
+egrep -i "$KEYWORDS_PS4" newpages.txt >> PlayStation4.txt
+
+PS4=`stat --print=%s PlayStation4.txt`
+
+if [ $PS$ -ne 0 ];
+then
+  export CATFILE="PlayStation4.txt"
+  export CATNAME="PlayStation 4"
+  $CATEGORIZE
+fi
+
+rm PlayStation4.txt
