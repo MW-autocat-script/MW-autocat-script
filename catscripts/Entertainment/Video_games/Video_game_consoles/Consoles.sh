9a090130d30765d626c1282d841cf64e3ff881fa
I am not me <impeccabletiming@gmail.com>
2013-06-27 12:26:14 -0400

    Adding PlayStation Network

--- catscripts/Entertainment/Video_games/Video_game_consoles/Consoles.sh
+++ catscripts/Entertainment/Video_games/Video_game_consoles/Consoles.sh
@@ -6,6 +6,7 @@ CURRENTDIR="./catscripts/Entertainment/Video_games/Video_game_consoles"
 . $CURRENTDIR/Sony/PlayStation2.sh
 . $CURRENTDIR/Sony/PlayStation3.sh
 . $CURRENTDIR/Sony/PlayStation4.sh
+. $CURRENTDIR/Sony/PlayStationNetwork.sh # Not a game console, but it's easier to categorize it here
 . $CURRENTDIR/Sony/PlayStationPortable.sh
 
 
---
+++ catscripts/Entertainment/Video_games/Video_game_consoles/Sony/PlayStationNetwork.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+KEYWORDS_PLAYSTATION_NETWORK="Play(| )Station(| )(Network|Store)|\bPSN\b"
+
+egrep -i "$KEYWORDS_PLAYSTATION_NETWORK" newpages.txt >> PlayStationNetwork.txt
+
+PSN=`stat --print=%s PlayStationNetwork.txt`
+
+if [ $PSN -ne 0 ];
+then
+  export CATFILE="PlayStationNetwork.txt"
+  export CATNAME="PlayStation Network"
+  $CATEGORIZE
+fi
+
+rm PlayStationNetwork.txt
