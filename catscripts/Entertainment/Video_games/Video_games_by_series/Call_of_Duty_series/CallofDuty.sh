69f108fddd1606508c90f0a72c46a11c875e8f03
I am not me <impeccabletiming@gmail.com>
2013-06-27 10:07:35 -0400

    Adding Call of Duty: Ghosts

--- catscripts/Entertainment/Video_games/Video_games_by_series/Call_of_Duty_series/CallofDuty.sh
+++ catscripts/Entertainment/Video_games/Video_games_by_series/Call_of_Duty_series/CallofDuty.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-egrep -i '(Call of Duty|CoD) series' newpages.txt >> CallofDuty.txt
-egrep -i 'Modern(| )Warfare|\bCOD(| )4|Call of Duty 4' newpages.txt | egrep -iv 'Modern(| )Warfare(| )(two|three|[2-9]{1,})' >> COD4.txt
-egrep -i '\bCOD(| )5|\bWaW\b|World at War|Call of Duty 5' newpages.txt >> WorldAtWar.txt
-egrep -i '\bCOD(| )6|\bMW2\b|Modern Warfare 2|Call of Duty 6' newpages.txt >> ModernWarfare2.txt
-egrep -i '\bCOD(| )7|Black Ops|Call of Duty 7' newpages.txt | egrep -iv 'Black Ops ([2-9]{1,}|\bII\b|\bIII\b|\bIV\b|\bV\b)' >> BlackOps.txt
-egrep -i '\bCOD(| )8|\bMW3\b|Modern Warfare 3|Call of Duty 8' newpages.txt >> ModernWarfare3.txt
-egrep -i '\bCOD(| )9|Black Ops (2|II\b)|Call of Duty 9' newpages.txt >> BlackOps2.txt
-egrep -i 'Call of Duty' newpages.txt | egrep -iv 'World At War|WaW|Black Ops|Modern Warfare|Call of Duty [2-9]{1,}' >> CallofDuty.txt
-egrep 'CoD' newpages.txt | egrep -v 'CoD(| )[2-9]' >> CallofDuty.txt
+KEYWORDS_COD4="Modern(| )Warfare|\bCOD(| )4|Call(| )of(| )Duty(| )4"
+KEYWORDS_COD4_EXCLUDE="Modern(| )Warfare(| )(two|three|[2-9]{1,})"
+KEYWORDS_CODWAW="\bCOD(| )5|\bWaW\b|World(| )at(| )War|Call(| )of(| )Duty(| )5"
+KEYWORDS_CODMW2="\bCOD(| )6|\bMW2\b|Modern(| )Warfare(| )2|Call(| )of(| )Duty(| )6"
+KEYWORDS_CODBO="\bCOD(| )7|Black Ops|Call of Duty 7"
+KEYWORDS_CODBO_EXCLUDE="Black(| )Ops(| )([2-9]{1,}|\bII\b|\bIII\b|\bIV\b|\bV\b)"
+KEYWORDS_CODMW3="\bCOD(| )8|\bMW3\b|Modern(| )Warfare(| )3|Call(| )of(| )Duty(| )8"
+KEYWORDS_CODBO2="\bCOD(| )9|Black Ops (2|II\b)|Call of Duty 9"
+KEYWORDS_CODGHOSTS="\bCOD(| )10|Call(| )Of(| )Duty(\:|)(| )Ghost"
+KEYWORDS_CALLOFDUTYSERIES="Call(| )of(| )Duty|CoD(| )series"
+KEYWORDS_CALLOFDUTYSERIES_EXCLUDE="$KEYWORDS_COD4|$KEYWORDS_CODWAW|$KEYWORDS_CODBO|$KEYWORDS_CODMW3|$KEYWORDS_CODBO2"
+KEYWORDS_CALLOFDUTYSERIES_CASESENSITIVE="CoD"
+KEYWORDS_CALLOFDUTYSERIES_CASESENSITIVE_EXCLUDE="CoD(| )[0-9]{1,}" 
+
+egrep -i "$KEYWORDS_COD4" newpages.txt | egrep -iv "$KEYWORDS_COD4_EXCLUDE" >> COD4.txt
+egrep -i "$KEYWORDS_CODWAW" newpages.txt >> WorldAtWar.txt
+egrep -i "$KEYWORDS_CODMW2" newpages.txt >> ModernWarfare2.txt
+egrep -i "$KEYWORDS_CODBO" newpages.txt | egrep -iv "$KEYWORDS_CODBO_EXCLUDE" >> BlackOps.txt
+egrep -i "$KEYWORDS_CODMW3" newpages.txt >> ModernWarfare3.txt
+egrep -i "$KEYWORDS_CODBO2" newpages.txt >> BlackOps2.txt
+egrep -i "$KEYWORDS_CODGHOSTS" newpages.txt >> Ghosts.txt
+egrep -i "$KEYWORDS_CALLOFDUTYSERIES" newpages.txt | egrep -iv "$KEYWORDS_CALLOFDUTYSERIES_EXCLUDE" >> CallofDuty.txt
+egrep "$KEYWORDS_CALLOFDUTYSERIES_CASESENSITIVE" newpages.txt | egrep -v "$KEYWORDS_CALLOFDUTYSERIES_CASESENSITIVE_EXCLUDE" >> CallofDuty.txt
 
 CALLOFDUTY=`stat --print=%s CallofDuty.txt`
 MODERN=`stat --print=%s COD4.txt`
@@ -17,6 +31,7 @@ MW2=`stat --print=%s ModernWarfare2.txt`
 BLACKOPS=`stat --print=%s BlackOps.txt`
 MW3=`stat --print=%s ModernWarfare3.txt`
 BO2=`stat --print=%s BlackOps2.txt`
+GHOSTS=`stat --print=%s Ghosts.txt`
 
 if [ $CALLOFDUTY -ne 0 ];
 then
@@ -67,6 +82,13 @@ then
   $CATEGORIZE
 fi
 
+if [ $GHOSTS -ne 0 ];
+then
+  export CATFILE="Ghosts.txt"
+  export CATNAME="Call of Duty: Ghosts"
+  $CATEGORIZE
+fi
+
 rm CallofDuty.txt
 rm COD4.txt
 rm WorldAtWar.txt
@@ -74,3 +96,4 @@ rm ModernWarfare2.txt
 rm ModernWarfare3.txt
 rm BlackOps.txt
 rm BlackOps2.txt
+rm Ghosts.txt
