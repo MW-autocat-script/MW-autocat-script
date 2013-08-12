845e0825da9c854b9971f0d8884d8bf4beb1bd53
I am not me <impeccabletiming@gmail.com>
2013-08-12 18:34:52 -0400

    Adding MS-DOS

--- catscripts/Technology/Computers/Software/Operating_systems/OperatingSystems.sh
+++ catscripts/Technology/Computers/Software/Operating_systems/OperatingSystems.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-egrep -i 'operating system| OS\b|\[\[OS ' newpages.txt | egrep -iv 'Windows|Unix|Linux|Mac OS|Open(| )BSD|Net(| )BSD|Free(| )BSD|\bDOS\b|Google Android' >> OperatingSystems.txt
+egrep -i 'operating system|(^| )OS\b|\[\[OS ' newpages.txt | egrep -iv 'Windows|Unix|Linux|Mac OS|Open(| )BSD|Net(| )BSD|Free(| )BSD|\bDOS\b|Google Android|MS(|-| )DOS' >> OperatingSystems.txt
+
+KEYWORDS_MSDOS="MS(|-| )DOS"
+
+egrep -i "$KEYWORDS_MSDOS" newpages.txt >> MS-DOS.txt
 
 OS=`stat --print=%s OperatingSystems.txt`
+MSDOS=`stat --print=%s MS-DOS.txt`
 
 if [ $OS -ne 0 ];
 then
   export CATFILE="OperatingSystems.txt"
   export CATNAME="Operating systems"
+  $CATEGORIZE
+fi
+
+if [ $MSDOS -ne 0 ];
+then
+  export CATFILE="MS-DOS.txt"
+  export CATNAME="MS-DOS"
   $CATEGORIZE
 fi
 
@@ -21,3 +33,4 @@ $CURRENTDIR/Unix/Unix.sh
 $CURRENTDIR/Windows/Windows.sh
 
 rm OperatingSystems.txt
+rm MS-DOS.txt
