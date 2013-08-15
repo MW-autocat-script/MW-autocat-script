4d9a84dd54d2f04a123a9c15a558cc6904c433b6
I am not me <impeccabletiming@gmail.com>
2013-08-14 22:37:42 -0400

    Adding Club Penguin

---
+++ catscripts/Entertainment/Video_games/MMORPGs/Club_Penguin/ClubPenguin.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export KEYWORDS_MMORPG_CLUBPENGUIN="Club(| )Penguin"
+
+egrep -i "$KEYWORDS_MMORPG_CLUBPENGUIN" newpages.txt >> ClubPenguin.txt
+
+CLUBPENGUIN=`stat --print=%s ClubPenguin.txt`
+
+if [ $CLUBPENGUIN -ne 0 ];
+then
+  export CATFILE="ClubPenguin.txt"
+  export CATNAME="Club Penguin"
+  $CATEGORIZE
+fi
+
+rm ClubPenguin.txt
