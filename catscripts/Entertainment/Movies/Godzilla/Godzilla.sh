288bbb5dcad250c9c810e006c1938fc896a02ecb
I am not me <impeccabletiming@gmail.com>
2013-08-21 07:11:28 -0400

    Adding Godzilla

---
+++ catscripts/Entertainment/Movies/Godzilla/Godzilla.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export KEYWORDS_MOVIES_GODZILLA="Godzilla"
+
+egrep -i "$KEYWORDS_MOVIES_GODZILLA" newpages.txt >> Godzilla.txt
+
+GODZILLA=`stat --print=%s Godzilla.txt`
+
+if [ $GODZILLA -ne 0 ];
+then
+  export CATFILE="Godzilla.txt"
+  export CATNAME="Godzilla"
+  $CATEGORIZE
+fi
+
+rm Godzilla.txt
