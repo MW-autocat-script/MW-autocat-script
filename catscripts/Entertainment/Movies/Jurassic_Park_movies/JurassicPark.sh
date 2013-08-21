115116944f5b784e856ff2b54f106b25ad03575b
I am not me <impeccabletiming@gmail.com>
2013-08-21 03:43:16 -0400

    Adding Jurassic Park

---
+++ catscripts/Entertainment/Movies/Jurassic_Park_movies/JurassicPark.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export KEYWORDS_MOVIES_JURASSIC_PARK="Jur(|r)as(|s)ic(| )Park"
+
+egrep -i "$KEYWORDS_MOVIES_JURASSIC_PARK" newpages.txt >> JurassicPark.txt
+
+JURASSICPARK=`stat -print=%s JurassicPark.txt`
+
+if [ $JURASSICPARK -ne 0 ];
+then
+  export CATFILE="JurassicPark.txt"
+  export CATNAME="Jurassic Park"
+  $CATEGORIZE
+fi
+
+rm JurassicPark.txt
--- catscripts/Entertainment/Movies/Movies.sh
+++ catscripts/Entertainment/Movies/Movies.sh
@@ -4,6 +4,6 @@ CURRENTDIR="./catscripts/Entertainment/Movies"
 
 $CURRENTDIR/Harry_Potter/HarryPotter.sh
 $CURRENTDIR/The_Hunger_Games/HungerGames.sh
+$CURRENTDIR/Jurassic_Park_movies/JurassicPark.sh
 $CURRENTDIR/Star\ Wars\ movies/StarWarsMovies.sh
 $CURRENTDIR/Toy_Story_movies/ToyStory.sh
-
