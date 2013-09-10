6432e0915b086b2cb8195fe5543611ecc665e5b3
I am not me <impeccabletiming@gmail.com>
2013-09-10 06:54:09 -0400

    Adding Kolkata

--- catscripts/Government/Countries/India/India.sh
+++ catscripts/Government/Countries/India/India.sh
@@ -3,7 +3,8 @@
 KEYWORDS_INDIA="India\b"
 KEYWORDS_NEWDELHI="Delhi"
 KEYWORDS_MUMBAI="Mumbai|Bombay"
-KEYWORDS_INDIA_EXCLUDE="$KEYWORDS_NEWDELHI|$KEYWORDS_MUMBAI"
+KEYWORDS_KOLKATA="Kolkata|Calcutta"
+KEYWORDS_INDIA_EXCLUDE="$KEYWORDS_NEWDELHI|$KEYWORDS_MUMBAI|$KEYWORDS_KOLKATA"
 
 if [ "$1" == "" ];
 then
@@ -11,10 +12,12 @@ then
   egrep -i "$KEYWORDS_INDIA" newpages.txt | egrep -iv "$KEYWORDS_INDIA_EXCLUDE" > India.txt
   egrep -i "$KEYWORDS_NEWDELHI" newpages.txt > NewDelhi.txt
   egrep -i "$KEYWORDS_MUMBAI" newpages.txt > Mumbai.txt
+  egrep -i "$KEYWORDS_KOLKATA" newpages.txt > Kolkata.txt
 
   INDIA=`stat --print=%s India.txt`
   NEWDELHI=`stat --print=%s NewDelhi.txt`
   MUMBAI=`stat --print=%s Mumbai.txt`
+  KOLKATA=`stat --print=%s Kolkata.txt`
 
   if [ $INDIA -ne 0 ];
   then
@@ -37,7 +40,15 @@ then
     $CATEGORIZE
   fi
 
+  if [ $KOLKATA -ne 0 ];
+  then
+    export CATFILE="Kolkata.txt"
+    export CATNAME="Kolkata"
+    $CATEGORIZE
+  fi
+
   rm India.txt
   rm NewDelhi.txt
   rm Mumbai.txt
+  rm Kolkata.txt
 fi
