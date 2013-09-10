23388ec437bd5ea8bc187b034e58ee9f347be98a
I am not me <impeccabletiming@gmail.com>
2013-09-10 07:03:38 -0400

    Adding Chennai

--- catscripts/Government/Countries/India/India.sh
+++ catscripts/Government/Countries/India/India.sh
@@ -4,7 +4,8 @@ KEYWORDS_INDIA="India\b"
 KEYWORDS_NEWDELHI="Delhi"
 KEYWORDS_MUMBAI="Mumbai|Bombay"
 KEYWORDS_KOLKATA="Kolkata|Calcutta"
-KEYWORDS_INDIA_EXCLUDE="$KEYWORDS_NEWDELHI|$KEYWORDS_MUMBAI|$KEYWORDS_KOLKATA"
+KEYWORDS_CHENNAI="Chennai|Madras"
+KEYWORDS_INDIA_EXCLUDE="$KEYWORDS_NEWDELHI|$KEYWORDS_MUMBAI|$KEYWORDS_KOLKATA|$KEYWORDS_CHENNAI"
 
 if [ "$1" == "" ];
 then
@@ -13,11 +14,13 @@ then
   egrep -i "$KEYWORDS_NEWDELHI" newpages.txt > NewDelhi.txt
   egrep -i "$KEYWORDS_MUMBAI" newpages.txt > Mumbai.txt
   egrep -i "$KEYWORDS_KOLKATA" newpages.txt > Kolkata.txt
+  egrep -i "$KEYWORDS_CHENNAI" newpages.txt > Chennai.txt
 
   INDIA=`stat --print=%s India.txt`
   NEWDELHI=`stat --print=%s NewDelhi.txt`
   MUMBAI=`stat --print=%s Mumbai.txt`
   KOLKATA=`stat --print=%s Kolkata.txt`
+  CHENNAI=`stat --print=%s Chennai.txt`
 
   if [ $INDIA -ne 0 ];
   then
@@ -47,8 +50,16 @@ then
     $CATEGORIZE
   fi
 
+  if [ $CHENNAI -ne 0 ];
+  then
+    export CATFILE="Chennai.txt"
+    export CATNAME="Chennai"
+    $CATEGORIZE
+  fi
+
   rm India.txt
   rm NewDelhi.txt
   rm Mumbai.txt
   rm Kolkata.txt
+  rm Chennai.txt
 fi
