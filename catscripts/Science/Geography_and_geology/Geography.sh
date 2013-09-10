b9248fd6093da6441c66dfbde2c0c9214031071b
I am not me <impeccabletiming@gmail.com>
2013-09-10 03:13:20 -0400

    Merge Geology and Geography categories

---
+++ catscripts/Science/Geography_and_geology/Geography.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+KEYWORDS_GEOGRAPHY="Geography"
+KEYWORDS_GEOLOGY="seismograph|seismology|geology|geological"
+KEYWORDS_VOLCANO="Volcano"
+KEYWORDS_VOLCANO_EXCLUDE="RuneScape"
+KEYWORDS_EARTHQUAKES="Earth(| )quake"
+KEYWORDS_GEOLOGY_EXCLUDE="$KEYWORDS_EARTHQUAKES|$KEYWORDS_VOLCANO"
+
+
+if [ "$1"=="" ];
+then
+  egrep -i "$KEYWORDS_VOLCANO" newpages.txt | egrep -iv "$KEYWORDS_VOLCANO_EXCLUDE" > Volcanoes.txt
+  egrep -i "$KEYWORDS_EARTHQUAKES" newpages.txt > Earthquakes.txt
+  egrep -i "$KEYWORDS_GEOLOGY|$KEYWORDS_GEOGRAPHY" newpages.txt | egrep -iv "$KEYWORDS_GEOLOGY_EXCLUDE" > Geology.txt
+
+  GEOLOGY=`stat --print=%s Geology.txt`
+  VOLCANOES=`stat --print=%s Volcanoes.txt`
+  EARTHQUAKES=`stat --print=%s Earthquakes.txt`
+
+  if [ $GEOLOGY -ne 0 ];
+  then
+    export CATFILE="Geology.txt"
+    export CATNAME="Geography and geology"
+    $CATEGORIZE
+  fi
+
+  if [ $EARTHQUAKES -ne 0 ];
+  then
+    export CATFILE="Earthquakes.txt"
+    export CATNAME="Earthquakes"
+    $CATEGORIZE
+  fi
+
+  if [ $VOLCANOES -ne 0 ];
+  then
+    export CATFILE="Volcanoes.txt"
+    export CATNAME="Volcanoes"
+    $CATEGORIZE
+  fi
+
+  rm Geology.txt
+  rm Volcanoes.txt
+  rm Earthquakes.txt
+
+fi
--- catscripts/Science/Science.sh
+++ catscripts/Science/Science.sh
@@ -5,7 +5,6 @@ CURRENTDIR="./catscripts/Science"
 $CURRENTDIR/Astronomy/Astronomy.sh
 $CURRENTDIR/Biology/Biology.sh
 $CURRENTDIR/Chemistry/Chemistry.sh
-$CURRENTDIR/Geography/Geography.sh
-$CURRENTDIR/Geology/Geology.sh
+$CURRENTDIR/Geography_and_geology/Geography.sh
 $CURRENTDIR/Physics/Physics.sh
 $CURRENTDIR/Scientists/Scientists.sh
