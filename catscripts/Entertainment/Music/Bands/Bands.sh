e9a030a5354d324231bbc9e2fd7ef1b39db4671e
I am not me <impeccabletiming@gmail.com>
2013-09-13 08:48:55 -0400

    Adding My Chemical Romance

--- catscripts/Entertainment/Music/Bands/Bands.sh
+++ catscripts/Entertainment/Music/Bands/Bands.sh
@@ -3,5 +3,6 @@
 ./catscripts/Entertainment/Music/Bands/Bon_Jovi.sh
 ./catscripts/Entertainment/Music/Bands/Linkin_Park.sh
 ./catscripts/Entertainment/Music/Bands/Metallica.sh
+./catscripts/Entertainment/Music/Bands/My_Chemical_Romance.sh
 ./catscripts/Entertainment/Music/Bands/One_Direction.sh
 ./catscripts/Entertainment/Music/Bands/The_Beatles.sh
---
+++ catscripts/Entertainment/Music/Bands/My_Chemical_Romance.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+KEYWORDS_MYCHEMICALROMANCE="My(| )Chemical(| )Romance|\bMCR\b"
+
+egrep -i "$KEYWORDS_MYCHEMICALROMANCE" newpages.txt >> MCR.txt
+
+MCR=`stat --print=%s MCR.txt`
+
+if [ $MCR -ne 0 ];
+then
+  export CATFILE="MCR.txt"
+  export CATNAME="My Chemical Romance"
+  $CATEGORIZE
+fi
+
+rm MCR.txt
