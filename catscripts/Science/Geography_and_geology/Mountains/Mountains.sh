772219ea1da60d5d10fc04149a9e3011958c6868
I am not me <impeccabletiming@gmail.com>
2013-09-17 09:36:12 -0400

    Adding Rocky Mountains

--- catscripts/Science/Geography_and_geology/Mountains/Mountains.sh
+++ catscripts/Science/Geography_and_geology/Mountains/Mountains.sh
@@ -5,3 +5,4 @@ CURRENTDIR="./catscripts/Science/Geography_and_geology/Mountains"
 . $CURRENTDIR/Andes/Andes.sh
 . $CURRENTDIR/Appalachian_Mountains/Appalachian.sh
 . $CURRENTDIR/Himalayas/Himalayas.sh
+. $CURRENTDIR/Rocky_Mountains/RockyMountains.sh
---
+++ catscripts/Science/Geography_and_geology/Mountains/Rocky_Mountains/RockyMountains.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+KEYWORDS_ROCKYMOUNTAINS="Rocky(| )Mountain|the(| )Rock(ies|ys)"
+KEYWORDS_ROCKYMOUNTAINS_EXCLUDE="Rocky(| )Mountain(| )Oysters"
+
+if [ "$1" == "" ];
+then
+  
+  egrep -i "$KEYWORDS_ROCKYMOUNTAINS" newpages.txt | egrep -iv "$KEYWORDS_ROCKYMOUNTAINS_EXCLUDE" >> RockyMountains.txt
+
+  ROCKYMOUNTAINS=`stat --print=%s RockyMountains.txt`
+
+  if [ $ROCKYMOUNTAINS -ne 0 ];
+  then
+    export CATFILE="RockyMountains.txt"
+    export CATNAME="Rocky Mountains"
+    $CATEGORIZE
+  fi
+
+  rm RockyMountains.txt
+
+fi
