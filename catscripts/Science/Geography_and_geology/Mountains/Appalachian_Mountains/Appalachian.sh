ce351f85a5d579fd50af8ba27f1fc076a8e97852
I am not me <impeccabletiming@gmail.com>
2013-09-17 09:30:55 -0400

    Adding Appalachian Mountains

---
+++ catscripts/Science/Geography_and_geology/Mountains/Appalachian_Mountains/Appalachian.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+KEYWORDS_APPALACHIAN="Appalachian"
+
+if [ "$1" == "" ];
+then
+  
+  egrep -i "$KEYWORDS_APPALACHIAN" newpages.txt >> Appalachian.txt
+
+  APPALACHIAN=`stat --print=%s Appalachian.txt`
+
+  if [ $APPALACHIAN -ne 0 ];
+  then
+    export CATFILE="Appalachian.txt"
+    export CATNAME="Appalachian Mountains"
+    $CATEGORIZE
+  fi
+
+  rm Appalachian.txt
+
+fi
--- catscripts/Science/Geography_and_geology/Mountains/Mountains.sh
+++ catscripts/Science/Geography_and_geology/Mountains/Mountains.sh
@@ -3,4 +3,5 @@
 CURRENTDIR="./catscripts/Science/Geography_and_geology/Mountains"
 
 . $CURRENTDIR/Andes/Andes.sh
+. $CURRENTDIR/Appalachian_Mountains/Appalachian.sh
 . $CURRENTDIR/Himalayas/Himalayas.sh
