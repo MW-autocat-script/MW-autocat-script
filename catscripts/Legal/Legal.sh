9c91bbd6e6ec94383f9fd02d94dcdfaf5e58de7f
I am not me <impeccabletiming@gmail.com>
2013-09-25 22:40:56 -0400

    Adding Divorce and Crime

--- catscripts/Legal/Legal.sh
+++ catscripts/Legal/Legal.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 KEYWORDS_LEGAL="\blegal|illegal|\blaw(s|)\b|lawyer|attorney"
+KEYWORDS_DIVORCE="Divorce|alimony"
+KEYWORDS_CRIME="\bCrime(|s)\b|criminal"
 
-egrep -i "$KEYWORDS_LEGAL" newpages.txt >> Legal.txt
+egrep -i "$KEYWORDS_LEGAL" newpages.txt | egrep -iv "$KEYWORDS_DIVORCE|$KEYWORDS_CRIME" >> Legal.txt
+egrep -i "$KEYWORDS_DIVORCE" newpages.txt >> Divorce.txt
+egrep -i "$KEYWORDS_CRIME" newpages.txt >> Crime
 
 LEGAL=`stat --print=%s Legal.txt`
+CRIME=`stat --print=%s Crime.txt`
+DIVORCE=`stat --print=%s Divorce.txt`
 
 if [ $LEGAL -ne 0 ];
 then
@@ -13,4 +19,20 @@ then
   $CATEGORIZE
 fi
 
+if [ $CRIME -ne 0 ];
+then
+  export CATFILE="Crime.txt"
+  export CATNAME="Crime"
+  $CATEGORIZE
+fi
+
+if [ $DIVORCE -ne 0 ];
+then
+  export CATFILE="Divorce.txt"
+  export CATNAME="Divorce"
+  $CATEGORIZE
+fi
+
 rm Legal.txt
+rm Crime.txt
+rm Divorce.txt
