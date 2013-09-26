66978d8e791051af34010b21e3fa0d3ba616630b
I am not me <impeccabletiming@gmail.com>
2013-09-26 16:56:11 -0400

    Adding Kesha

--- catscripts/Legal/Legal.sh
+++ catscripts/Legal/Legal.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-KEYWORDS_LEGAL="\blegal|illegal|\blaw(s|)\b|lawyer|attorney"
+KEYWORDS_LEGAL="\blegal|illegal|\blaw(s|)\b"
+KEYWORDS_LAWYERS="attorney|lawyer"
 KEYWORDS_DIVORCE="Divorce|alimony"
 KEYWORDS_CRIME="\bCrime(|s)\b|criminal"
 
-egrep -i "$KEYWORDS_LEGAL" newpages.txt | egrep -iv "$KEYWORDS_DIVORCE|$KEYWORDS_CRIME" >> Legal.txt
+egrep -i "$KEYWORDS_LEGAL" newpages.txt | egrep -iv "$KEYWORDS_DIVORCE|$KEYWORDS_CRIME|$KEYWORDS_LAWYERS" >> Legal.txt
 egrep -i "$KEYWORDS_DIVORCE" newpages.txt >> Divorce.txt
-egrep -i "$KEYWORDS_CRIME" newpages.txt >> Crime
+egrep -i "$KEYWORDS_CRIME" newpages.txt >> Crime.txt
+egrep -i "$KEYWORDS_LAWYERS" newpages.txt >> Lawyers.txt
 
 LEGAL=`stat --print=%s Legal.txt`
 CRIME=`stat --print=%s Crime.txt`
 DIVORCE=`stat --print=%s Divorce.txt`
+LAWYERS=`stat --print=%s Lawyers.txt`
 
 if [ $LEGAL -ne 0 ];
 then
@@ -33,6 +36,14 @@ then
   $CATEGORIZE
 fi
 
+if [ $LAWYERS -ne 0 ];
+then
+  export CATFILE="Lawyers.txt"
+  export CATNAME="Lawyers"
+  $CATEGORIZE
+fi
+
 rm Legal.txt
 rm Crime.txt
 rm Divorce.txt
+rm Lawyers.txt
