0f82d8552fe8f3e8762fd22a87b341177b4bd9e0
I am not me <impeccabletiming@gmail.com>
2013-10-12 15:35:13 -0400

    Adding English to Latin

---
+++ catscripts/Language/Non-English_languages/English_to_Latin/EnglishtoLatin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+egrep -i 'How do you say.+in Latin' newpages.txt >> EnglishtoLatin.txt
+egrep -i 'How do you spell.+in Latin' newpages.txt >> EnglishtoLatin.txt
+egrep -i '^[abcdefghijklmopqrstuvwxyz]{1,} in Latin' newpages.txt >> EnglishtoLatin.txt
+egrep -i 'What is the Latin word for' newpages.txt >> EnglishtoLatin.txt
+egrep -i 'How to say.+in Latin' newpages.txt >> EnglishtoLatin.txt
+
+LATIN=`stat --print=%s EnglishtoLatin.txt`
+
+if [ $Latin -ne 0 ];
+then
+  export CATFILE="EnglishtoLatin.txt"
+  export CATNAME="English to Latin"
+  $CATEGORIZE
+fi
+
+rm EnglishtoLatin.txt
--- catscripts/Language/Non-English_languages/NonEnglish.sh
+++ catscripts/Language/Non-English_languages/NonEnglish.sh
@@ -7,4 +7,5 @@ $CURRENTDIR/English_to_German/EnglishtoGerman.sh
 $CURRENTDIR/English_to_Greek/EnglishtoGreek.sh
 $CURRENTDIR/English_to_Italian/EnglishtoItalian.sh
 $CURRENTDIR/English_to_Korean/EnglishtoKorean.sh
+$CURRENTDIR/English_to_Latin/EnglishtoLatin.sh
 $CURRENTDIR/English_to_Spanish/EnglishtoSpanish.sh
