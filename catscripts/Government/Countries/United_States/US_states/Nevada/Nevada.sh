80e778894fca62e5c415cad6218e946448e50863
I am not me <impeccabletiming@gmail.com>
2013-10-13 14:25:23 -0400

    Adding Las Vegas

--- catscripts/Government/Countries/United_States/US_states/Nevada/Nevada.sh
+++ catscripts/Government/Countries/United_States/US_states/Nevada/Nevada.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 KEYWORDS_NEVADA="Nevada"
+KEYWORDS_LASVEGAS="Las(| )Vegas"
+KEYWORDS_NEVADA_EXCLUDE="$KEYWORDS_LASVEGAS"
 
 if [ "$1" == "" ];
 then
 
-  egrep -i "$KEYWORDS_NEVADA" newpages.txt >> Nevada.txt
+  egrep -i "$KEYWORDS_NEVADA" newpages.txt | egrep -iv "$KEYWORDS_NEVADA_EXCLUDE" >> Nevada.txt
+  egrep -i "KEYWORDS_LASVEGAS" newpages.txt >> LasVegas.txt
 
   NEVADA=`stat --print=%s Nevada.txt`
+  LASVEGAS=`stat --print=%s LasVegas.txt`
 
   if [ $NEVADA -ne 0 ];
   then
@@ -15,7 +19,15 @@ then
     export CATNAME="Nevada"
     $CATEGORIZE
   fi
+  
+  if [ $LASVEGAS -ne 0 ];
+  then
+    export CATFILE="LasVegas.txt"
+    export CATNAME="Las Vegas"
+    $CATEGORIZE
+  fi
 
   rm Nevada.txt
+  rm LasVegas.txt
 
 fi
