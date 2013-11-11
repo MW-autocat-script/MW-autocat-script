90e9ab85205a84204e6deb1356a944eae05285c5
I am not me <impeccabletiming@gmail.com>
2013-11-11 02:21:31 -0500

    Adding Cleveland and Cincinnati

--- catscripts/Government/Countries/United_States/US_states/Ohio/Ohio.sh
+++ catscripts/Government/Countries/United_States/US_states/Ohio/Ohio.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
-egrep -i 'Ohio' newpages.txt >> Ohio.txt
+KEYWORDS_OHIO="Ohio"
+KEYWORDS_CLEVELAND="Cleveland"
+KEYWORDS_CLEVELAND_EXCLUDE="Cleveland(| )Brown|Grover(| )Cleveland|President(| )Cleveland"
+KEYWORDS_CINCINNATI="Cincinnati"
+KEYWORDS_OHIO_EXCLUDE="$KEYWORDS_CLEVELAND|$KEYWORDS_CINCINNATI"
 
-OHIO=`stat --print=%s Ohio.txt`
+OHIO=`egrep -i "$KEYWORDS_OHIO" newpages.txt | egrep -iv "$KEYWORDS_OHIO_EXCLUDE"`
+CLEVELAND=`egrep -i "$KEYWORDS_CLEVELAND" newpages.txt | egrep -iv "$KEYWORDS_CLEVELAND_EXCLUDE"`
+CINCINNATI=`egrep -i "$KEYWORDS_CINCINNATI" newpages.txt`
 
-if [ $OHIO -ne 0 ];
+if [ "$OHIO" != "" ];
 then
+  egrep -i "$KEYWORDS_OHIO" newpages.txt | egrep -iv "$KEYWORDS_OHIO_EXCLUDE" > Ohio.txt
   export CATFILE="Ohio.txt"
   export CATNAME="Ohio"
   $CATEGORIZE
+  rm Ohio.txt
 fi
 
-rm Ohio.txt
+if [ "$CLEVELAND" != "" ];
+then
+  egrep -i "$KEYWORDS_CLEVELAND" newpages.txt | egrep -iv "$KEYWORDS_CLEVELAND_EXCLUDE" > Cleveland.txt
+  export CATFILE="Cleveland.txt"
+  export CATNAME="Cleveland"
+  $CATEGORIZE
+  rm Cleveland.txt
+fi
+
+if [ "$CINCINNATI" != "" ];
+then
+  egrep -i "$KEYWORDS_CINCINNATI" newpages.txt > Cincinnati.txt
+  export CATFILE="Cincinnati.txt"
+  export CATNAME="Cincinnati"
+  $CATEGORIZE
+  rm Cincinnati.txt
+fi
