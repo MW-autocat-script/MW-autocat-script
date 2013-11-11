57eabd805a61e8571bb2e75f5a6650d467b31d6c
I am not me <impeccabletiming@gmail.com>
2013-11-11 06:56:16 -0500

    Make I/O performance changes to Alabama

--- catscripts/Government/Countries/United_States/US_states/Alabama/Alabama.sh
+++ catscripts/Government/Countries/United_States/US_states/Alabama/Alabama.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-egrep -i 'Alabama' newpages.txt | egrep -iv 'Sweet Home Alabama' >> Alabama.txt
+KEYWORDS_ALABAMA="Alabama"
+KEYWORDS_ALABAMA_EXCLUDE="Sweet(| )Home(| )Alabama"
 
-ALABAMA=`stat --print=%s Alabama.txt`
+ALABAMA=`egrep -i "$KEYWORDS_ALABAMA" newpages.txt | egrep -iv "$KEYWORDS_ALABAMA_EXCLUDE"` 
 
-if [ $ALABAMA -ne 0 ];
+if [ "$ALABAMA" != "" ];
 then
+  egrep -i "$KEYWORDS_ALABAMA" newpages.txt | egrep -iv "$KEYWORDS_ALABAMA_EXCLUDE" > Alabama.txt
   export CATFILE="Alabama.txt"
   export CATNAME="Alabama"
   $CATEGORIZE
+  rm Alabama.txt
 fi
-
-rm Alabama.txt
