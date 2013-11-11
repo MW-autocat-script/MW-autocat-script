ab8351c4eadc05d888af0b277ce9d41771228526
I am not me <impeccabletiming@gmail.com>
2013-11-11 09:29:03 -0500

    Make I/O performance changes to Colorado

--- catscripts/Government/Countries/United_States/US_states/Colorado/Colorado.sh
+++ catscripts/Government/Countries/United_States/US_states/Colorado/Colorado.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-egrep -i 'Colorado' newpages.txt | egrep -iv 'Denver' >> Colorado.txt
-egrep -i 'Denver' newpages.txt | egrep -iv 'Denver(| )Broncos|Denver(| )Nuggets|John(| )Denver' >> Denver.txt
+KEYWORDS_COLORADO="Colorado"
+KEYWORDS_DENVER="Denver"
+KEYWORDS_COLORADO_EXCLUDE="$KEYWORDS_DENVER"
+KEYWORDS_DENVER_EXCLUDE="Denver(| )Broncos|Denver(| )Nuggets|John(| )Denver"
 
-COLORADO=`stat --print=%s Colorado.txt`
-DENVER=`stat --print=%s Denver.txt`
+COLORADO=`egrep -i "$KEYWORDS_COLORADO" newpages.txt | egrep -iv "$KEYWORDS_COLORADO_EXCLUDE"`
+DENVER=`egrep -i "$KEYWORDS_DENVER" newpages.txt | egrep -iv "$KEYWORDS_DENVER_EXCLUDE"`
 
-if [ $COLORADO -ne 0 ];
+if [ "$COLORADO" != "" ];
 then
+  egrep -i "$KEYWORDS_COLORADO" newpages.txt | egrep -iv "$KEYWORDS_COLORADO_EXCLUDE" > Colorado.txt
   export CATFILE="Colorado.txt"
   export CATNAME="Colorado"
   $CATEGORIZE
+  rm Colorado.txt
+  unset COLORADO
 fi
 
-if [ $DENVER -ne 0 ];
+if [ "$DENVER" != "" ];
 then
+  egrep -i "$KEYWORDS_DENVER" newpages.txt | egrep -iv "$KEYWORDS_DENVER_EXCLUDE" > Denver.txt
   export CATFILE="Denver.txt"
   export CATNAME="Denver, Colorado"
   $CATEGORIZE
+  rm Denver.txt
+  unset DENVER
 fi
-
-rm Colorado.txt
-rm Denver.txt
