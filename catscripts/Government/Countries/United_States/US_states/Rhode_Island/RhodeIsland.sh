87aedb9c68803b0797940cd890d763a6ad6343e7
I am not me <impeccabletiming@gmail.com>
2013-11-13 05:07:41 -0500

    Adding Rhode Island

---
+++ catscripts/Government/Countries/United_States/US_states/Rhode_Island/RhodeIsland.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+KEYWORDS_RHODEISLAND="Rhode(| )Island"
+
+RHODEISLAND=`egrep -i "$KEYWORDS_RHODEISLAND" newpages.txt`
+
+if [ "$RHODEISLAND" != "" ];
+then
+  egrep -i "$KEYWORDS_RHODEISLAND" newpages.txt > RhodeIsland.txt
+  export CATFILE="RhodeIsland.txt"
+  export CATNAME="Rhode Island"
+  $CATEGORIZE
+  rm RhodeIsland.txt
+  unset RHODEISLAND
+fi
