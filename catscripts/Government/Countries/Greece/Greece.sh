7a0c7f54794326c27e37de8d6e5696ca670784ee
I am not me <impeccabletiming@gmail.com>
2013-11-17 12:46:06 -0500

    Adding Sparta and Athens

--- catscripts/Government/Countries/Greece/Greece.sh
+++ catscripts/Government/Countries/Greece/Greece.sh
@@ -1,24 +1,56 @@
 #!/bin/bash
 
-egrep -i 'Greece' newpages.txt | egrep -iv 'ancient Greece' >> Greece.txt
-egrep -i 'ancient Greece' newpages.txt >> Ancient.txt
+KEYWORDS_GREECE="Greece"
+KEYWORDS_ANCIENTGREECE="Ancient(| )Gree(k|ce)"
+KEYWORDS_ATHENS="Athens|Athenian"
+KEYWORDS_SPARTA="Sparta(s|n)\b"
+KEYWORDS_SPARTA_EXCLUDE="Halo"
+KEYWORDS_SPARTA_EXCLUDE_CASESENSITVE="SPARTAN"
+KEYWORDS_ANCIENTGREECE_EXCLUDE="$KEYWORDS_SPARTA|$KEYWORDS_ATHENS"
+KEYWORDS_GREECE_EXCLUDE="$KEYWORDS_ANCIENTGREECE|$KEYWORDS_ATHENS|$KEYWORDS_SPARTA"
 
-GREECE=`stat --print=%s Greece.txt`
-ANCIENT=`stat --print=%s Ancient.txt`
+GREECE=`egrep -i "$KEYWORDS_GREECE" newpages.txt | egrep -iv "$KEYWORDS_GREECE_EXCLUDE"`
+ANCIENT=`egrep -i "$KEYWORDS_ANCIENTGREECE" newpages.txt | egrep -iv "$KEYWORDS_ANCIENTGREECE_EXCLUDE"`
+ATHENS=`egrep -i "$KEYWORDS_ATHENS" newpages.txt`
+SPARTA=`egrep -i "$KEYWORDS_SPARTA" newpages.txt | egrep -iv "$KEYWORDS_SPARTA_EXCLUDE" | egrep -v "$KEYWORDS_SPARTA_EXCLUDE_CASESENSITVE"`
 
-if [ $GREECE -ne 0 ];
+if [ "$GREECE" != "" ];
 then
+  egrep -i "$KEYWORDS_GREECE" newpages.txt | egrep -iv "$KEYWORDS_GREECE_EXCLUDE" > Greece.txt
   export CATFILE="Greece.txt"
   export CATNAME="Greece"
   $CATEGORIZE
+  rm Greece.txt
+  unset GREECE
 fi
 
-if [ $ANCIENT -ne 0 ];
+if [ "$ANCIENT" != "" ];
 then
+  egrep -i "$KEYWORDS_ANCIENTGREECE" newpages.txt | egrep -iv "$KEYWORDS_ANCIENTGREECE_EXCLUDE" > Ancient.txt
   export CATFILE="Ancient.txt"
   export CATNAME="Ancient Greece"
   $CATEGORIZE
+  rm Ancient.txt
+  unset ANCIENT
 fi
 
-rm Greece.txt
-rm Ancient.txt
+if [ "$ATHENS" != "" ];
+then
+  egrep -i "$KEYWORDS_ATHENS" newpages.txt > Athens.txt
+  export CATFILE="Athens.txt"
+  export CATNAME="Athens"
+  $CATEGORIZE
+  rm Athens.txt
+  unset ATHENS
+fi
+
+if [ "$SPARTA" != "" ];
+then
+  egrep -i "$KEYWORDS_SPARTA" newpages.txt | egrep -iv "$KEYWORDS_SPARTA_EXCLUDE" | egrep -v "$KEYWORDS_SPARTA_EXCLUDE_CASESENSITVE" > Sparta.txt
+  export CATFILE="Sparta.txt"
+  export CATNAME="Sparta"
+  $CATEGORIZE
+  rm Sparta.txt
+  unset SPARTA
+fi
+
