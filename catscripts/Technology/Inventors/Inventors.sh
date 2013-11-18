2ec3cabb90e4347124937d2769354d378f6ee938
I am not me <impeccabletiming@gmail.com>
2013-11-18 12:23:04 -0500

    Optimize Inventors

--- catscripts/Technology/Inventors/Inventors.sh
+++ catscripts/Technology/Inventors/Inventors.sh
@@ -1,120 +1,131 @@
 #!/bin/bash
 
-export KEYWORDS_BABBAGE="Babbage"
-export KEYWORDS_BENZ="Karl Benz"
-export KEYWORDS_ALEXANDERBELL="Alex(|ander)(| )(|Graham|G\.)(| )Bell"
-export KEYWORDS_BENFRANKLIN="Ben(|jamin)(| )Franklin"
-export KEYWORDS_GEORGEWASHINGTONCARVER="George(| )(|Washington)(| )Carver"
-export KEYWORDS_DAVINCI="da(| )vinci"
-export KEYWORDS_EDISON="Edison"
-export KEYWORDS_TESLA="Tesla"
-export KEYWORDS_ELIWHITNEY="Eli(| )Whitney"
+KEYWORDS_INVENTORS="Invent(e|o)r"
+KEYWORDS_INVENTORS_OTHER="Ralph(| )(|H(\.|))(| )Baer|Louis(| )Braille" #These are inventors who are notable enough to have Wikipedia articles but do not yet have enough questions about them to warrant their own category
+KEYWORDS_BABBAGE="Babbage"
+KEYWORDS_BENZ="Karl Benz"
+KEYWORDS_ALEXANDERBELL="Alex(|ander)(| )(|Graham|G\.)(| )Bell"
+KEYWORDS_BENFRANKLIN="Ben(|jamin)(| )Franklin"
+KEYWORDS_GEORGEWASHINGTONCARVER="George(| )(|Washington)(| )Carver"
+KEYWORDS_DAVINCI="da(| )vinci"
+KEYWORDS_EDISON="Edison"
+KEYWORDS_TESLA="Tesla"
+KEYWORDS_TESLA_EXCLUDE="Fallout(| )3|Brotherhood(| )of(| )Steel|Tesla(| )Roadster|Tesla(| )Motors"
+KEYWORDS_ELIWHITNEY="Eli(| )Whitney"
+KEYWORDS_INVENTORS_EXCLUDE="$KEYWORDS_ALEXANDERBELL|$KEYWORDS_BABBAGE|$KEYWORDS_BENFRANKLIN|$KEYWORDS_BENZ|$KEYWORDS_DAVINCI|$KEYWORDS_EDISON|$KEYWORDS_ELIWHITNEY|$KEYWORDS_GEORGEWASHINGTONCARVER|$KEYWORDS_TESLA"
+KEYWORDS_INVENTORS_ALL="$KEYWORDS_INVENTORS|$KEYWORDS_INVENTORS_EXCLUDE|$KEYWORDS_INVENTORS_OTHER"
 
-export INVENTORSLIST="$KEYWORDS_BABBAGE|$KEYWORDS_ALEXANDERBELL|$KEYWORDS_BENZ|$KEYWORDS_BENFRANKLIN|$KEYWORDS_GEORGEWASHINGTONCARVER|$KEYWORDS_DAVINCI|$KEYWORDS_EDISON|$KEYWORDS_TESLA|$KEYWORDS_ELIWHITNEY"
-
-#These are inventors who are notable enough to have Wikipedia articles but do not yet have enough questions about them to warrant their own category
-
-export INVENTORS_OTHER="Ralph(| )(|H(\.|))(| )Baer|Louis(| )Braille"
-
-egrep -i "$KEYWORDS_BABBAGE" newpages.txt >> CharlesBabbage.txt
-egrep -i "$KEYWORDS_ALEXANDERBELL" newpages.txt > AlexanderGrahamBell.txt
-egrep -i "$KEYWORDS_BENZ" newpages.txt > KarlBenz.txt
-egrep -i "$KEYWORDS_BENFRANKLIN" newpages.txt > BenjaminFranklin.txt
-egrep -i "$KEYWORDS_GEORGEWASHINGTONCARVER" newpages.txt > GeorgeWashingtonCarver.txt
-egrep -i "$KEYWORDS_DAVINCI" newpages.txt > LeonardoDaVinci.txt
-egrep -i "$KEYWORDS_EDISON" newpages.txt > ThomasEdison.txt
-egrep -i "$KEYWORDS_TESLA" newpages.txt | egrep -iv "Fallout(| )3|Brotherhood(| )of(| )Steel|Tesla(| )Roadster|Tesla(| )Motors" > NikolaTesla.txt
-egrep -i "$KEYWORDS_ELIWHITNEY" newpages.txt > EliWhitney.txt
-egrep -i 'Invent(e|o)r(|s)\b' newpages.txt | egrep -iv "$INVENTORSLIST" > Inventors.txt
-
-INVENTORS=`stat --print=%s Inventors.txt`
-BABBAGE=`stat --print=%s CharlesBabbage.txt`
-BELL=`stat --print=%s AlexanderGrahamBell.txt`
-BENZ=`stat --print=%s KarlBenz.txt`
-FRANKLIN=`stat --print=%s BenjaminFranklin.txt`
-CARVER=`stat --print=%s GeorgeWashingtonCarver.txt`
-DAVINCI=`stat --print=%s LeonardoDaVinci.txt`
-EDISON=`stat --print=%s ThomasEdison.txt`
-TESLA=`stat --print=%s NikolaTesla.txt`
-WHITNEY=`stat --print=%s EliWhitney.txt`
-
-if [ $INVENTORS -ne 0 ];
+if [ "$1" == "" ]; #Normal operation
 then
-  export CATFILE="Inventors.txt"
-  export CATNAME="Inventors"
-  $CATEGORIZE
-fi
 
-if [ $BABBAGE -ne 0 ];
-then
-  export CATFILE="CharlesBabbage.txt"
-  export CATNAME="Charles Babbage"
-  $CATEGORIZE
-fi
+  INVENTORS=`egrep -i "$KEYWORDS_INVENTORS|$KEYWORDS_INVENTORS_OTHER" newpages.txt | egrep -iv "$KEYWORDS_INVENTORS_EXCLUDE"`
+  BABBAGE=`egrep -i "$KEYWORDS_BABBAGE" newpages.txt`
+  BELL=`egrep -i "$KEYWORDS_ALEXANDERBELL" newpages.txt`
+  BENZ=`egrep -i "$KEYWORDS_BENZ" newpages.txt`
+  FRANKLIN=`egrep -i "$KEYWORDS_BENFRANKLIN" newpages.txt`
+  CARVER=`egrep -i "$KEYWORDS_GEORGEWASHINGTONCARVER" newpages.txt`
+  DAVINCI=`egrep -i "$KEYWORDS_DAVINCI" newpages.txt`
+  EDISON=`egrep -i "$KEYWORDS_EDISON" newpages.txt`
+  TESLA=`egrep -i "$KEYWORDS_TESLA" newpages.txt | egrep -iv "$KEYWORDS_TESLA_EXCLUDE"`
+  WHITNEY=`egrep -i "$KEYWORDS_ELIWHITNEY" newpages.txt`
 
-if [ $BELL -ne 0 ];
-then
-  export CATFILE="AlexanderGrahamBell.txt"
-  export CATNAME="Alexander Graham Bell"
-  $CATEGORIZE
-fi
+  if [ "$INVENTORS" != "" ];
+  then
+    egrep -i "$KEYWORDS_INVENTORS" newpages.txt | egrep -iv "$KEYWORDS_INVENTORS_EXCLUDE" > Inventors.txt
+    export CATFILE="Inventors.txt"
+    export CATNAME="Inventors"
+    $CATEGORIZE
+    rm Inventors.txt
+    unset INVENTORS
+  fi
 
-if [ $BENZ -ne 0 ];
-then
-  export CATFILE="KarlBenz.txt"
-  export CATNAME="Karl Benz"
-  $CATEGORIZE
-fi
+  if [ "$BABBAGE"  != "" ];
+  then
+    egrep -i "$KEYWORDS_BABBAGE" newpages.txt > CharlesBabbage.txt
+    export CATFILE="CharlesBabbage.txt"
+    export CATNAME="Charles Babbage"
+    $CATEGORIZE
+    rm CharlesBabbage.txt
+    unset BABBAGE
+  fi
 
-if [ $FRANKLIN -ne 0 ];
-then
-  export CATFILE="BenjaminFranklin.txt"
-  export CATNAME="Benjamin Franklin"
-  $CATEGORIZE
-fi
+  if [ "$BELL" != "" ];
+  then
+    egrep -i "$KEYWORDS_ALEXANDERBELL" newpages.txt > AlexanderGrahamBell.txt
+    export CATFILE="AlexanderGrahamBell.txt"
+    export CATNAME="Alexander Graham Bell"
+    $CATEGORIZE
+    rm AlexanderGrahamBell.txt
+    unset BELL
+  fi
 
-if [ $CARVER -ne 0 ];
-then
-  export CATFILE="GeorgeWashingtonCarver.txt"
-  export CATNAME="George Washington Carver"
-  $CATEGORIZEfi
-fi
+  if [ "$BENZ" != "" ];
+  then
+    egrep -i "$KEYWORDS_BENZ" newpages.txt > KarlBenz.txt
+    export CATFILE="KarlBenz.txt"
+    export CATNAME="Karl Benz"
+    $CATEGORIZE
+    rm KarlBenz.txt
+    unset BENZ
+  fi
 
-if [ $DAVINCI -ne 0 ];
-then
-  export CATFILE="LeonardoDaVinci.txt"
-  export CATNAME="Leonardo da Vinci"
-  $CATEGORIZE
-fi
+  if [ "$FRANKLIN" != "" ];
+  then
+    egrep -i "$KEYWORDS_BENFRANKLIN" newpages.txt > BenjaminFranklin.txt
+    export CATFILE="BenjaminFranklin.txt"
+    export CATNAME="Benjamin Franklin"
+    $CATEGORIZE
+    rm BenjaminFranklin.txt
+    unset FRANKLIN
+  fi
 
-if [ $EDISON -ne 0 ];
-then
-  export CATFILE="ThomasEdison.txt"
-  export CATNAME="Thomas Edison"
-  $CATEGORIZE
-fi
+  if [ "$CARVER" != "" ];
+  then
+    egrep -i "$KEYWORDS_GEORGEWASHINGTONCARVER" newpages.txt > GeorgeWashingtonCarver.txt
+    export CATFILE="GeorgeWashingtonCarver.txt"
+    export CATNAME="George Washington Carver"
+    $CATEGORIZE
+    rm GeorgeWashingtonCarver.txt
+    unset CARVER
+  fi
 
-if [ $TESLA -ne 0 ];
-then
-  export CATFILE="NikolaTesla.txt"
-  export CATNAME="Nikola Tesla"
-  $CATEGORIZE
-fi
+  if [ "$DAVINCI" != "" ];
+  then
+    egrep -i "$KEYWORDS_DAVINCI" newpages.txt > LeonardoDaVinci.txt
+    export CATFILE="LeonardoDaVinci.txt"
+    export CATNAME="Leonardo da Vinci"
+    $CATEGORIZE
+    rm LeonardoDaVinci.txt
+    unset DAVINCI
+  fi
 
-if [ $WHITNEY -ne 0 ];
-then
-  export CATFILE="EliWhitney.txt"
-  export CATNAME="Eli Whitney"
-  $CATEGORIZE
-fi
+  if [ "$EDISON" != "" ];
+  then
+    egrep -i "$KEYWORDS_EDISON" newpages.txt > ThomasEdison.txt
+    export CATFILE="ThomasEdison.txt"
+    export CATNAME="Thomas Edison"
+    $CATEGORIZE
+    rm Edison.txt
+    unset EDISON
+  fi
 
-rm Inventors.txt
-rm CharlesBabbage.txt
-rm AlexanderGrahamBell.txt
-rm KarlBenz.txt
-rm BenjaminFranklin.txt
-rm GeorgeWashingtonCarver.txt
-rm LeonardoDaVinci.txt
-rm ThomasEdison.txt
-rm NikolaTesla.txt
-rm EliWhitney.txt
+  if [ "$TESLA" != "" ];
+  then
+    egrep -i "$KEYWORDS_TESLA" newpages.txt | egrep -iv "$KEYWORDS_TESLA_EXCLUDE" > NikolaTesla.txt
+    export CATFILE="NikolaTesla.txt"
+    export CATNAME="Nikola Tesla"
+    $CATEGORIZE
+    rm NikolaTesla.txt
+    unset TESLA
+  fi
+
+  if [ "$WHITNEY" != "" ];
+  then
+    egrep -i "$KEYWORDS_ELIWHITNEY" newpages.txt > EliWhitney.txt
+    export CATFILE="EliWhitney.txt"
+    export CATNAME="Eli Whitney"
+    $CATEGORIZE
+    rm EliWhitney.txt
+    unset WHITNEY
+  fi
+fi
