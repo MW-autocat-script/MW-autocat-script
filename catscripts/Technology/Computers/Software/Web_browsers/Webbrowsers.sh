77bce101c543807a5951c76217dfe7a53e7b239a
I am not me <impeccabletiming@gmail.com>
2013-11-18 18:09:47 -0500

    Optimize Web browsers

--- catscripts/Technology/Computers/Software/Web_browsers/Webbrowsers.sh
+++ catscripts/Technology/Computers/Software/Web_browsers/Webbrowsers.sh
@@ -5,52 +5,55 @@ KEYWORDS_GOOGLECHROME="Google(| )Chrome|Chrome(| )browser"
 KEYWORDS_INTERNETEXPLORER="Internet(| )Explorer|\bIE(| )[0-9]{1,2}\b"
 KEYWORDS_WEBBROWSER="Web(| )browser"
 KEYWORDS_WEBBROWSER_EXCLUDE="$KEYWORDS_FIREFOX|$KEYWORDS_GOOGLECHROME|$KEYWORDS_INTERNETEXPLORER"
-KEYWORDS_WEBBROWSER_OTHER="NCSA(| )Mosaic|Safari(| )browser|Chromium(| )browser|\bDillo|Netscape(| )Navigator|Arachne|\bAmaya\b|NetFront|NetSurf|WebKit|Konqueror|Kazekhase|Mozilla"
+KEYWORDS_WEBBROWSER_OTHER="NCSA(| )Mosaic|Safari(| )browser|Chromium(| )browser|\bDillo\b|Netscape(| )Navigator|Arachne|\bAmaya\b|NetFront|NetSurf|WebKit|Konqueror|Kazekhase|Mozilla"
 KEYWORDS_WEBBROWSER_ALL="$KEYWORDS_WEBBROWSER|$KEYWORDS_WEBBROWSER_EXCLUDE|$KEYWORDS_WEBBROWSER_OTHER"
 
 if [ "$1" == "" ]; #Normal operation
 then
-  egrep -i "$KEYWORDS_INTERNETEXPLORER" newpages.txt >> InternetExplorer.txt
-  egrep -i "$KEYWORDS_FIREFOX" newpages.txt >> MozillaFirefox.txt
-  egrep -i "$KEYWORDS_GOOGLECHROME" newpages.txt >> GoogleChrome.txt
-  egrep -i "$KEYWORDS_WEBBROWSER|$KEYWORDS_WEBBROWSER_OTHER" newpages.txt | egrep -iv "$KEYWORDS_WEBBROWSER_EXCLUDE" >> Webbrowsers.txt
 
-  IEXPLORER=`stat --print=%s InternetExplorer.txt`
-  FIREFOX=`stat --print=%s MozillaFirefox.txt`
-  CHROME=`stat --print=%s GoogleChrome.txt`
-  BROWSERS=`stat --print=%s Webbrowsers.txt`
+  IEXPLORER=`egrep -i "$KEYWORDS_INTERNETEXPLORER" newpages.txt`
+  FIREFOX=`egrep -i "$KEYWORDS_FIREFOX" newpages.txt`
+  CHROME=`egrep -i "$KEYWORDS_GOOGLECHROME" newpages.txt`
+  BROWSERS=`egrep -i "$KEYWORDS_WEBBROWSER|$KEYWORDS_WEBBROWSER_OTHER" newpages.txt | egrep -iv "$KEYWORDS_WEBBROWSER_EXCLUDE"`
 
-  if [ $IEXPLORER -ne 0 ];
+  if [ "$IEXPLORER" != "" ];
   then
+    egrep -i "$KEYWORDS_INTERNETEXPLORER" newpages.txt > InternetExplorer.txt
     export CATFILE="InternetExplorer.txt"
     export CATNAME="Internet Explorer"
     $CATEGORIZE
+    rm InternetExplorer.txt
+    unset IEXPLORER
   fi
 
-  if [ $FIREFOX -ne 0 ];
+  if [ "$FIREFOX" != "" ];
   then
+    egrep -i "$KEYWORDS_FIREFOX" newpages.txt > MozillaFirefox.txt
     export CATFILE="MozillaFirefox.txt"
     export CATNAME="Mozilla Firefox"
     $CATEGORIZE
+    rm MozillaFirefox.txt
+    unset FIREFOX
   fi
 
-  if [ $CHROME -ne 0 ];
+  if [ "$CHROME" != "" ];
   then
+    egrep -i "$KEYWORDS_GOOGLECHROME" newpages.txt > GoogleChrome.txt
     export CATFILE="GoogleChrome.txt"
     export CATNAME="Google Chrome"
     $CATEGORIZE
+    rm GoogleChrome.txt
+    unset CHROME
   fi
 
-  if [ $BROWSERS -ne 0 ];
+  if [ "$BROWSERS" != "" ];
   then
+    egrep -i "$KEYWORDS_WEBBROWSER|$KEYWORDS_WEBBROWSER_OTHER" newpages.txt | egrep -iv "$KEYWORDS_WEBBROWSER_EXCLUDE" > Webbrowsers.txt
     export CATFILE="Webbrowsers.txt"
     export CATNAME="Web browsers"
     $CATEGORIZE
+    rm Webbrowsers.txt
+    unset BROWSERS
   fi
-
-  rm InternetExplorer.txt
-  rm MozillaFirefox.txt
-  rm GoogleChrome.txt
-  rm Webbrowsers.txt
 
 fi
