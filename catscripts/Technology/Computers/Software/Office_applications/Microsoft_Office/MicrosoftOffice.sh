60b2c11c83b3b5e39a970a1490e58a6a36e16ebe
I am not me <impeccabletiming@gmail.com>
2013-11-18 20:41:42 -0500

    Optimize Microsoft Office

--- catscripts/Technology/Computers/Software/Office_applications/Microsoft_Office/MicrosoftOffice.sh
+++ catscripts/Technology/Computers/Software/Office_applications/Microsoft_Office/MicrosoftOffice.sh
@@ -5,63 +5,67 @@ KEYWORDS_MICROSOFTACCESS="Microsoft(| )Access|\bms(|)access"
 KEYWORDS_MICROSOFTEXCEL="Excel\b|\.xls"
 KEYWORDS_MICROSOFTEXCEL_EXCLUDE="Hyundai"
 KEYWORDS_MICROSOFTPOWERPOINT="power(| )point|\.ppt"
-KEYWORDS_MICROSOFTWORD="Microsoft(| )Word|\bms(|-| )word|\.doc"
+KEYWORDS_MICROSOFTWORD="Microsoft(| )(|Office)(| )Word|\bms(|-| )word|\.doc"
 KEYWORDS_MICROSOFTOFFICE_EXCLUDE="$KEYWORDS_MICROSOFTACCESS|$KEYWORDS_MICROSOFTEXCEL|$KEYWORDS_MICROSOFTPOWERPOINT|$KEYWORDS_MICROSOFTWORD"
 KEYWORDS_MICROSOFTOFFICE_ALL="$KEYWORDS_MICROSOFTOFFICE|$KEYWORDS_MICROSOFTOFFICE_EXCLUDE"
 
 if [ "$1" == "" ]; #Normal operation
 then
-  egrep -i "$KEYWORDS_MICROSOFTACCESS" newpages.txt >> MicrosoftAccess.txt
-  egrep -i "$KEYWORDS_MICROSOFTEXCEL" newpages.txt | egrep -iv "$KEYWORDS_MICROSOFTEXCEL_EXCLUDE" >> MicrosoftExcel.txt
-  egrep -i "$KEYWORDS_MICROSOFTPOWERPOINT" newpages.txt >> MicrosoftPowerPoint.txt
-  egrep -i "$KEYWORDS_MICROSOFTWORD" newpages.txt >> MicrosoftWord.txt
-  egrep -i "$KEYWORDS_MICROSOFTOFFICE" newpages.txt | egrep -iv "$KEYWORDS_MICROSOFTOFFICE_EXCLUDE" >> MicrosoftOffice.txt
 
-  OFFICE=`stat --print=%s MicrosoftOffice.txt`
-  ACCESS=`stat --print=%s MicrosoftAccess.txt`
-  EXCEL=`stat --print=%s MicrosoftExcel.txt`
-  POWERPOINT=`stat --print=%s MicrosoftPowerPoint.txt`
-  WORD=`stat --print=%s MicrosoftWord.txt`
+  OFFICE=`egrep -i "$KEYWORDS_MICROSOFTOFFICE" newpages.txt | egrep -iv "$KEYWORDS_MICROSOFTOFFICE_EXCLUDE"`
+  ACCESS=`egrep -i "$KEYWORDS_MICROSOFTACCESS" newpages.txt`
+  EXCEL=`egrep -i "$KEYWORDS_MICROSOFTEXCEL" newpages.txt | egrep -iv "$KEYWORDS_MICROSOFTEXCEL_EXCLUDE"`
+  POWERPOINT=`egrep -i "$KEYWORDS_MICROSOFTPOWERPOINT" newpages.txt`
+  WORD=`egrep -i "$KEYWORDS_MICROSOFTWORD" newpages.txt`
 
-  if [ $OFFICE -ne 0 ];
+  if [ "$OFFICE" != "" ];
   then
+      egrep -i "$KEYWORDS_MICROSOFTOFFICE" newpages.txt | egrep -iv "$KEYWORDS_MICROSOFTOFFICE_EXCLUDE" > MicrosoftOffice.txt
     export CATFILE="MicrosoftOffice.txt"
     export CATNAME="Microsoft Office"
     $CATEGORIZE
+    rm MicrosoftOffice.txt
+    unset OFFICE
   fi
 
-  if [ $ACCESS -ne 0 ];
+  if [ "$ACCESS" != "" ];
   then
+    egrep -i "$KEYWORDS_MICROSOFTACCESS" newpages.txt > MicrosoftAccess.txt
     export CATFILE="MicrosoftAccess.txt"
     export CATNAME="Microsoft Access"
     $CATEGORIZE
+    rm MicrosoftAccess.txt
+    unset ACCESS
   fi
 
-  if [ $EXCEL -ne 0 ];
+  if [ "$EXCEL" != "" ];
   then
+    egrep -i "$KEYWORDS_MICROSOFTEXCEL" newpages.txt | egrep -iv "$KEYWORDS_MICROSOFTEXCEL_EXCLUDE" > MicrosoftExcel.txt
     export CATFILE="MicrosoftExcel.txt"
     export CATNAME="Microsoft Excel"
     $CATEGORIZE
+    rm MicrosoftExcel.txt
+    unset EXCEL
   fi
 
-  if [ $POWERPOINT -ne 0 ];
+  if [ "$POWERPOINT" != "" ];
   then
+    egrep -i "$KEYWORDS_MICROSOFTPOWERPOINT" newpages.txt > MicrosoftPowerPoint.txt
     export CATFILE="MicrosoftPowerPoint.txt"
     export CATNAME="Microsoft PowerPoint"
     $CATEGORIZE
+    rm MicrosoftPowerPoint.txt
+    unset POWERPOINT
   fi
 
-  if [ $WORD -ne 0 ];
+  if [ "$WORD" != "" ];
   then
+    egrep -i "$KEYWORDS_MICROSOFTWORD" newpages.txt > MicrosoftWord.txt
     export CATFILE="MicrosoftWord.txt"
     export CATNAME="Microsoft Word"
     $CATEGORIZE
+    rm MicrosoftWord,txt
+    unset WORD
   fi
-
-  rm MicrosoftOffice.txt
-  rm MicrosoftAccess.txt
-  rm MicrosoftExcel.txt
-  rm MicrosoftPowerPoint.txt
-  rm MicrosoftWord.txt
 
 fi
