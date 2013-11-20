97a03178814ba7272820eb2447c87a5dd286f1a8
I am not me <impeccabletiming@gmail.com>
2013-11-20 18:46:35 -0500

    Adding Mongolia

--- catscripts/Government/Countries/Countries.sh
+++ catscripts/Government/Countries/Countries.sh
@@ -76,6 +76,7 @@ $CURRENTDIR/Madagascar/Madagascar.sh
 $CURRENTDIR/Malaysia/Malaysia.sh
 $CURRENTDIR/Mali/Mali.sh
 $CURRENTDIR/Mexico/Mexico.sh
+$CURRENTDIR/Mongolia/Mongolia.sh
 $CURRENTDIR/Morocco/Morocco.sh
 $CURRENTDIR/Namibia/Namibia.sh
 $CURRENTDIR/Nepal/Nepal.sh
---
+++ catscripts/Government/Countries/Mongolia/Mongolia.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+KEYWORDS_MONGOLIA="Mongolia"
+
+if [ "$1" == "" ]; #Normal operation
+then
+  
+  MONGOLIA=`egrep -i "$KEYWORDS_MONGOLIA" newpages.txt`
+
+  if [ "$MONGOLIA" != "" ];
+  then
+    egrep -i "$KEYWORDS_MONGOLIA" newpages.txt > Mongolia.txt
+    CATFILE="Mongolia.txt"
+    CATNAME="Mongolia"
+    $CATEGORIZE
+    rm Mongolia.txt
+    unset MONGOLIA
+  fi
+
+fi
