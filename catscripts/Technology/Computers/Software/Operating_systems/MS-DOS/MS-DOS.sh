6880f3246707df3f7b2cee91e15b27de440f0f47
I am not me <impeccabletiming@gmail.com>
2013-11-24 19:25:01 -0500

    Adding keywords to MS-DOS

--- catscripts/Technology/Computers/Software/Operating_systems/MS-DOS/MS-DOS.sh
+++ catscripts/Technology/Computers/Software/Operating_systems/MS-DOS/MS-DOS.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 KEYWORDS_MSDOS="\bMS(|-| )DOS|\bDOS\b"
+KEYWORDS_MSDOS_EXCLUDE="Giovani dos Santos"
+KEYWORDS_MSDOS_CASESENSITIVE_EXCLUDE="DoS"
 
 if [ "$1" == "" ]; #Normal operation
 then
 
-  MSDOS=`egrep -i "$KEYWORDS_MSDOS" newpages.txt`
+  MSDOS=`egrep -i "$KEYWORDS_MSDOS" newpages.txt | egrep -iv "$KEYWORDS_MSDOS_EXCLUDE" | egrep -v "$KEYWORDS_MSDOS_CASESENSITIVE_EXCLUDE"`
 
   if [ "$MSDOS" != "" ];
   then
-    egrep -i "$KEYWORDS_MSDOS" newpages.txt > MS-DOS.txt
+    egrep -i "$KEYWORDS_MSDOS" newpages.txt | egrep -iv "$KEYWORDS_MSDOS_EXCLUDE" | egrep -v "$KEYWORDS_MSDOS_CASESENSITIVE_EXCLUDE" > MS-DOS.txt
     export CATFILE="MS-DOS.txt"
     export CATNAME="MS-DOS"
     $CATEGORIZE
