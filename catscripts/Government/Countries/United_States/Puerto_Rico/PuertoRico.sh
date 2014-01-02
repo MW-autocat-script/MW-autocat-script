ad524806844403aaabca1dd75f75af9c6324bd63
I am not me <impeccabletiming@gmail.com>
2014-01-02 04:09:46 -0500

    Adding Puerto Rico

---
+++ catscripts/Government/Countries/United_States/Puerto_Rico/PuertoRico.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+KEYWORDS_PUERTORICO="Puerto(| )Rico|Puerto(| )Rican"
+
+if [ "$1" == "" ];
+then
+  
+  if [ "$DEBUG" == "yes" ];
+  then
+    printf "Starting Puerto Rico\n"
+  fi
+
+  PUERTORICO=`egrep -i "$KEYWORDS_PUERTORICO" newpages.txt`
+
+  if [ "$PUERTORICO" != "" ];
+  then
+    printf "$PUERTORICO" > PuertoRico.txt
+    export CATFILE="PuertoRico.txt"
+    export CATNAME="Puerto Rico"
+    $CATEGORIZE
+    rm PuertoRico.txt
+    unset PUERTORICO
+  fi
+
+  if [ "$DEBUG" == "yes" ];
+  then
+    printf "Finishing Puerto Rico\n"
+  fi
+
+fi
--- catscripts/Government/Countries/United_States/UnitedStates.sh
+++ catscripts/Government/Countries/United_States/UnitedStates.sh
@@ -9,6 +9,7 @@ CURRENTDIR="./catscripts/Government/Countries/United_States"
 $CURRENTDIR/US_military/USmilitary.sh
 $CURRENTDIR/US_Presidents/Presidents.sh
 $CURRENTDIR/US_states/States.sh
+$CURRENTDIR/Puerto_Rico/PuertoRico.sh
 
 if [ "$DEBUG" == "yes" ];
 then
