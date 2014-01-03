139bb6b474fb4d851424b7bdb02bd2c988c58755
I am not me <impeccabletiming@gmail.com>
2014-01-03 04:00:16 -0500

    Adding keywords to Washington

--- catscripts/Government/Countries/United_States/US_states/Washington/Washington.sh
+++ catscripts/Government/Countries/United_States/US_states/Washington/Washington.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 KEYWORDS_WASHINGTON="Washington"
-KEYWORDS_WASHINGTON_EXCLUDE="(George|General)(| )Washington|Washington(|,)(| )D(|\.)C(|\.)|District(| )of(| )columbia"
+KEYWORDS_WASHINGTON_EXCLUDE="(George|General|President|Martha|King|Abraham)(| )Washington|Washington(|,)(| )D(|\.)C(|\.)|District(| )of(| )columbia|Washington(| )Parkway|Denzel(| )Washington|Booker(| )T(|\.)(| )Washington|Washington(| )Post|Washington(| )Redskins|Washington(| )Monument|Washington(| )Capitals|FBI_Washington|March(| )on(| )Washington|Washington(| )Irving"
 
 if [ "$1" == "" ];
 then
