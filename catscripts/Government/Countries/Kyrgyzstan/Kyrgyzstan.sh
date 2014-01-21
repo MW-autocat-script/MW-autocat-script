550709a2077350111e363f09e2ea8a874773ce23
I am not me <impeccabletiming@gmail.com>
2014-01-20 20:05:48 -0500

    Adding Kyrgyzstan to categories

---
+++ catscripts/Government/Countries/Kyrgyzstan/Kyrgyzstan.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+KEYWORDS_KYRGYZSTAN="Kyrgyzstan"
+KEYWORDS_KYRGYZSTAN_ALL="$KEYWORDS_KYRGYZSTAN"
+
+if [ "$1" == "" ]; #Normal operation
+then
+  
+  if [ "$DEBUG" == "yes" ];
+  then
+    printf "Starting Kyrgyzstan\n"
+  fi
+
+  KYRGYZSTAN=`egrep -i "$KEYWORDS_KYRGYZSTAN" newpages.txt`
+
+  if [ "$KYRGYZSTAN" != "" ];
+  then
+    printf "$KYRGYZSTAN" > Kyrgyzstan.txt
+    export CATFILE="Kyrgyzstan.txt"
+    export CATNAME="Kyrgyzstan"
+    $CATEGORIZE
+    rm Kyrgyzstan.txt
+    unset KYRGYZSTAN
+  fi
+
+  if [ "$DEBUG" == "yes" ];
+  then
+    printf "Finishing Kyrgyzstan\n"
+  fi
+
+fi
