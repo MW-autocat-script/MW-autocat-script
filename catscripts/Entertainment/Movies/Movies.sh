dd7ac7c6308b5f58f3b97e1fbf2d3a70553c362a
I am not me <impeccabletiming@gmail.com>
2014-05-11 16:14:52 -0400

    Adding Shrek

--- catscripts/Entertainment/Movies/Movies.sh
+++ catscripts/Entertainment/Movies/Movies.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-CURRENTDIR="./catscripts/Entertainment/Movies"
+MOVIESDIR="./catscripts/Entertainment/Movies"
 
-$CURRENTDIR/Ghostbusters/Ghostbusters.sh
-$CURRENTDIR/Godzilla/Godzilla.sh
-$CURRENTDIR/Harry_Potter/HarryPotter.sh
-$CURRENTDIR/The_Hunger_Games/HungerGames.sh
-$CURRENTDIR/Jurassic_Park_movies/JurassicPark.sh
-$CURRENTDIR/The_Lion_King/TheLionKing.sh
-$CURRENTDIR/Star\ Wars\ movies/StarWarsMovies.sh
-$CURRENTDIR/Toy_Story_movies/ToyStory.sh
+$MOVIESDIR/Ghostbusters/Ghostbusters.sh
+$MOVIESDIR/Godzilla/Godzilla.sh
+$MOVIESDIR/Harry_Potter/HarryPotter.sh
+$MOVIESDIR/The_Hunger_Games/HungerGames.sh
+$MOVIESDIR/Jurassic_Park_movies/JurassicPark.sh
+$MOVIESDIR/The_Lion_King/TheLionKing.sh
+$MOVIESDIR/Shrek/Shrek.sh
+$MOVIESDIR/Star\ Wars\ movies/StarWarsMovies.sh
+$MOVIESDIR/Toy_Story_movies/ToyStory.sh
---
+++ catscripts/Entertainment/Movies/Shrek/Shrek.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+KEYWORDS_SHREK="\bShrek"
+
+if [ "$1" == "" ]; #Normal operation
+then
+  
+  debug_start "Shrek"
+
+  SHREK="$(egrep -i "$KEYWORDS_SHREK" newpages.txt)"
+
+  if [ "$SHREK" != "" ];
+  then
+    printf "%s" "$SHREK" > Shrek.txt
+    export CATFILE="Shrek.txt"
+    export CATNAME="Shrek"
+    $CATEGORIZE
+    rm Shrek.txt
+    unset SHREK
+  fi
+
+fi
