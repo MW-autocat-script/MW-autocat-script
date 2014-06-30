8507662f7125d40f9910c08088ecdcc5c4603ed0
I am not me <impeccabletiming@gmail.com>
2014-06-30 04:26:49 -0400

    Updating Pets

--- catscripts/Lifestyle/Pets/Pets.sh
+++ catscripts/Lifestyle/Pets/Pets.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-if [ "$DEBUG" == "yes" ];
-then
-  printf "Starting Pets\n" 
-fi
-#Dogs keywords
 
-. ./catscripts/Science/Biology/Animals/Vertebrates/Reptiles/Reptiles.sh norun #Import variables from Reptiles
+#Dogs keywords
 
 KEYWORDS_DOGS_TOY="Affenpinscher|Australian(| )Silky(| )Terrier|Spaniel|Chihuahua|Chinese(| )Crested(| )Dog|Griffon(| )Bruxellois|Havanese|Italian(| )Greyhound|Japanese(| )Chin|King(| )Charles(| )Spaniel|Maltese|Miniature(| )Pinscher|Papillon|Pekingese|Pomeranian|\bPug(|s)\b|Shih(| )Tzu|Toy(| )Fox(| )Terrier|Toy(| )Manchester(| )Terrier|Yorkshire(| )Terrier|\bYorkie|Manchester(| )Terrier"
 KEYWORDS_DOGS_WORKING="Malamute|\bAkita|Anatolian Shepherd|Shepherd dog|Sheep(| )dog|Bernese(| )Mountain(| )Dog|Black(| )Russian(| )Terrier|\bBoxer\b|mastiff|Cane(| )Corso|Doberman|Dogue(| )de(| )Bordeaux|German(| )Pinscher|Schnauzer|Great(| )Dane|Greater(| )Swiss(| )Mountain(| )Dog|Komondor|Kuvasz|Leonberger|Newfoundland (dog|puppy)|my Newfoundland|Portuguese( |)Water(| )Dog|Rottweiler|Samoyed|Siberian(| )Husky|(Saint|St(|\.))(| )Bernard"
@@ -31,71 +26,79 @@ KEYWORDS_GERBILS="Gerbil"
 KEYWORDS_GUINEA_PIGS="Guinea(| )pig"
 
 
-egrep -i "$KEYWORDS_DOGS" newpages.txt | egrep -iv "$KEYWORDS_DOGS_EXCLUDE" >> Dogs.txt #Search dogs
-egrep -i "$KEYWORDS_GOLDFISH" newpages.txt | egrep -iv "$KEYWORDS_GOLDFISH_EXCLUDE" >> Goldfish.txt #Search goldfish
-egrep -i "$KEYWORDS_HAMSTERS" newpages.txt >> Hamsters.txt #Search hamsters
-egrep -i "$KEYWORDS_GERBILS" newpages.txt >> Gerbils.txt #search gerbils
-egrep -i "$KEYWORDS_GUINEA_PIGS" newpages.txt >> GuineaPigs.txt #Search guinea pigs
-
-DOGS=$(stat --print=%s Dogs.txt) #stat dogs
-GOLDFISH=$(stat --print=%s Goldfish.txt) #stat goldfish
-HAMSTERS=$(stat --print=%s Hamsters.txt) #stat hamsters
-GERBILS=$(stat --print=%s Gerbils.txt) #stat gerbils
-GUINEAPIGS=$(stat --print=%s GuineaPigs.txt) #stat Guinea pigs
-
-#Categorize dogs
-
-if [ $DOGS -ne 0 ];
+if [ "$1" == "" ]; #Normal operation
 then
-  export CATFILE="Dogs.txt"
-  export CATNAME="Dogs"
-  $CATEGORIZE
-fi
 
-#Categorize goldfish
+  debug_start "Pets"
 
-if [ $GOLDFISH -ne 0 ];
-then
-  export CATFILE="Goldfish.txt"
-  export CATNAME="Goldfish"
-  $CATEGORIZE
-fi
+  . ./catscripts/Science/Biology/Animals/Vertebrates/Reptiles/Reptiles.sh norun #Import variables from Reptiles
 
-#Categorize hamsters
+  DOGS=$(egrep -i "$KEYWORDS_DOGS" newpages.txt | egrep -iv "$KEYWORDS_DOGS_EXCLUDE") 
+  GOLDFISH=$(egrep -i "$KEYWORDS_GOLDFISH" newpages.txt | egrep -iv "$KEYWORDS_GOLDFISH_EXCLUDE") 
+  HAMSTERS=$(egrep -i "$KEYWORDS_HAMSTERS" newpages.txt) 
+  GERBILS=$(egrep -i "$KEYWORDS_GERBILS" newpages.txt) 
+  GUINEAPIGS=$(egrep -i "$KEYWORDS_GUINEA_PIGS" newpages.txt) 
 
-if [ $HAMSTERS -ne 0 ];
-then
-  export CATFILE="Hamsters.txt"
-  export CATNAME="Hamsters"
-  $CATEGORIZE
-fi
+  #Categorize dogs
 
-#Categorize gerbils
+  if [ "$DOGS" != "" ];
+  then
+    printf "%s" "$DOGS" > Dogs.txt
+    export CATFILE="Dogs.txt"
+    export CATNAME="Dogs"
+    $CATEGORIZE
+    rm Dogs.txt
+    unset DOGS
+  fi
 
-if [ $GERBILS -ne 0 ];
-then
-  export CATFILE="Gerbils.txt"
-  export CATNAME="Gerbils"
-  $CATEGORIZE
-fi
+  #Categorize goldfish
 
-#Categorize Guinea pigs
+  if [ "$GOLDFISH" != "" ];
+  then
+    printf "%s" "$GOLDFISH" > Goldfish.txt
+    export CATFILE="Goldfish.txt"
+    export CATNAME="Goldfish"
+    $CATEGORIZE
+    rm Goldfish.txt
+    unset GOLDFISH
+  fi
 
-if [ $GUINEAPIGS -ne 0 ];
-then
-  export CATFILE="GuineaPigs.txt"
-  export CATNAME="Guinea pigs"
-  $CATEGORIZE
-fi
+  #Categorize hamsters
 
+  if [ "$HAMSTERS" != "" ];
+  then
+    printf "%s" "$HAMSTERS" > Hamsters.txt
+    export CATFILE="Hamsters.txt"
+    export CATNAME="Hamsters"
+    $CATEGORIZE
+    rm Hamsters.txt
+    unset HAMSTERS
+  fi
 
-rm Dogs.txt #cleanup dogs
-rm Goldfish.txt #Cleanup goldfish
-rm Hamsters.txt #cleanup hamsters
-rm Gerbils.txt #cleanup Gerbils
-rm GuineaPigs.txt #Cleanup Guinea pigs
+  #Categorize gerbils
 
-if [ "$DEBUG" == "yes" ];
-then
-  printf "Finishing pets\n" 
+  if [ "$GERBILS" != "" ];
+  then
+    printf "%s" "$GERBILS" > Gerbils.txt
+    export CATFILE="Gerbils.txt"
+    export CATNAME="Gerbils"
+    $CATEGORIZE
+    rm Gerbils.txt
+    unset GERBILS
+  fi
+
+  #Categorize Guinea pigs
+
+  if [ "$GUINEAPIGS" != "" ];
+  then
+    printf "%s" "$GUINEAPIGS" > GuineaPigs.txt
+    export CATFILE="GuineaPigs.txt"
+    export CATNAME="Guinea pigs"
+    $CATEGORIZE
+    rm GuineaPigs.txt
+    unset GUINEAPIGS
+  fi
+
+  debug_end "Pets"
+
 fi
