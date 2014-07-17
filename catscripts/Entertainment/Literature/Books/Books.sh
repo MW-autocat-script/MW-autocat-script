8e4b826bd2f64e4c93ceb71c3033ff6f2c1d2eea
I am not me <impeccabletiming@gmail.com>
2014-07-17 18:51:45 -0400

    Adding 'To Kill a Mockingbird'

--- catscripts/Entertainment/Literature/Books/Books.sh
+++ catscripts/Entertainment/Literature/Books/Books.sh
@@ -10,6 +10,7 @@ then
   . $BOOKSDIR/Lord_of_the_Flies/LordoftheFlies.sh #KEYWORDS_LORDOFTHEFLIES
   . $BOOKSDIR/Redwall/Redwall.sh #KEYWORDS_REDWALL
   . $BOOKSDIR/The_Giver/TheGiver.sh #KEYWORDS_THEGIVER
+  . $BOOKSDIR/To_Kill_a_Mockingbird/ToKillAMockingbird.sh #KEYWORDS_TOKILLAMOCKINGBIRD
 
   debug_end "Books"
 
---
+++ catscripts/Entertainment/Literature/Books/To_Kill_a_Mockingbird/ToKillAMockingbird.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+KEYWORDS_TOKILLAMOCKINGBIRD="To(| )Kill(| )A(| )Mocking(| )bird|Atticus.+(Jem|Scout)|Jem\b.+(Scout|Atticus)|Scout.+(Atticus|Jem)"
+
+if [ "$1" == "" ];
+then
+
+  debug_start "To Kill a Mockingbird"
+
+  MOCKINGBIRD=$(egrep -i "$KEYWORDS_TOKILLAMOCKINGBIRD" newpages.txt)
+
+  if [ "$MOCKINGBIRD" != "" ];
+  then
+    printf "%s" "$MOCKINGBIRD" > Mockingbird.txt
+    export CATFILE="Mockingbird.txt"
+    export CATNAME="To Kill a Mockingbird"
+    $CATEGORIZE
+    rm Mockingbird.txt
+    unset MOCKINGBIRD
+  fi
+
+  debug_end "To Kill a Mockingbird"
+
+fi
