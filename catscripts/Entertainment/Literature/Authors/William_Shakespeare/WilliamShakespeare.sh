8bb3e62e41ac2b71cffdb7990b4fc296ad4fe3bd
I am not me <impeccabletiming@gmail.com>
2014-07-17 21:29:24 -0400

    Adding Twelfth Night, Titus Andronicus, The Taming of the Shrew, The Merchant of Venice, Much Ado About Nothing, and As You Like It

--- catscripts/Entertainment/Literature/Authors/William_Shakespeare/WilliamShakespeare.sh
+++ catscripts/Entertainment/Literature/Authors/William_Shakespeare/WilliamShakespeare.sh
@@ -4,12 +4,19 @@ KEYWORDS_WILLIAMSHAKESPEARE="Shake(| )spear(|e)"
 KEYWORDS_ROMEOANDJULIET="\bRomeo(|')(|s)\b|Juliet(|'s)(|s)|Montague|Capulet|(Friar|Father)(| )(Laurence|Lawrence)"
 KEYWORDS_ROMEOANDJULIET_EXCLUDE="Rune(| )Scape|Juliet(| )(Butler|Burke)|Alfa(| )Romeo|Romeo(| )(Santos|Beckham)|Louis(| )Juliet|on(| )Lost|Unseen(| )Academicals|wizards(| )of(| )waverly(| )place"
 KEYWORDS_MACBETH="Macbeth"
-KEYWORDS_KINGLEAR="King(| )Lear"
+KEYWORDS_KINGLEAR="\bKing(| )Lear"
 KEYWORDS_HAMLET="Hamlet"
 KEYWORDS_OTHELLO="Othello"
 KEYWORDS_MIDSUMMERNIGHT="Mid(| )summer(| )Night"
 KEYWORDS_THETEMPEST="The(| )Tempest"
-KEYWORDS_WILLIAMSHAKESPEARE_EXCLUDE="$KEYWORDS_HAMLET|$KEYWORDS_KINGLEAR|$KEYWORDS_MACBETH|$KEYWORDS_MIDSUMMERNIGHT|$KEYWORDS_OTHELLO|$KEYWORDS_ROMEOANDJULIET|$KEYWORDS_THETEMPEST"
+KEYWORDS_MUCHADO="Much(| )Ado(| )About(| )Nothing"
+KEYWORDS_CORIOLANUS="Coriolanus"
+KEYWORDS_TAMINGSHREW="Taming(| )of(| )the(| )Shrew"
+KEYWORDS_ASYOULIKEIT="As(| )You(| )Like(| )It"
+KEYWORDS_TITUSANDRONICUS="Titus(| )Andronicus"
+KEYWORDS_MERCHANTVENICE="Merchant(| )of(| )Venice"
+KEYWORDS_TWELFTHNIGHT="(12th|Twelfth)(| )Night"
+KEYWORDS_WILLIAMSHAKESPEARE_EXCLUDE="$KEYWORDS_HAMLET|$KEYWORDS_KINGLEAR|$KEYWORDS_MACBETH|$KEYWORDS_MIDSUMMERNIGHT|$KEYWORDS_OTHELLO|$KEYWORDS_ROMEOANDJULIET|$KEYWORDS_THETEMPEST|$KEYWORDS_MUCHADO|$KEYWORDS_CORIOLANUS|$KEYWORDS_TAMINGSHREW|$KEYWORDS_ASYOULIKEIT|$KEYWORDS_TITUSANDRONICUS|$KEYWORDS_MERCHANTVENICE|$KEYWORDS_TWELFTHNIGHT"
 
 if [ "$1" == "" ];
 then
@@ -24,6 +31,12 @@ then
   OTHELLO=$(egrep -i "$KEYWORDS_OTHELLO" newpages.txt)
   MIDSUMMER=$(egrep -i "$KEYWORDS_MIDSUMMERNIGHT" newpages.txt)
   TEMPEST=$(egrep -i "$KEYWORDS_THETEMPEST" newpages.txt)
+  ASYOULIKEIT=$(egrep -i "$KEYWORDS_ASYOULIKEIT" newpages.txt)
+  MUCHADO=$(egrep -i "$KEYWORDS_MUCHADO" newpages.txt)
+  MERCHANT=$(egrep -i "$KEYWORDS_MERCHANTVENICE" newpages.txt)
+  SHREW=$(egrep -i "$KEYWORDS_TAMINGSHREW" newpages.txt)
+  TWELFTH=$(egrep -i "$KEYWORDS_TWELFTHNIGHT" newpages.txt)
+  TITUS=$(egrep -i "$KEYWORDS_TITUSANDRONICUS" newpages.txt)
 
   if [ "$SHAKESPEARE" != "" ];
   then
@@ -103,6 +116,66 @@ then
     $CATEGORIZE
     rm Tempest.txt
     unset TEMPEST
+  fi
+
+  if [ "$ASYOULIKEIT" != "" ];
+  then
+    printf "%s" "$ASYOULIKEIT" > AsYouLikeIt.txt
+    export CATFILE="AsYouLikeIt.txt"
+    export CATNAME="As You Like It"
+    $CATEGORIZE
+    rm AsYouLikeIt.txt
+    unset ASYOULIKEIT
+  fi
+
+  if [ "$MUCHADO" != "" ];
+  then
+    printf "%s" "$MUCHADO" > MuchAdo.txt
+    export CATFILE="MuchAdo.txt"
+    export CATNAME="Much Ado About Nothing"
+    $CATEGORIZE
+    rm MuchAdo.txt
+    unset MUCHADO
+  fi
+
+  if [ "$MERCHANT" != "" ];
+  then
+    printf "%s" "$MERCHANT" > Merchant.txt
+    export CATFILE="Merchant.txt"
+    export CATNAME="The Merchant of Venice"
+    $CATEGORIZE
+    rm Merchant.txt
+    unset MERCHANT
+  fi
+  
+  if [ "$SHREW" != "" ];
+  then
+    printf "%s" "$SHREW" > Shrew.txt
+    export CATFILE="Shrew.txt"
+    export CATNAME="The Taming of the Shrew"
+    $CATEGORIZE
+    rm Shrew.txt
+    unset SHREW
+  fi
+
+  if [ "$TWELFTH" != "" ];
+  then
+    printf "%s" "$TWELFTH" > Twelfth.txt
+    export CATFILE="Twelfth.txt"
+    export CATNAME="Twelfth Night"
+    $CATEGORIZE
+    rm Twelfth.txt
+    unset TWELFTH
+  fi
+
+  if [ "$TITUS" != "" ];
+  then
+    printf "%s" "$TITUS" > Titus.txt
+    export CATFILE="Titus.txt"
+    export CATNAME="Titus Andronicus"
+    $CATEGORIZE
+    rm Titus.txt
+    unset TITUS
   fi
 
   debug_end "William Shakespeare"
