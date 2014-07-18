09d10d05085222cbd325db39bc391ccc51aac31a
I am not me <impeccabletiming@gmail.com>
2014-07-18 16:02:23 -0400

    Adding Charles Dickens

--- catscripts/Entertainment/Literature/Authors/Authors.sh
+++ catscripts/Entertainment/Literature/Authors/Authors.sh
@@ -4,6 +4,7 @@ debug_start "Authors"
 
 AUTHORSDIR="./catscripts/Entertainment/Literature/Authors"
 
+  . $AUTHORSDIR/Charles_Dickens/CharlesDickens.sh #KEYWORDS_CHARLESDICKENS
   . $AUTHORSDIR/Dr_Seuss/DrSeuss.sh #KEYWORDS_DRSEUSS
   . $AUTHORSDIR/J_K_Rowling/JKRowling.sh #KEYWORDS_JKROWLING
   . $AUTHORSDIR/Lewis_Carroll/LewisCarroll.sh #KEYWORDS_LEWISCARROLL
---
+++ catscripts/Entertainment/Literature/Authors/Charles_Dickens/CharlesDickens.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+KEYWORDS_CHARLESDICKENS="Charles(| )Dickens"
+
+if [ "$1" == "" ];
+then
+  
+  debug_start "Charles Dickens"
+
+  DICKENS=$(egrep -i "$KEYWORDS_CHARLESDICKENS" newpages.txt)
+
+  if [ "$DICKENS" != "" ];
+  then
+    printf "%s" "$DICKENS" > Dickens.txt
+    export CATFILE="Dickens.txt"
+    export CATNAME="Charles Dickens"
+    $CATEGORIZE
+    rm Dickens.txt
+    unset DICKENS
+  fi
+
+  debug_end "Charles Dickens"
+
+fi
