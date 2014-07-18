ec1c26389837728b67806a9fd1e3b2add318979d
I am not me <impeccabletiming@gmail.com>
2014-07-17 21:06:39 -0400

    Adding Romeon and Juliet, Macbeth, King Lear, Hamlet, Othello, A Midsummer Night's Dream, and The Tempest

--- catscripts/Entertainment/Literature/Authors/William_Shakespeare/WilliamShakespeare.sh
+++ catscripts/Entertainment/Literature/Authors/William_Shakespeare/WilliamShakespeare.sh
@@ -17,6 +17,13 @@ then
   debug_start "William Shakespeare"
 
   SHAKESPEARE=$(egrep -i "$KEYWORDS_WILLIAMSHAKESPEARE" newpages.txt | egrep -iv "$KEYWORDS_WILLIAMSHAKESPEARE_EXCLUDE")
+  ROMEOANDJULIET=$(egrep -i "$KEYWORDS_ROMEOANDJULIET" newpages.txt | egrep -iv "$KEYWORDS_ROMEOANDJULIET_EXCLUDE")
+  MACBETH=$(egrep -i "$KEYWORDS_MACBETH" newpages.txt)
+  KINGLEAR=$(egrep -i "$KEYWORDS_KINGLEAR" newpages.txt)
+  HAMLET=$(egrep -i "$KEYWORDS_HAMLET" newpages.txt)
+  OTHELLO=$(egrep -i "$KEYWORDS_OTHELLO" newpages.txt)
+  MIDSUMMER=$(egrep -i "$KEYWORDS_MIDSUMMERNIGHT" newpages.txt)
+  TEMPEST=$(egrep -i "$KEYWORDS_THETEMPEST" newpages.txt)
 
   if [ "$SHAKESPEARE" != "" ];
   then
@@ -26,6 +33,76 @@ then
     $CATEGORIZE
     rm Shakespeare.txt
     unset SHAKESPEARE
+  fi
+
+  if [ "$ROMEOANDJULIET" != "" ];
+  then
+    printf "%s" "$ROMEOANDJULIET" > RomeoandJuliet.txt
+    export CATFILE="RomeoandJuliet.txt"
+    export CATNAME="Romeo and Juliet"
+    $CATEGORIZE
+    rm RomeoandJuliet.txt
+    unset ROMEOANDJULIET
+  fi
+
+  if [ "$MACBETH" != "" ];
+  then
+    printf "%s" "$MACBETH" > Macbeth.txt
+    export CATFILE="Macbeth.txt"
+    export CATNAME="Macbeth"
+    $CATEGORIZE
+    rm Macbeth.txt
+    unset MACBETH
+  fi
+
+  if [ "$KINGLEAR" != "" ];
+  then
+    printf "%s" "$KINGLEAR" > KingLear.txt
+    export CATFILE="KingLear.txt"
+    export CATNAME="King Lear"
+    $CATEGORIZE
+    rm KingLear.txt
+    unset KINGLEAR
+  fi
+
+  if [ "$HAMLET" != "" ];
+  then
+    printf "%s" "$HAMLET" > Hamlet.txt
+    export CATFILE="Hamlet.txt"
+    export CATNAME="Hamlet"
+    $CATEGORIZE
+    rm Hamlet.txt
+    unset HAMLET
+  fi
+
+  if [ "$OTHELLO" != "" ];
+  then
+    printf "%s" "$OTHELLO" > Othello.txt
+    export CATFILE="Othello.txt"
+    export CATNAME="Othello"
+    $CATEGORIZE
+    rm Othello.txt
+    unset OTHELLO
+  fi
+
+  if [ "$MIDSUMMER" != "" ];
+  then
+    printf "%s" "$MIDSUMMER" > Midsummer.txt
+    export CATFILE="Midsummer.txt"
+    export CATNAME="A Midsummer Night's Dream"
+    $CATEGORIZE
+    rm Midsummer.txt
+    unset MIDSUMMER
+  fi
+
+  if [ "$TEMPEST" != "" ];
+  then
+    printf "%s" "$TEMPEST" > Tempest.txt
+    export CATFILE="Tempest.txt"
+    export CATNAME="The Tempest"
+    $CATEGORIZE
+    rm Tempest.txt
+    unset TEMPEST
   fi
 
   debug_end "William Shakespeare"
