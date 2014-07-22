95ef46c5e8e1887a8dbb4ac56d4af0569bd0967d
I am not me <impeccabletiming@gmail.com>
2014-07-22 07:28:59 -0400

    Adding M16 and AK-47

---
+++ catscripts/Lifestyle/Hobbies/Firearms/Firearms.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+KEYWORDS_FIREARMS="Fire(| )arm"
+KEYWORDS_AK47="AK(| |-)47"
+KEYWORDS_M16="\bM16(|s)\b|(|X)M16(a|e)[1-5]|AR(|-)15"
+KEYWORDS_FIREARMS_ALL="$KEYWORDS_FIREARMS|$KEYWORDS_AK47|$KEYWORDS_M16"
+
+if [ "$1" == "" ];
+then
+  
+  debug_start "Firearms"
+
+  AK47=$(egrep -i "$KEYWORDS_AK47" newpages.txt)
+  M16=$(egrep -i "$KEYWORDS_M16" newpages.txt)
+
+  if [ "$AK47" != "" ];
+  then
+    printf "%s" "$AK47" > AK47.txt
+    export CATFILE="AK47.txt"
+    export CATNAME="AK-47"
+    $CATEGORIZE
+    rm AK47.txt
+    unset AK47
+  fi
+
+  if [ "$M16" != "" ];
+  then
+    printf "%s" "$M16" > M16.txt
+    export CATFILE="M16.txt"
+    export CATNAME="M16"
+    $CATEGORIZE
+    rm M16.txt
+    unset M16
+  fi
+
+  debug_end "Firearms"
+
+fi
--- catscripts/Lifestyle/Hobbies/Hobbies.sh
+++ catscripts/Lifestyle/Hobbies/Hobbies.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-. ./catscripts/Technology/Cameras/Cameras.sh norun #KEYWORDS_CAMERAS_ALL
-
 KEYWORDS_HOBBIES="Hobbies|hobby"
 KEYWORDS_SEWING="\bsew(|ing)\b|embroidery"
 KEYWORDS_KNITTING="\bknit(|ting)(|s)\b|crochet"
@@ -9,12 +7,16 @@ KEYWORDS_SEWING_EXCLUDE="$KEYWORDS_KNITTING"
 KEYWORDS_PHOTOGRAPHY="Photography"
 KEYWORDS_PHOTOGRAPHY_EXCLUDE="$KEYWORDS_CAMERAS_ALL"
 KEYWORDS_BASEBALLCARDS="baseball(| )card"
-KEYWORDS_HOBBIES_EXCLUDE="$KEYWORDS_SEWING|$KEYWORDS_KNITTING|$KEYWORDS_PHOTOGRAPHY|$KEYWORDS_BASEBALLCARDS"
 
 if [ "$1" == "" ]; #Normal operation
 then
 
   debug_start "Hobbies"
+
+  . ./catscripts/Technology/Cameras/Cameras.sh norun #KEYWORDS_CAMERAS_ALL
+  . ./catscripts/Lifestyle/Hobbies/Firearms/Firearms.sh #KEYWORDS_FIREARMS_ALL
+
+  KEYWORDS_HOBBIES_EXCLUDE="$KEYWORDS_SEWING|$KEYWORDS_KNITTING|$KEYWORDS_PHOTOGRAPHY|$KEYWORDS_BASEBALLCARDS|$KEYWORDS_CAMERAS_ALL|$KEYWORDS_FIREARMS_ALL"
 
   HOBBIES=$(egrep -i "$KEYWORDS_HOBBIES" newpages.txt | egrep -iv "$KEYWORDS_HOBBIES_EXCLUDE")
   SEWING=$(egrep -i "$KEYWORDS_SEWING" newpages.txt | egrep -iv "$KEYWORDS_SEWING_EXCLUDE")
@@ -73,5 +75,12 @@ then
   fi
 
   debug_end "Hobbies"
+
+else
+
+  . ./catscripts/Technology/Cameras/Cameras.sh norun #KEYWORDS_CAMERAS_ALL
+  . ./catscripts/Lifestyle/Hobbies/Firearms/Firearms.sh norun #KEYWORDS_FIREARMS_ALL
+
+  KEYWORDS_HOBBIES_EXCLUDE="$KEYWORDS_SEWING|$KEYWORDS_KNITTING|$KEYWORDS_PHOTOGRAPHY|$KEYWORDS_BASEBALLCARDS|$KEYWORDS_CAMERAS_ALL|$KEYWORDS_FIREARMS_ALL"
 
 fi
