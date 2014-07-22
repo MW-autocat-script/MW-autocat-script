41411b661425fc93e94bd64ca06b4c841fe03fdc
I am not me <impeccabletiming@gmail.com>
2014-07-22 17:44:21 -0400

    Adding Firearms

--- catscripts/Lifestyle/Hobbies/Firearms/Firearms.sh
+++ catscripts/Lifestyle/Hobbies/Firearms/Firearms.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-KEYWORDS_FIREARMS="Fire(| )arm"
+KEYWORDS_FIREARMS="\bFire(| )arm"
 KEYWORDS_AK47="AK(| |-)47"
 KEYWORDS_M16="\bM16(|s)\b|(|X)M16(a|e)[1-5]|AR(|-)15"
+KEYWORDS_FIREARMS_EXCLUDE="$KEYWORDS_AK47|$KEYWORDS_M16"
 KEYWORDS_FIREARMS_ALL="$KEYWORDS_FIREARMS|$KEYWORDS_AK47|$KEYWORDS_M16"
 
 if [ "$1" == "" ];
@@ -10,8 +11,20 @@ then
   
   debug_start "Firearms"
 
+  FIREARMS=$(egrep -i "$KEYWORDS_FIREARMS" newpages.txt| egrep -iv "$KEYWORDS_FIREARMS_EXCLUDE")
   AK47=$(egrep -i "$KEYWORDS_AK47" newpages.txt)
   M16=$(egrep -i "$KEYWORDS_M16" newpages.txt)
+
+  if [ "$FIREARMS" != "" ];
+  then
+    printf "%s" "$FIREARMS" > Firearms.txt
+    export CATFILE="Firearms.txt"
+    export CATNAME="Firearms"
+    $CATEGORIZE
+    rm Firearms.txt
+    unset FIREARMS
+  fi
+    
 
   if [ "$AK47" != "" ];
   then
