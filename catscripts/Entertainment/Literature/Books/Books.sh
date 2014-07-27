27c72bdc812819302c0e8cdb99086c0261fd5698
I am not me <impeccabletiming@gmail.com>
2014-07-26 20:31:03 -0400

    Adding Percy Jackson and the Olympians

--- catscripts/Entertainment/Literature/Books/Books.sh
+++ catscripts/Entertainment/Literature/Books/Books.sh
@@ -11,6 +11,7 @@ then
   . $BOOKSDIR/Black_Beauty/BlackBeauty.sh #KEYWORDS_BLACKBEAUTY
   . $BOOKSDIR/Lord_of_the_Flies/LordoftheFlies.sh #KEYWORDS_LORDOFTHEFLIES
   . $BOOKSDIR/Moby_Dick/MobyDick.sh #KEYWORDS_MOBYDICK
+  . $BOOKSDIR/Percy_Jackson/PercyJackson.sh #KEYWORDS_PERCYJACKSON
   . $BOOKSDIR/Redwall/Redwall.sh #KEYWORDS_REDWALL
   . $BOOKSDIR/The_39_Clues/The39Clues.sh #KEYWORDS_THE39CLUES
   . $BOOKSDIR/The_Giver/TheGiver.sh #KEYWORDS_THEGIVER
---
+++ catscripts/Entertainment/Literature/Books/Percy_Jackson/PercyJackson.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+KEYWORDS_PERCYJACKSON="Percy(| )Jackson"
+
+if [ "$1" == "" ];
+then
+  
+  debug_start "Percy Jackson and the Olympians"
+
+  PERCY=$(egrep -i "$KEYWORDS_PERCYJACKSON" newpages.txt)
+
+  if [ "$PERCY" != "" ];
+  then
+    printf "%s" "$PERCY" > PercyJackson.txt
+    export CATFILE="PercyJackson.txt"
+    export CATNAME="Percy Jackson and the Olympians"
+    $CATEGORIZE
+    rm PercyJackson.txt
+    unset PERCY
+  fi
+
+  debug_end "Percy Jackson and the Olympians"
+
+fi
