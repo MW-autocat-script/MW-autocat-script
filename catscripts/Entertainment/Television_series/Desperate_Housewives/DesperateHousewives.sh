09d58c4e8428ea2b97318bd9d74bc8cb0081599a
I am not me <impeccabletiming@gmail.com>
2014-08-01 02:37:06 -0400

    Adding Desperate Housewives

---
+++ catscripts/Entertainment/Television_series/Desperate_Housewives/DesperateHousewives.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+KEYWORDS_DESPERATEHOUSEWIVES="Desperate(| )House(| |-)Wives"
+
+if [ "$1" == "" ];
+then
+  
+  debug_start "Desperate Housewives"
+
+  HOUSEWIVES=$(egrep -i "$KEYWORDS_DESPERATEHOUSEWIVES" newpages.txt)
+
+  if [ "$HOUSEWIVES" != "" ];
+  then
+    printf "%s" "$HOUSEWIVES" > DesperateHousewives.txt
+    export CATFILE="DesperateHousewives.txt"
+    export CATNAME="Desperate Housewives"
+    $CATEGORIZE
+    rm DesperateHousewives.txt
+    unset HOUSEWIVES
+  fi
+
+  debug_end "Desperate Housewives"
+
+fi
