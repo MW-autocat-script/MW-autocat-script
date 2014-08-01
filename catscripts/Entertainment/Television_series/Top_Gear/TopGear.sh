ecf6028d2124a7c66acd296340937b6af3feeb0d
I am not me <impeccabletiming@gmail.com>
2014-08-01 04:25:50 -0400

    Adding Top Gear

---
+++ catscripts/Entertainment/Television_series/Top_Gear/TopGear.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+KEYWORDS_TOPGEAR="Top(| )Gear|\bstig\b"
+
+if [ "$1" == "" ];
+then
+  
+  debug_start "Top Gear"
+
+  TOPGEAR=$(egrep -i "$KEYWORDS_TOPGEAR" newpages.txt)
+
+  if [ "$TOPGEAR" != "" ];
+  then
+    printf "%s" "$TOPGEAR" > TopGear.txt
+    export CATFILE="TopGear.txt"
+    export CATNAME="Top Gear"
+    $CATEGORIZE
+    rm TopGear.txt
+    unset TOPGEAR
+  fi
+
+  debug_end "Top Gear"
+
+fi
