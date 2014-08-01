19047c83c653b0f9373dc3b36155ffe588330068
I am not me <impeccabletiming@gmail.com>
2014-08-01 02:27:00 -0400

    Adding Smallville

---
+++ catscripts/Entertainment/Television_series/Smallville/Smallville.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+KEYWORDS_SMALLVILLE="Small(| )ville"
+
+if [ "$1" == "" ];
+then
+  
+  debug_start "Smallville"
+
+  SMALLVILLE=$(egrep -i "$KEYWORDS_SMALLVILLE" newpages.txt)
+
+  if [ "$SMALLVILLE" != "" ];
+  then
+    printf "%s" "$SMALLVILLE" > Smallville.txt
+    export CATFILE="Smallville.txt"
+    export CATNAME="Smallville"
+    $CATEGORIZE
+    rm Smallville.txt
+    unset SMALLVILLE
+  fi
+
+  debug_end "Smallville"
+
+fi
+    
