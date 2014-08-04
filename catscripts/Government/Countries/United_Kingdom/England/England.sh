645f5a413d7fa6f01d6b354b0007ff4e0ee82b41
I am not me <impeccabletiming@gmail.com>
2014-08-04 09:52:18 -0400

    Adding Elizabeth Tower

--- catscripts/Government/Countries/United_Kingdom/England/England.sh
+++ catscripts/Government/Countries/United_Kingdom/England/England.sh
@@ -3,8 +3,9 @@
 KEYWORDS_ENGLAND="England|British" #Since most people don't know the difference between British and English
 KEYWORDS_LONDON="London"
 KEYWORDS_BUCKINGHAMPALACE="Buckingham(| )Palace"
-KEYWORDS_LONDON_EXCLUDE="Jack(| )London|$KEYWORDS_BUCKINGHAMPALACE" #Jack London was an American author
-KEYWORDS_LONDON_ALL="$KEYWORDS_LONDON|$KEYWORDS_BUCKINGHAMPALACE"
+KEYWORDS_ELIZABETHTOWER="Elizabeth(| )Tower"
+KEYWORDS_LONDON_EXCLUDE="Jack(| )London|$KEYWORDS_BUCKINGHAMPALACE|$KEYWORDS_ELIZABETHTOWER" #Jack London was an American author
+KEYWORDS_LONDON_ALL="$KEYWORDS_LONDON|$KEYWORDS_BUCKINGHAMPALACE|$KEYWORDS_ELIZABETHTOWER"
 KEYWORDS_STONEHENGE="Stonehenge"
 KEYWORDS_ENGLAND_EXCLUDE="$KEYWORDS_LONDON_ALL|$KEYWORDS_STONEHENGE|British(| )Columbia'|New(| )England|Church(| )of(| )England|British(| )Isles"
 KEYWORDS_ENGLAND_ALL="$KEYWORDS_ENGLAND|$KEYWORDS_LONDON_ALL|$KEYWORDS_STONEHENGE"
@@ -17,6 +18,7 @@ then
   ENGLAND=$(egrep -i "$KEYWORDS_ENGLAND" newpages.txt | egrep -iv "$KEYWORDS_ENGLAND_EXCLUDE")
   LONDON=$(egrep -i "$KEYWORDS_LONDON" newpages.txt | egrep -iv "$KEYWORDS_LONDON_EXCLUDE")
   BUCKINGHAMPALACE=$(egrep -i "$KEYWORDS_BUCKINGHAMPALACE" newpages.txt)
+  ELIZABETHTOWER=$(egrep -i "$KEYWORDS_ELIZABETHTOWER" newpages.txt)
   STONEHENGE=$(egrep -i "$KEYWORDS_STONEHENGE" newpages.txt)
 
   if [ "$ENGLAND" != "" ];
@@ -47,6 +49,16 @@ then
     $CATEGORIZE
     rm BuckinghamPalace.txt
     unset BUCKINGHAMPALACE
+  fi
+
+  if [ "$ELIZABETHTOWER" != "" ];
+  then
+    printf "%s" "$ELIZABETHTOWER" > ElizabethTower.txt
+    export CATFILE="ElizabethTower.txt"
+    export CATNAME="Elizabeth Tower"
+    $CATEGORIZE
+    rm ElizabethTower.txt
+    unset ELIZABETHTOWER
   fi
 
   if [ "$STONEHENGE" != "" ];
