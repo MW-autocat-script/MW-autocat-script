81db45c3ec77fd4e2634b91c179f9950900c0b27
I am not me <impeccabletiming@gmail.com>
2014-08-10 20:13:50 -0400

    Adding Gambia

---
+++ catscripts/Government/Countries/Gambia/Gambia.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+KEYWORDS_GAMBIA="Gambia"
+KEYWORDS_GAMBIA_ALL="$KEYWORDS_GAMBIA"
+
+if [ "$1" == "" ];
+then
+
+  debug_start "Gambia"
+
+  GAMBIA=$(egrep -i "$GAMBIA" newpages.txt)
+
+  if [ "$GAMBIA" != "" ];
+  then
+    printf "%s" "$GAMBIA" > Gambia.txt
+    export CATFILE="Gambia.txt"
+    export CATNAME="Gambia"
+    $CATEGORIZE
+    rm Gambia.txt
+    unset GAMBIA
+  fi
+
+  debug_end "Gambia"
+
+fi
