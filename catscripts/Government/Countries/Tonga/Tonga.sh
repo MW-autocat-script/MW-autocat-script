a91e72b890f9afe66992a8212f847eaa3cd85616
I am not me <impeccabletiming@gmail.com>
2014-08-10 20:59:24 -0400

    Adding Tonga

---
+++ catscripts/Government/Countries/Tonga/Tonga.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+KEYWORDS_TONGA="\bTonga(|s)\b|Tongan"
+KEYWORDS_TONGA_ALL="$KEYWORDS_TONGA"
+
+if [ "$1" == "" ];
+then
+  
+  debug_start "Tonga" 
+
+  TONGA=$(egrep -i "$KEYWORDS_TONGA" newpages.txt)
+
+  if [ "$TONGA" != "" ];
+  then
+    printf "%s" "$TONGA" > Tonga.txt
+    export CATFILE="Tonga.txt"
+    export CATNAME="Tonga.txt"
+    $CATEGORIZE
+    rm Tonga.txt
+    unset TONGA
+  fi
+
+  debug_end "Tonga"
+
+fi
