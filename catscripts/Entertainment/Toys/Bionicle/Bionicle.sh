638727b7c88b99e66f94888cf87413ce39444482
I am not me <impeccabletiming@gmail.com>
2014-08-13 14:25:47 -0400

    Adding Bionicle

---
+++ catscripts/Entertainment/Toys/Bionicle/Bionicle.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+KEYWORDS_BIONICLE="Bionicle"
+KEYWORDS_BIONICLE_ALL="$KEYWORDS_BIONICLE"
+
+if [ "$1" == "" ];
+then
+  
+  debug_start "Bionicle"
+
+  BIONICLE=$(egrep -i "$KEYWORDS_BIONICLE" newpages.txt)
+
+  if [ "$BIONICLE" != "" ];
+  then
+    printf "%s" "$BIONICLE" > Bionicle.txt
+    export CATFILE="Bionicle.txt"
+    export CATNAME="Bionicle"
+    $CATEGORIZE
+    rm Bionicle.txt
+    unset BIONICLE
+  fi
+
+  debug_end "Bionicle"
+
+fi
--- catscripts/Entertainment/Toys/Toys.sh
+++ catscripts/Entertainment/Toys/Toys.sh
@@ -4,6 +4,7 @@ TOYDIR="./catscripts/Entertainment/Toys"
 
 debug_start "Toys"
 
+$TOYDIR/Bionicle/Bionicle.sh #KEYWORDS_BIONICLE_ALL
 $TOYDIR/Build-A-Bear/BuildABear.sh #KEYWORDS_BUILDABEAR
 $TOYDIR/Hello_Kitty/HelloKitty.sh #KEYWORDS_HELLOKITTY_ALL
 $TOYDIR/Hot_Wheels/HotWheels.sh #KEYWORDS_HOTWHEELS_ALL
