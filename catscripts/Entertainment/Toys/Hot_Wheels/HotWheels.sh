e5f2eb11f03dd38e36afbb728ffbc75ed8427673
I am not me <impeccabletiming@gmail.com>
2014-08-13 09:25:42 -0400

    Adding Hot Wheels

---
+++ catscripts/Entertainment/Toys/Hot_Wheels/HotWheels.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+KEYWORDS_HOTWHEELS="Hot(| )Wheels"
+KEYWORDS_HOTWHEELS_ALL="$KEYWORDS_HOTWHEELS"
+
+if [ "$1" == "" ];
+then
+  
+  debug_start "Hot Wheels"
+
+  HOTWHEELS=$(egrep -i "$KEYWORDS_HOTWHEELS" newpages.txt)
+
+  if [ "$HOTWHEELS" != "" ];
+  then
+    printf "%s" "$HOTWHEELS" > HotWheels.txt
+    export CATFILE="HotWheels.txt"
+    export CATNAME="Hot Wheels"
+    $CATEGORIZE
+    rm HotWheels.txt
+    unset HOTWHEELS
+  fi
+
+  debug_end "Hot Wheels"
+
+fi
--- catscripts/Entertainment/Toys/Toys.sh
+++ catscripts/Entertainment/Toys/Toys.sh
@@ -5,6 +5,7 @@ TOYDIR="./catscripts/Entertainment/Toys"
 debug_start "Toys"
 
 $TOYDIR/Build-A-Bear/BuildABear.sh #KEYWORDS_BUILDABEAR
+$TOYDIR/Hot_Wheels/HotWheels.sh #KEYWORDS_HOTWHEELS_ALL
 $TOYDIR/Tamagotchi/Tamagotchi.sh #KEYWORDS_TAMAGOTCHI
 
 debug_end "Toys"
