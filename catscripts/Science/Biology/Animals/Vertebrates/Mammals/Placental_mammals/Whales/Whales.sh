85f8c22cc048e390ea46a79e86b1c2ae0fe1c860
I am not me <impeccabletiming@gmail.com>
2014-10-21 21:08:19 -0400

    Adding Whales, Blue whales, and Humpback whales

---
+++ catscripts/Science/Biology/Animals/Vertebrates/Mammals/Placental_mammals/Whales/Whales.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+KEYWORDS_WHALES="Whale|baleen"
+KEYWORDS_BLUEWHALE="blue(| )whale"
+KEYWORDS_HUMPBACK="humpback"
+KEYWORDS_WHALES_EXCLUDE="$KEYWORDS_BLUEWHALE|$KEYWORDS_HUMPBACK|whale(| )shark|killer(| )whale"
+KEYWORDS_WHALES_ALL="$KEYWORDS_WHALES|$KEYWORDS_BLUEWHALE|$KEYWORDS_HUMPBACK"
+
+if [ "$1" == "" ];
+then
+  
+  debug_start "Whales"
+
+  WHALES=$(egrep -i "$KEYWORDS_WHALES" "$NEWPAGES" | egrep -iv "$KEYWORDS_WHALES_EXCLUDE")
+  BLUEWHALE=$(egrep -i "$KEYWORDS_BLUEWHALE" "$NEWPAGES")
+  HUMPBACK=$(egrep -i "$KEYWORDS_HUMPBACK" "$NEWPAGES")
+
+  categorize "WHALES" "Whales"
+  categorize "BLUEWHALES" "Blue whales"
+  categorize "HUMPBACK" "Humpback whales"
+
+  debug_end "Whales"
+
+fi
