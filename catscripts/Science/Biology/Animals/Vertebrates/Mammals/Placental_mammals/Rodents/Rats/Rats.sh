3dbb54fb856b9907dc01899be59d934e33f74504
I am not me <impeccabletiming@gmail.com>
2014-11-21 11:45:03 -0500

    Adding Rats

---
+++ catscripts/Science/Biology/Animals/Vertebrates/Mammals/Placental_mammals/Rodents/Rats/Rats.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+KEYWORDS_RATS="\brat(|s)\b"
+KEYWORDS_RATS_EXCLUDE="lab rats(|:)(| )virtual(| )(|battle)(| )trainer|Poptropica|Alxemy|Rune(| )Scape|Adventure(| )Quest|Final(| )Fantasy|rat(-| )tail(| )fish|[0-9]-letter|rat snake|Ian Hawk|rat(-| )tailed(| )maggot|(mole|womp|desert|kangaroo) rat|NIMH"
+KEYWORDS_RATS_ALL="$KEYWORDS_RATS"
+
+if [ "$1" == "" ];
+then
+
+  debug_start "Rats"
+
+  RATS=$(egrep -i "$KEYWORDS_RATS" "$NEWPAGES")
+
+  categorize "RATS" "Rats"
+
+  debug_end "Rats"
+
+fi
