60e29a4b58a2e3db6537229153783e880923c0a3
I am not me <impeccabletiming@gmail.com>
2014-11-20 21:29:47 -0500

    Adding Beavers

---
+++ catscripts/Science/Biology/Animals/Vertebrates/Mammals/Placental_mammals/Rodents/Beavers/Beavers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+KEYWORDS_BEAVERS="Beaver|(Castor|C\.)(| )(fiber|canadensis|californicus)"
+KEYWORDS_BEAVERS_EXCLUDE="leave(| )it(| )to(| )beaver|dustbin(| )beaver"
+KEYWORDS_BEAVERS_ALL="$KEYWORDS_BEAVERS"
+
+if [ "$1" == "" ];
+then
+  
+  debug_start "Beavers"
+
+  BEAVERS=$(egrep -i "$KEYWORDS_BEAVERS" "$NEWPAGES")
+
+  categorize "BEAVERS" "Beavers"
+
+  debug_end "Beavers"
+
+fi
