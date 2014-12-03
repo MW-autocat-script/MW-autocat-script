30fc72471d5a1fd9ccdb6ee1cc0e07d10b181280
I am not me <impeccabletiming@gmail.com>
2014-12-03 03:22:21 -0500

    Adding exclusin rules to United Nations

--- catscripts/Government/United_Nations/UnitedNations.sh
+++ catscripts/Government/United_Nations/UnitedNations.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 KEYWORDS_UNITEDNATIONS="United(| )Nations|\bU(|\.)N(|\.)\b"
+KEYWORDS_UNITEDNATIONS_EXCLUDE="\bun-\b" #Because people are dumb
+KEYWORDS_UNITEDNATIONS_ALL="$KEYWORDS_UNITEDNATIONS"
 
 if [ "$1" == "" ]; #normal operation
 then
 
   debug_start "United Nations"
 
-  UNITED=$(egrep -i "$KEYWORDS_UNITEDNATIONS" "$NEWPAGES")
+  UNITED=$(egrep -i "$KEYWORDS_UNITEDNATIONS" "$NEWPAGES" | egrep -i "$KEYWORDS_UNITEDNATIONS_EXCLUDE")
 
   categorize "UNITED" "United Nations"
 
