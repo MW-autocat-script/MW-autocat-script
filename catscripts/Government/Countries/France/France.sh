094b8580df0af3774b6b3eb2ef2284fd83e3e1cb
I am not me <impeccabletiming@gmail.com>
2014-12-05 23:18:13 -0500

    Adding Eiffel Tower

--- catscripts/Government/Countries/France/France.sh
+++ catscripts/Government/Countries/France/France.sh
@@ -3,9 +3,10 @@
 KEYWORDS_FRANCE="France"
 KEYWORDS_PARIS="\bParis\b"
 KEYWORDS_TAHITI="Tahiti"
-KEYWORDS_PARIS_EXCLUDE="Plaster(| )of(| )Paris|Tom(| )Paris|Paris(| )Hilton|Romeo|Juliet" #Tom Paris is a fictional character on Star Trek: Voyager
+KEYWORDS_EIFFELTOWER="Eiffel(| )Tower"
+KEYWORDS_PARIS_EXCLUDE="Plaster(| )of(| )Paris|Tom(| )Paris|Paris(| )Hilton|Romeo|Juliet|$KEYWORDS_EIFFELTOWER" #Tom Paris is a fictional character on Star Trek: Voyager
 KEYWORDS_FRENCH_REVOLUTION="French(| )Revolution|Robespierre"
-KEYWORDS_FRANCE_EXCLUDE="$KEYWORDS_PARIS|$KEYWORDS_TAHITI|$KEYWORDS_FRENCH_REVOLUTION"
+KEYWORDS_FRANCE_EXCLUDE="$KEYWORDS_PARIS|$KEYWORDS_TAHITI|$KEYWORDS_FRENCH_REVOLUTION|$KEYWORDS_EIFFELTOWER"
 KEYWORDS_FRANCE_ALL="$KEYWORDS_FRANCE|$KEYWORDS_PARIS|$KEYWORDS_TAHITI|$KEYWORDS_FRENCH_REVOLUTION"
 
 
@@ -16,11 +17,13 @@ then
 
   FRANCE=$(egrep -i "$KEYWORDS_FRANCE" "$NEWPAGES" | egrep -iv "$KEYWORDS_FRANCE_EXCLUDE")
   PARIS=$(egrep -i "$KEYWORDS_PARIS" "$NEWPAGES" | egrep -iv "$KEYWORDS_PARIS_EXCLUDE")
+  EIFFELTOWER=$(egrep -i "$KEYWORDS_EIFFELTOWER" "$NEWPAGES")
   TAHITI=$(egrep -i "$KEYWORDS_TAHITI" "$NEWPAGES")
   FRENCHREVOLUTION=$(egrep -i "$KEYWORDS_FRENCH_REVOLUTION" "$NEWPAGES")
 
   categorize "FRANCE" "France"
   categorize "PARIS" "Paris"
+  categorize "EIFFELTOWER" "Eiffel Tower"
   categorize "TAHITI" "Tahiti"
   categorize "FRENCHREVOLUTION" "French Revolution"
 
