dd234792fb08197e2c0489ca1af79b15e77449ff
I am not me <impeccabletiming@gmail.com>
2014-12-08 04:58:21 -0500

    Adding 'General Educational Development tests (GEDs)'

--- catscripts/Education/Education.sh
+++ catscripts/Education/Education.sh
@@ -8,11 +8,13 @@ then
   debug_start "Education"
 
   . $EDUCATIONDIR/GCSE/GCSE.sh #KEYWORDS_GCSE_ALL
+  . $EDUCATIONDIR/GED/GED.sh #KEYWORDS_GED_ALL
 
   debug_end "Education"
 
 else
 
   . $EDUCATIONDIR/GCSE/GCSE.sh norun #KEYWORDS_GCSE_ALL
+  . $EDUCATIONDIR/GED/GED.sh norun #KEYWORDS_GED_ALL
 
 fi
---
+++ catscripts/Education/GED/GED.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+KEYWORDS_GED="\bGED(|s)\b|General(| )Educational(| )Development"
+KEYWORDS_GED_ALL="$KEYWORDS_GED"
+
+if [ "$1" == "" ];
+then
+  
+  debug_start "General Educational Development tests (GEDs)"
+
+  GED=$(egrep -i "$KEYWORDS_GED" "$NEWPAGES")
+
+  categorize "GED" "General Educational Development tests (GEDs)"
+
+  debug_end "General Educational Development tests (GEDs)"
+
+fi
