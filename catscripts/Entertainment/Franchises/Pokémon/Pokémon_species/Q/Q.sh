2800996d581d331f3a7b58a4fed6dea8da3b79b5
I am not me <impeccabletiming@gmail.com>
2014-12-13 08:25:35 -0500

    Adding Quilladin

--- catscripts/Entertainment/Franchises/Pokémon/Pokémon_species/Q/Q.sh
+++ catscripts/Entertainment/Franchises/Pokémon/Pokémon_species/Q/Q.sh
@@ -2,8 +2,9 @@
 
 KEYWORDS_QUAGSIRE="Quagsire"
 KEYWORDS_QUILAVA="Quilava"
+KEYWORDS_QUILLADIN="Quilladin"
 KEYWORDS_QWILFISH="Qwilfish"
-KEYWORDS_POKEMONSPECIES_Q="$KEYWORDS_QUAGSIRE|$KEYWORDS_QUILAVA|$KEYWORDS_QWILFISH"
+KEYWORDS_POKEMONSPECIES_Q="$KEYWORDS_QUAGSIRE|$KEYWORDS_QUILAVA|$KEYWORDS_QUILLADIN|$KEYWORDS_QWILFISH"
 
 if [ "$1" == "" ]; #Normal operation
 then
@@ -12,10 +13,12 @@ then
 
   QUAGSIRE=$(egrep -i "$KEYWORDS_QUAGSIRE" "$NEWPAGES")
   QUILAVA=$(egrep -i "$KEYWORDS_QUILAVA" "$NEWPAGES")
+  QUILLADIN=$(egrep -i "$KEYWORDS_QUILLADIN" "$NEWPAGES")
   QWILFISH=$(egrep -i "$KEYWORDS_QWILFISH" "$NEWPAGES")
 
   categorize "QUAGSIRE" "Quagsire"
   categorize "QUILAVA" "Quilava"
+  categorize "QUILLADIN" "Quilladin"
   categorize "QWILFISH" "Qwilfish"
 
   debug_end "Pokemon Q scripts"
