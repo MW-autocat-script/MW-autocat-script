d08f1a497a5b7f960aad104f9e0ef0e7335f46e5
I am not me <impeccabletiming@gmail.com>
2014-12-13 10:12:34 -0500

    Adding Yveltal

--- catscripts/Entertainment/Franchises/Pokémon/Pokémon_species/Y/Y.sh
+++ catscripts/Entertainment/Franchises/Pokémon/Pokémon_species/Y/Y.sh
@@ -3,7 +3,9 @@
 KEYWORDS_YAMASK="Yamask"
 KEYWORDS_YANMA="\bYanma"
 KEYWORDS_YANMEGA="Yanmega"
-KEYWORDS_POKEMONSPECIES_Y="$KEYWORDS_YAMASK|$KEYWORDS_YANMA|$KEYWORDS_YANMEGA"
+KEYWORDS_YVELTAL="Yveltal"
+
+KEYWORDS_POKEMONSPECIES_Y="$KEYWORDS_YAMASK|$KEYWORDS_YANMA|$KEYWORDS_YANMEGA|$KEYWORDS_YVELTAL"
 
 if [ "$1" == "" ];
 then
@@ -13,10 +15,12 @@ then
   YAMASK=$(egrep -i "$KEYWORDS_YAMASK" "$NEWPAGES")
   YANMA=$(egrep -i "$KEYWORDS_YANMA" "$NEWPAGES")
   YANMEGA=$(egrep -i "$KEYWORDS_YANMEGA" "$NEWPAGES")
+  YVELTAL=$(egrep -i "$KEYWORDS_YVELTAL" "$NEWPAGES")
 
   categorize "YAMASK" "Yamask"
   categorize "YANMA" "Yanma"
   categorize "YANMEGA" "Yanmega"
+  categorize "YVELTAL" "Yveltal"
 
   debug_end "Pokemon Y script"
 
