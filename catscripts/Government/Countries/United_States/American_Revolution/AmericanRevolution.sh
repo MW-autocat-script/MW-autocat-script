0320c0fd9c8f3774a178ac38f4fb70036be2a9df
I am not me <impeccabletiming@gmail.com>
2014-12-19 21:18:41 -0500

    Adding American Revolution

---
+++ catscripts/Government/Countries/United_States/American_Revolution/AmericanRevolution.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+KEYWORDS_AMERICANREVOLUTION="American(| )Revolution"
+KEYWORDS_AMERICANREVOLUTION_ALL="$KEYWORDS_AMERICANREVOLUTION"
+
+if [ "$1" == "" ];
+then
+  
+  debug_start "American Revolution"
+
+  AREVOLUTION=$(egrep -i "$KEYWORDS_AMERICANREVOLUTION" "$NEWPAGES")
+
+  categorize "AREVOLUTION" "American Revolution"
+
+  debug_end "American Revolution"
+
+fi
