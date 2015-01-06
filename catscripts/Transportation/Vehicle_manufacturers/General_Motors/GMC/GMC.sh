5a6b2598dccf1bdfc69338b0eb2e5cbf8649bd20
I am not me <impeccabletiming@gmail.com>
2015-01-06 06:29:47 -0500

    Adding GMC Envoy

--- catscripts/Transportation/Vehicle_manufacturers/General_Motors/GMC/GMC.sh
+++ catscripts/Transportation/Vehicle_manufacturers/General_Motors/GMC/GMC.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+KEYWORDS_GMC_ENVOY="GMC(| )Envoy"
 KEYWORDS_GMC_YUKON_XL="GMC(| )Yukon(| )XL|GMC(| )Suburban"
 
 if [ "$1" == "" ];
@@ -7,8 +8,10 @@ then
 
   debug_start "GMC"
 
+  ENVOY=$(egrep -i "$KEYWORDS_GMC_ENVOY" "$NEWPAGES")
   YUKONXL=$(egrep -i "$KEYWORDS_GMC_YUKON_XL" "$NEWPAGES")
 
+  categorize "ENVOY" "GMC Envoy"
   categorize "YUKONXL" "GMC Yukon XL"
 
   debug_end "GMC"
