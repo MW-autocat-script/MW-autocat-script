ee17d6d3f301ee559b941ed63cb169c943447464
I am not me <impeccabletiming@gmail.com>
2015-01-06 06:52:51 -0500

    Adding GMC Sierra and GMC Sonoma

--- catscripts/Transportation/Vehicle_manufacturers/General_Motors/GMC/GMC.sh
+++ catscripts/Transportation/Vehicle_manufacturers/General_Motors/GMC/GMC.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-KEYWORDS_GMC_ENVOY="GMC(| )Envoy"
-KEYWORDS_GMC_YUKON_XL="GMC(| )Yukon(| )XL|GMC(| )Suburban"
+KEYWORDS_GMC_ENVOY="(GMC|[0-9]{2,})(| )Envoy"
+KEYWORDS_GMC_SIERRA="(GMC|[0-9]{2,})(| )Sierra"
+KEYWORDS_GMC_SONOMA="(GMC|[0-9]{2,})(| )Sonoma"
+KEYWORDS_GMC_YUKON_XL="Yukon(| )XL|GMC(| )Suburban"
 
 if [ "$1" == "" ];
 then
@@ -9,9 +11,13 @@ then
   debug_start "GMC"
 
   ENVOY=$(egrep -i "$KEYWORDS_GMC_ENVOY" "$NEWPAGES")
+  SIERRA=$(egrep -i "$KEYWORDS_GMC_SIERRA" "$NEWPAGES")
+  SONOMA=$(egrep -i "$KEYWORDS_GMC_SONOMA" "$NEWPAGES")
   YUKONXL=$(egrep -i "$KEYWORDS_GMC_YUKON_XL" "$NEWPAGES")
 
   categorize "ENVOY" "GMC Envoy"
+  categorize "SIERRA" "GMC Sierra"
+  categorize "SONOMA" "GMC Sonoma"
   categorize "YUKONXL" "GMC Yukon XL"
 
   debug_end "GMC"
