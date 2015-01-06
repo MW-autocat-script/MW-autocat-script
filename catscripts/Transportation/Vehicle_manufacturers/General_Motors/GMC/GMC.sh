b6bdc1abc91910e9b54e7233ea7af601915e5b61
I am not me <impeccabletiming@gmail.com>
2015-01-06 07:50:43 -0500

    Adding GMC Acadia

--- catscripts/Transportation/Vehicle_manufacturers/General_Motors/GMC/GMC.sh
+++ catscripts/Transportation/Vehicle_manufacturers/General_Motors/GMC/GMC.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
+#SUVs
+
+KEYWORDS_GMC_ACADIA="(GMC|[0-9]{2,})(| )Acadia"
 KEYWORDS_GMC_ENVOY="(GMC|[0-9]{2,})(| )Envoy"
+KEYWORDS_GMC_YUKON_XL="Yukon(| )XL|GMC(| )Suburban"
+
+#Trucks
+
 KEYWORDS_GMC_SIERRA="(GMC|[0-9]{2,})(| )Sierra"
 KEYWORDS_GMC_SONOMA="(GMC|[0-9]{2,})(| )Sonoma"
-KEYWORDS_GMC_YUKON_XL="Yukon(| )XL|GMC(| )Suburban"
 
 if [ "$1" == "" ];
 then
 
   debug_start "GMC"
 
+  ACADIA=$(egrep -i "$KEYWORDS_GMC_ACADIA" "$NEWPAGES")
   ENVOY=$(egrep -i "$KEYWORDS_GMC_ENVOY" "$NEWPAGES")
   SIERRA=$(egrep -i "$KEYWORDS_GMC_SIERRA" "$NEWPAGES")
   SONOMA=$(egrep -i "$KEYWORDS_GMC_SONOMA" "$NEWPAGES")
   YUKONXL=$(egrep -i "$KEYWORDS_GMC_YUKON_XL" "$NEWPAGES")
 
+  categorize "ACADIA" "GMC Acadia"
   categorize "ENVOY" "GMC Envoy"
   categorize "SIERRA" "GMC Sierra"
   categorize "SONOMA" "GMC Sonoma"
