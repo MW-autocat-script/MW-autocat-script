1d2cbda975130cd209922a4f03c5da24fa38d8b1
I am not me <impeccabletiming@gmail.com>
2015-02-06 13:59:12 -0500

    Adding keywords to RuneScape

--- catscripts/Entertainment/Video_games/MMORPGs/RuneScape/RuneScape.sh
+++ catscripts/Entertainment/Video_games/MMORPGs/RuneScape/RuneScape.sh
@@ -2,8 +2,10 @@
 
 KEYWORDS_RUNESCAPE_PLACES="Al(| )Kharid|Ardougne|Barbarian(| )Village|Burthorpe|Braindeath(| )Island|Daemonheim|dwarven(| )mine|ectofuntus|Edgeville|\bEntrana|Falador|God(| )Wars(| )Dungeon|Gunnar's(| )Ground|Gunnarsgrunn|Karamja|Kharidian(| )Desert|Lumbridge|Shilo(| )Village|Soul(| )Wars|Varrock"
 KEYWORDS_RUNESCAPE_NPC="Sir(| )Amik(| )Varze|Melzar|Zuriel|Turael|Elvarg|Abbess(| )Benita|\bAberab|50\%(| )Luke|Redbeard(| )Frank"
+KEYWORDS_RUNESCAPE_QUESTS="Stealing(| )Creation"
+KEYWORDS_RUNESCAPE_WEAPONS="(addy|rune|dragon|adamant)(| )(longsword|kite|scimitar|scimmy)|(abyssal|abby)(| )whip|(obsidian|obby)(| )maul"
 
-export KEYWORDS_MMORPG_RUNESCAPE="$KEYWORDS_RUNESCAPE_PLACES|$KEYWORDS_RUNESCAPE_NPC|RuneScape|Rune scape|rune longsword|rune kite|strength potion|(Zamorak|Armadyl|Zaros|Saradomin|Guthix|Bandos)|(Zamorak|Armadyl|Zaros|Saradomin|Guthix|Bandos).+godsword|Third Age Arm(or|our)|(abyssal|abby) whip|(addy|rune|dragon) kite|(rune|dragon) (scimitar|scimmy)|zammy|full sara|(obsidian|obby) maul|(king|queen) black dragon|corporeal beast|cockroach (soldier|worker|drone)|strength pure|abyssal demon|(air|water|earth|fire|Mind|Body|Chaos|Law|Blood|Soul|Cosmic|Death|Nature)(| )(rune|talisman|altar)|Fremen(|n)ik|\bStatius|Dungeoneering|Fractite|Evolution of Combat|Dagannoth|skill(| )cape|abyssal(| )orb"
+export KEYWORDS_MMORPG_RUNESCAPE="$KEYWORDS_RUNESCAPE_PLACES|$KEYWORDS_RUNESCAPE_NPC|$KEYWORDS_RUNESCAPE_QUESTS|$KEYWORDS_RUNESCAPE_WEAPONS|Rune(| )Scape|strength potion|(Zamorak|Armadyl|Zaros|Saradomin|Guthix|Bandos)|Third Age Arm(or|our)|zammy|full sara|(king|queen) black dragon|corporeal beast|cockroach (soldier|worker|drone)|strength pure|abyssal demon|(air|water|earth|fire|Mind|Body|Chaos|Law|Blood|Soul|Cosmic|Death|Nature)(| )(rune|talisman|altar)|Fremen(|n)ik|\bStatius|Dungeoneering|Fractite|Evolution of Combat|Dagannoth|skill(| )cape|abyssal(| )orb"
 
 
 if [ "$1" == "" ];
