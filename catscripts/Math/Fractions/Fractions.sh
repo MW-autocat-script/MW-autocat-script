e929f50c03eb0e7bee1960c46680e5ed92b0a442
I am not me <impeccabletiming@gmail.com>
2015-02-15 07:09:14 -0500

    Adding keywords to Fractions

--- catscripts/Math/Fractions/Fractions.sh
+++ catscripts/Math/Fractions/Fractions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-KEYWORDS_FRACTIONS="(as(| )a|into(| )a)(| )fraction$|in(| )(|its)(lowest(| )terms|simplest(| )form)$|reduced(| )fraction|simplest(| )form(| )of(| )[0-9]{1,}/[0-9]{1,}|[0-9]{1,}/[0-9]{1,}(| )of(| )[0-9]{1,}|[0-9]{1,}/[0-9]{1,}(| )(plus|minus|times|divided(| )by)(| )[0-9]{1,}/[0-9]{1,}|[0-9]{1,}/[0-9]{1,}(| )(plus|minus|times|divided)(| )by(| )[0-9]{1.}(| )[0-9]{1,}/[0-9]{1,}|(im|)proper(| )fraction|mixed(| )number"
+KEYWORDS_FRACTIONS="(as(| )a|into(| )a)(| )fraction$|in(| )(|its)(lowest(| )terms|simplest(| )form)$|reduced(| )fraction|simplest(| )form(| )of(| )[0-9]{1,}/[0-9]{1,}|[0-9]{1,}/[0-9]{1,}(| )of(| )[0-9]{1,}|[0-9]{1,}/[0-9]{1,}(| )(plus|\+|minus|\-|times|\*|divided(| )by)(| )[0-9]{1,}/[0-9]{1,}|[0-9]{1,}/[0-9]{1,}(| )(plus|\+|minus|\-|times|\*|divided)(| )by(| )[0-9]{1.}(| )[0-9]{1,}/[0-9]{1,}|(im|)proper(| )fraction|mixed(| )number"
 
 if [ "$1" == "" ];
 then
