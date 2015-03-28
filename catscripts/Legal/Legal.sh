e18121fcf32dcb6fc881a94a771d4fbe62f7551e
I am not me <impeccabletiming@gmail.com>
2015-03-28 08:44:37 -0400

    Adding Child support

--- catscripts/Legal/Legal.sh
+++ catscripts/Legal/Legal.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 KEYWORDS_LEGAL="\blegal|illegal|\blaw(s|)\b"
+KEYWORDS_CHILDSUPPORT="Child(| )support"
 KEYWORDS_LAWYERS="attorney|lawyer"
 KEYWORDS_DIVORCE="Divorce|alimony"
 KEYWORDS_CRIME="\bCrime(|s)\b|criminal|\bincest"
 KEYWORDS_JACKTHERIPPER="Jack(| )the(| )Ripper"
 KEYWORDS_COPYRIGHT="copyright|copyleft|fair use|(movie|software)(| )piracy|intellectual(| )property"
 . ./catscripts/Government/Countries/United_States/US_constitution/USConstitution.sh norun #KEYWORDS_USCONSTITUTION_ALL
-KEYWORDS_LEGAL_EXCLUDE="$KEYWORDS_DIVORCE|$KEYWORDS_CRIME|$KEYWORDS_LAWYERS|$KEYWORDS_COPYRIGHT|$KEYWORDS_USCONSTITUTION_ALL|$KEYWORDS_JACKTHERIPPER"
+KEYWORDS_LEGAL_EXCLUDE="$KEYWORDS_DIVORCE|$KEYWORDS_CRIME|$KEYWORDS_LAWYERS|$KEYWORDS_COPYRIGHT|$KEYWORDS_USCONSTITUTION_ALL|$KEYWORDS_JACKTHERIPPER|$KEYWORDS_CHILDSUPPORT"
 
 if [ "$1" == "" ]; #Normal operation
 then
@@ -15,6 +16,7 @@ then
   debug_start "Legal"
 
   LEGAL=$(egrep -i "$KEYWORDS_LEGAL" "$NEWPAGES" | egrep -iv "$KEYWORDS_LEGAL_EXCLUDE")
+  CHILDSUPPORT=$(egrep -i "$KEYWORDS_CHILDSUPPORT" "$NEWPAGES")
   CRIME=$(egrep -i "$KEYWORDS_CRIME" "$NEWPAGES")
   DIVORCE=$(egrep -i "$KEYWORDS_DIVORCE" "$NEWPAGES")
   LAWYERS=$(egrep -i "$KEYWORDS_LAWYERS" "$NEWPAGES")
@@ -22,6 +24,7 @@ then
   JACKTHERIPPER=$(egrep -i "$KEYWORDS_JACKTHERIPPER" "$NEWPAGES")
 
   categorize "LEGAL" "Legal"
+  categorize "CHILDSUPPORT" "Child support"
   categorize "CRIME" "Crime"
   categorize "DIVORCE" "Divorce"
   categorize "LAWYERS" "Lawyers"
