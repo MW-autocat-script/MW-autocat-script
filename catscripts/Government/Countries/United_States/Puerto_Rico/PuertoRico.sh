ed392752cbe74567d515134d8013b139ba483bf2
I am not me <impeccabletiming@gmail.com>
2015-04-06 17:04:29 -0400

    Miscellaneous changes to US-related scripts

--- catscripts/Government/Countries/United_States/Puerto_Rico/PuertoRico.sh
+++ catscripts/Government/Countries/United_States/Puerto_Rico/PuertoRico.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 KEYWORDS_PUERTORICO="Puerto(| )Rico|Puerto(| )Rican"
+KEYWORDS_PUERTORICO_ALL="$KEYWORDS_PUERTORICO"
 
 if [ "$1" == "" ]; #Normal operation
 then
--- catscripts/Government/Countries/United_States/US_military/USmilitary.sh
+++ catscripts/Government/Countries/United_States/US_military/USmilitary.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-KEYWORDS_USMILITARY="\bdod\b|Department(| )of(| )Defense|(US|U\.S(|\.)|United(| )States)(| )military"
-KEYWORDS_USMC="(US|U\.S\.|United(| )States)(| )Marine(|s)(| )(|Corps)|USMC"
-KEYWORDS_USARMY="(US|U\.S\.|United(| )States)(| )Army"
-KEYWORDS_USAIRFORCE="(US|U\.S\.|United(| )States)(| )(|ch)Air(| )Force|USAF\b"
-KEYWORDS_USNAVY="(US|U\.S\.|United(| )States)(| )Navy|\bUSN\b"
-KEYWORDS_USCG="(US|U\.S\.|United(| )States)(| )Coast(| )Guard|USCG"
+KEYWORDS_USMILITARY="\bdod\b|Department(| )of(| )Defense|(U(|\.)S(|\.)|United(| )States)(| )military"
+KEYWORDS_USMC="(U(|\.)S(|\.)|United(| )States)(| )Marine(|s)(| )(|Corps)|USMC"
+KEYWORDS_USARMY="(U(|\.)S(|\.)|United(| )States)(| )Army"
+KEYWORDS_USAIRFORCE="(U(|\.)S(|\.)|United(| )States)(| )(|ch)Air(| )Force|USAF\b"
+KEYWORDS_USNAVY="(U(|\.)S(|\.)|United(| )States)(| )Navy|\bUSN\b"
+KEYWORDS_USCG="(U(|\.)S(|\.)|United(| )States)(| )Coast(| )Guard|USCG"
 KEYWORDS_USMILITARY_EXCLUDE="$KEYWORDS_USMC|$KEYWORDS_USARMY|$KEYWORDS_USAIRFORCE|$KEYWORDS_USNAVY|$KEYWORDS_USCG|Lott Dod"
 KEYWORDS_USMILITARY_ALL="$KEYWORDS_USMILITARY|$KEYWORDS_USMC|$KEYWORDS_USARMY|$KEYWORDS_USAIRFORCE|$KEYWORDS_USNAVY|$KEYWORDS_USCG"
 
