aefde6150f5673a881b1ee3ab62423f0a29eee9a
I am not me <impeccabletiming@gmail.com>
2015-04-22 00:48:31 -0400

    Adding 'Neymar da Silva Santos Júnior'

--- catscripts/Lifestyle/Sports/Soccer/Soccer.sh
+++ catscripts/Lifestyle/Sports/Soccer/Soccer.sh
@@ -3,7 +3,8 @@
 KEYWORDS_SOCCER="soccer|footballer"
 KEYWORDS_CRONALDO="Cristiano(| )Ronaldo|\bC(\.|-| )(| )ronaldo"
 KEYWORDS_LMESSI="\bMessi(|s)\b"
-KEYWORDS_SOCCER_EXCLUDE="$KEYWORDS_CRONALDO|$KEYWORDS_LMESSI"
+KEYWORDS_NEYMAR="Neymar"
+KEYWORDS_SOCCER_EXCLUDE="$KEYWORDS_CRONALDO|$KEYWORDS_LMESSI|$KEYWORDS_NEYMAR"
 
 if [ "$1" == "" ];
 then
@@ -13,10 +14,12 @@ then
   SOCCER=$(egrep -i "$KEYWORDS_SOCCER" "$NEWPAGES" | egrep -iv "$KEYWORDS_SOCCER_EXCLUDE")
   CRONALDO=$(egrep -i "$KEYWORDS_CRONALDO" "$NEWPAGES")
   LMESSI=$(egrep -i "$KEYWORDS_LMESSI" "$NEWPAGES")
+  NEYMAR=$(egrep -i "$KEYWORDS_NEYMAR" "$NEWPAGES")
 
   categorize "SOCCER" "Soccer (association football)"
   categorize "CRONALDO" "Cristiano Ronaldo"
   categorize "LMESSI" "Lionel Messi"
+  categorize "NEYMAR" "Neymar da Silva Santos Júnior"
 
   debug_end "Soccer"
 
