c7f8b10566561d25b4d9e7f239a835e27bc3bac2
I am not me <impeccabletiming@gmail.com>
2015-05-03 04:10:58 -0400

    Adding David Beckham

--- catscripts/Lifestyle/Sports/Soccer/Soccer.sh
+++ catscripts/Lifestyle/Sports/Soccer/Soccer.sh
@@ -4,7 +4,8 @@ KEYWORDS_SOCCER="soccer|footballer"
 KEYWORDS_CRONALDO="Cristiano(| )Ronaldo|\bC(\.|-| )(| )ronaldo"
 KEYWORDS_LMESSI="\bMessi(|s)\b"
 KEYWORDS_NEYMAR="Neymar"
-KEYWORDS_SOCCER_EXCLUDE="$KEYWORDS_CRONALDO|$KEYWORDS_LMESSI|$KEYWORDS_NEYMAR"
+KEYWORDS_DAVIDBECKHAM="David(| )Beckham"
+KEYWORDS_SOCCER_EXCLUDE="$KEYWORDS_CRONALDO|$KEYWORDS_LMESSI|$KEYWORDS_NEYMAR|$KEYWORDS_DAVIDBECKHAM"
 
 if [ "$1" == "" ];
 then
@@ -15,11 +16,13 @@ then
   CRONALDO=$(egrep -i "$KEYWORDS_CRONALDO" "$NEWPAGES")
   LMESSI=$(egrep -i "$KEYWORDS_LMESSI" "$NEWPAGES")
   NEYMAR=$(egrep -i "$KEYWORDS_NEYMAR" "$NEWPAGES")
+  DAVIDBECKHAM=$(egrep -i "$KEYWORDS_DAVIDBECKHAM" "$NEWPAGES")
 
   categorize "SOCCER" "Soccer (association football)"
   categorize "CRONALDO" "Cristiano Ronaldo"
   categorize "LMESSI" "Lionel Messi"
   categorize "NEYMAR" "Neymar da Silva Santos Júnior"
+  categorize "DAVIDBECKHAM" "David Beckham"
 
   debug_end "Soccer"
 
