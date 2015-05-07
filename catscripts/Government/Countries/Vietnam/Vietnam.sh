5c4ce57afdf4051a6d8e41234e1be177aacbfd6e
I am not me <impeccabletiming@gmail.com>
2015-05-07 10:48:28 -0400

    Adding Hanoi

--- catscripts/Government/Countries/Vietnam/Vietnam.sh
+++ catscripts/Government/Countries/Vietnam/Vietnam.sh
@@ -2,7 +2,8 @@
 
 KEYWORDS_VIETNAM="Vietnam(|s)\b"
 KEYWORDS_VIETNAMWAR="Vietnam(| )(War|Vet(eran|(\b|s)))|(North|South)(|ern)(| )Vietnam|Tet(| )Offensive"
-KEYWORDS_VIETNAM_EXCLUDE="$KEYWORDS_VIETNAMWAR"
+KEYWORDS_HANOI="\bHanoi"
+KEYWORDS_VIETNAM_EXCLUDE="$KEYWORDS_VIETNAMWAR|$KEYWORDS_HANOI"
 KEYWORDS_VIETNAM_ALL="$KEYWORDS_VIETNAM|$KEYWORDS_VIETNAMWAR"
 
 if [ "$1" == "" ];
@@ -12,9 +13,11 @@ then
 
   VIETNAM=$(egrep -i "$KEYWORDS_VIETNAM" "$NEWPAGES" | egrep -iv "$KEYWORDS_VIETNAM_EXCLUDE")
   VIETNAMWAR=$(egrep -i "$KEYWORDS_VIETNAMWAR" "$NEWPAGES")
+  HANOI=$(egrep -i "$KEYWORDS_HANOI" "$NEWPAGES")
 
   categorize "VIETNAM" "Vietnam"
   categorize "VIETNAMWAR" "Vietnam War"
+  categorize "HANOI" "Hanoi"
 
   debug_end "Vietnam"
 
