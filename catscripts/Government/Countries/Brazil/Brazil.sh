54e7ce46277b9ba702424f0e326ad75d7f3fbe62
I am not me <impeccabletiming@gmail.com>
2015-06-16 01:16:24 -0400

    Adding Rio de Janeiro

--- catscripts/Government/Countries/Brazil/Brazil.sh
+++ catscripts/Government/Countries/Brazil/Brazil.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 KEYWORDS_BRAZIL="Brazil"
-KEYWORDS_BRAZIL_ALL="$KEYWORDS_BRAZIL"
+KEYWORDS_RIODEJANEIRO="Rio(| )de(| )Janeiro"
+KEYWORDS_BRAZIL_EXCLUDE="$KEYWORDS_RIODEJANEIRO"
+KEYWORDS_BRAZIL_ALL="$KEYWORDS_BRAZIL|$KEYWORDS_RIODEJANEIRO"
 
 if [ "$1" == "" ];
 then
@@ -11,6 +13,10 @@ then
   BRAZIL=$(egrep -i "$KEYWORDS_BRAZIL" "$NEWPAGES")
 
   categorize "BRAZIL" "Brazil"
+
+  RIODEJANEIRO=$(egrep -i "$KEYWORDS_RIODEJANEIRO" "$NEWPAGES")
+
+  categorize "RIODEJANEIRO" "Rio de Janeiro"
 
   debug_end "Brazil"
 
