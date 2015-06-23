b7abec756f54db8d44354ccf1f7b3021c61a2aa3
I am not me <impeccabletiming@gmail.com>
2015-06-23 02:38:56 -0400

    Adding exclusion rules to Definitions

--- catscripts/Language/Definitions/Definitions.sh
+++ catscripts/Language/Definitions/Definitions.sh
@@ -5,6 +5,7 @@ KEYWORDS_WHATDOES="^What(| )does(| )(|the(| )word)(| )[a-z]{1,} mean$|^What(| )d
 KEYWORDS_WHATARE="^What(| )are(| )[a-z]{1,}$|^What(| )are(| )[a-z]{1,} [a-z]{1,}$"
 KEYWORDS_MEANING="^What(| )is(| )the(| )meaning(| )of(| )(|the(| )word)(| )[a-z]{1,}$|^What(| )is(| )the(| )meaning(| )of(| )(|the(| )word)(| )[a-z]{1,} [a-z]{1,}$"
 KEYWORDS_DEFINITIONS="$KEYWORDS_WHATIS|$KEYWORDS_WHATDOES|$KEYWORDS_WHATARE|$KEYWORDS_MEANING"
+KEYWORDS_DEFINITIONS_EXCLUDE="^What(| )is(| )in(| )[a-z]{1,}$"
 KEYWORDS_NAMES="^What does the (sur|)name (|\"|')[a-z]{1,}(|\"|') mean|What is the meaning of the (sur|)name (|'|\")[a-z]{1,}(|'|\")"
 
 if [ "$1" == "" ]; #Normal operation
@@ -12,7 +13,7 @@ then
 
   debug_start "Definitions"
 
-  DEFINITIONS=$(egrep -i "$KEYWORDS_DEFINITIONS" "$NEWPAGES")
+  DEFINITIONS=$(egrep -i "$KEYWORDS_DEFINITIONS" "$NEWPAGES" | egrep -iv "$KEYWORDS_DEFINITIONS_EXCLUDE")
   NAMEMEANINGS=$(egrep -i "$KEYWORDS_NAMES" "$NEWPAGES")
 
   categorize "DEFINITIONS" "Definitions"
