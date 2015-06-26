adfe774c65e4779374b757de33aa0c229984cd92
I am not me <impeccabletiming@gmail.com>
2015-06-26 02:31:36 -0400

    Adding C programming

---
+++ catscripts/Technology/Computers/Computer_programming/C_programming/CProgramming.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+KEYWORDS_CPROGRAMMING="\bC program|\bin C$"
+KEYWORDS_CPROGRAMMING_ALL="$KEYWORDS_CPROGRAMMING"
+
+if [ "$1" == "" ];
+then
+  
+  debug_start "C programming"
+
+  CPROGRAMMING=$(egrep -i "$KEYWORDS_CPROGRAMMING" "$NEWPAGES")
+
+  categorize "CPROGRAMMING" "C programming"
+
+  debug_end "C programming"
+
+fi
