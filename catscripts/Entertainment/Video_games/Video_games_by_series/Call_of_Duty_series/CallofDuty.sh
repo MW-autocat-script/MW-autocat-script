7d02fa13398efe7c8b5342e56a8fb4c9aadec4ad
I am not me <impeccabletiming@gmail.com>
2015-10-17 04:07:47 -0400

    Adding Call of Duty: Adavanced Warfare and Call of Duty: Black Ops III

--- catscripts/Entertainment/Video_games/Video_games_by_series/Call_of_Duty_series/CallofDuty.sh
+++ catscripts/Entertainment/Video_games/Video_games_by_series/Call_of_Duty_series/CallofDuty.sh
@@ -9,8 +9,10 @@ KEYWORDS_CODBO_EXCLUDE="Black(| )Ops(| )([2-9]{1,}|\bII\b|\bIII\b|\bIV\b|\bV\b)"
 KEYWORDS_CODMW3="\bCOD(| )8|\bMW3\b|Modern(| )Warfare(| )3|Call(| )of(| )Duty(| )8"
 KEYWORDS_CODBO2="\bCOD(| )9|Black Ops (2|II\b)|Call of Duty 9"
 KEYWORDS_CODGHOSTS="\bCOD(| )10|Call(| )Of(| )Duty(\:|)(| )Ghost"
+KEYWORDS_CODAW="\bCOD(| )11|Call(| )of(| )Duty(\:|)(| )(AW|Advanced)(| )(Warfare|)"
+KEYWORDS_CODBO3="COD(| )12|Black(| )Ops(| )(3|III)|Call(| )of(| )Duty(| )12"
 KEYWORDS_CALLOFDUTYSERIES="Call(| )of(| )Duty|CoD(| )series"
-KEYWORDS_CALLOFDUTYSERIES_EXCLUDE="$KEYWORDS_COD4|$KEYWORDS_CODWAW|$KEYWORDS_CODBO|$KEYWORDS_CODMW3|$KEYWORDS_CODBO2|$KEYWORDS_CODGHOSTS"
+KEYWORDS_CALLOFDUTYSERIES_EXCLUDE="$KEYWORDS_COD4|$KEYWORDS_CODWAW|$KEYWORDS_CODBO|$KEYWORDS_CODMW3|$KEYWORDS_CODBO2|$KEYWORDS_CODGHOSTS|$KEYWORDS_CODAW"
 KEYWORDS_CALLOFDUTYSERIES_CASESENSITIVE="CoD"
 KEYWORDS_CALLOFDUTYSERIES_CASESENSITIVE_EXCLUDE="CoD(| )[0-9]{1,}" 
 
@@ -19,7 +21,7 @@ then
 
   debug_start "Call of Duty series"
 
-  CALLOFDUTY=$(egrep -i "$KEYWORDS_CALLOFDUTYSERIES" "$NEWPAGES" | egrep -iv "$KEYWORDS_CALLOFDUTYSERIES_EXCLUDE" && egrep "$KEYWORDS_CALLOFDUTYSERIES_CASESENSITIVE" "$NEWPAGES" | egrep -v "$KEYWORDS_CALLOFDUTYSERIES_CASESENSITIVE_EXCLUDE")
+  CALLOFDUTY=$(egrep -i "$KEYWORDS_CALLOFDUTYSERIES" "$NEWPAGES" | egrep -iv "$KEYWORDS_CALLOFDUTYSERIES_EXCLUDE"; egrep "$KEYWORDS_CALLOFDUTYSERIES_CASESENSITIVE" "$NEWPAGES" | egrep -v "$KEYWORDS_CALLOFDUTYSERIES_CASESENSITIVE_EXCLUDE")
   MODERN=$(egrep -i "$KEYWORDS_COD4" "$NEWPAGES" | egrep -iv "$KEYWORDS_COD4_EXCLUDE")
   WORLDATWAR=$(egrep -i "$KEYWORDS_CODWAW" "$NEWPAGES")
   MW2=$(egrep -i "$KEYWORDS_CODMW2" "$NEWPAGES")
@@ -27,6 +29,8 @@ then
   MW3=$(egrep -i "$KEYWORDS_CODMW3" "$NEWPAGES")
   BO2=$(egrep -i "$KEYWORDS_CODBO2" "$NEWPAGES")
   GHOSTS=$(egrep -i "$KEYWORDS_CODGHOSTS" "$NEWPAGES")
+  CODAW=$(egrep -i "$KEYWORDS_CODWAW" "$NEWPAGES")
+  BO3=$(egrep -i "$KEYWORDS_CODBO3" "$NEWPAGES")
 
   categorize "CALLOFDUTY" "Call of Duty series"
   categorize "MODERN" "Call of Duty 4: Modern Warfare"
@@ -36,6 +40,8 @@ then
   categorize "MW3" "Call of Duty: Modern Warfare 3"
   categorize "BO2" "Call of Duty: Black Ops II"
   categorize "GHOSTS" "Call of Duty: Ghosts"
+  categorize "CODAW" "Call of Duty: Advanced Warfare"
+  categorize "BO3" "Call of Duty: Black Ops III"
 
   debug_end "Call of Duty series"
 
